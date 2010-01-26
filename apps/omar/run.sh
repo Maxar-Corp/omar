d1ac261e373e0612e23a4c5c5334c808bdf6da71
sbortman <sbortman@b630af17-092b-0410-afba-a002956477a4>
2010-01-26 19:16:13 +0000

    Got tired of modding these files.  They now accept params
    setupdb.sh [mode]  where mode is either dev or prod (defaults to prod)
    
    run.sh [mode] [style] [foreground] where
     
    mode is either dev or prod (defaults to prod)
    style is either app or war (defaults to war)
    foreground is either true or false (defaults to false)
    
    if run.sh is invoked with no params,  it will use the default 
    deployment profile:
    
    nohup grails prod run-war &
    
    
    
    git-svn-id: http://svn.osgeo.org/ossim/omar@16418 b630af17-092b-0410-afba-a002956477a4

--- apps/omar/run.sh
+++ apps/omar/run.sh
@@ -7,5 +7,32 @@ export JAVA_OPTS="-Xmx1024m -Xms256m -Djava.awt.headless=true"
 #grails prod run-app
 #grails prod run-war
 #grails run-war
-grails run-app
+#grails run-app
+
+case "$1" in
+dev)
+export mode="dev"
+;;
+*)
+export mode="prod"
+;;
+esac
+
+case "$2" in
+app)
+export style="app"
+;;
+*)
+export style="war"
+;;
+esac
+
+case "$3" in
+true)
+grails ${mode} run-${style}  
+;;
+*)
+nohup grails ${mode} run-${style} & 
+;;
+esac
 
