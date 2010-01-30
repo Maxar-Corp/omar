37a36dbf53d5b3dd92367873631e6fe6b0739303
sbortman <sbortman@b630af17-092b-0410-afba-a002956477a4>
2010-01-30 20:37:53 +0000

    Just trying to make things more flexible.
    git-svn-id: http://svn.osgeo.org/ossim/omar@16455 b630af17-092b-0410-afba-a002956477a4

--- apps/uimakeover/grails-app/controllers/LayoutTestController.groovy
+++ apps/uimakeover/grails-app/controllers/LayoutTestController.groovy
@@ -6,30 +6,33 @@ class LayoutTestController
 
   def index = {
 
-    //def baseAddress = "http://hypercube.telascience.org/cgi-bin/haiti?map=/geo/haiti/mapfiles/4326.map&"
-    //def baseAddress = "http://hypercube.telascience.org/cgi-bin/mapserv?map=/home/racicot/haiti/mapfiles/basedata.map&"
-    //def baseAddress = "http://${InetAddress.localHost.hostAddress}/tilecache/tilecache.py?"
-    //def baseAddress = "http://hypersphere.telascience.org/cgi-bin/bmng?"
-
-    //def baseAddress = "http://${InetAddress.localHost.hostAddress}/cgi-bin/mapserv?map=/data/bmng.map&"
-    def baseAddress = "http://hypersphere.telascience.org/geothumper/tilecache/tilecache.cgi?"
-
-    def layers = wmsUtilsService.getLayerList(baseAddress)
+    def baseAddresses = [
+        //"http://hypercube.telascience.org/cgi-bin/haiti?map=/geo/haiti/mapfiles/4326.map&",
+        //"http://hypercube.telascience.org/cgi-bin/mapserv?map=/home/racicot/haiti/mapfiles/basedata.map&",
+        "http://${InetAddress.localHost.hostAddress}/tilecache/tilecache.py?"
+        //"http://hypersphere.telascience.org/cgi-bin/bmng?",
+        //"http://${InetAddress.localHost.hostAddress}/cgi-bin/mapserv?map=/data/bmng.map&",
+        //"http://hypersphere.telascience.org/geothumper/tilecache/tilecache.cgi?"
+    ]
 
     def baseLayers = []
     def overlayLayers = []
 
-    layers.each {
-      switch ( it.name )
-      {
-        case "omar":
-        case "basic":
-        case "world_topo_bathy":
-        case "Reference":  
-          baseLayers << it
-          break
-        default:
-          overlayLayers << it
+    baseAddresses.each {baseAddress ->
+      def layers = wmsUtilsService.getLayerList(baseAddress)
+
+      layers.each {
+        switch ( it.name )
+        {
+          case "omar":
+          case "basic":
+          case "world_topo_bathy":
+          case "Reference":
+            baseLayers << it
+            break
+          default:
+            overlayLayers << it
+        }
       }
     }
 
@@ -45,23 +48,33 @@ class LayoutTestController
   def test1 = {}
 
   def index2 = {
-    //def baseAddress = "http://hypercube.telascience.org/cgi-bin/haiti?map=/geo/haiti/mapfiles/4326.map&"
-    //def baseAddress = "http://hypercube.telascience.org/cgi-bin/mapserv?map=/home/racicot/haiti/mapfiles/basedata.map&"
-    def baseAddress = "http://${InetAddress.localHost.hostAddress}/tilecache/tilecache.py?"
-    def layers = wmsUtilsService.getLayerList(baseAddress)
+    def baseAddresses = [
+        //"http://hypercube.telascience.org/cgi-bin/haiti?map=/geo/haiti/mapfiles/4326.map&",
+        //"http://hypercube.telascience.org/cgi-bin/mapserv?map=/home/racicot/haiti/mapfiles/basedata.map&",
+        "http://${InetAddress.localHost.hostAddress}/tilecache/tilecache.py?"
+        //"http://hypersphere.telascience.org/cgi-bin/bmng?",
+        //"http://${InetAddress.localHost.hostAddress}/cgi-bin/mapserv?map=/data/bmng.map&",
+        //"http://hypersphere.telascience.org/geothumper/tilecache/tilecache.cgi?"
+    ]
 
     def baseLayers = []
     def overlayLayers = []
 
-    layers.each {
-      switch ( it.name )
-      {
-        case "omar":
-        case "basic":
-          baseLayers << it
-          break
-        default:
-          overlayLayers << it
+    baseAddresses.each {baseAddress ->
+      def layers = wmsUtilsService.getLayerList(baseAddress)
+
+      layers.each {
+        switch ( it.name )
+        {
+          case "omar":
+          case "basic":
+          case "world_topo_bathy":
+          case "Reference":
+            baseLayers << it
+            break
+          default:
+            overlayLayers << it
+        }
       }
     }
 
