682ab063ab14d3f0332562cfc7b0a0c26f663a17
sbortman <sbortman@b630af17-092b-0410-afba-a002956477a4>
2010-02-05 03:39:50 +0000

    Added new file
    git-svn-id: http://svn.osgeo.org/ossim/omar@16537 b630af17-092b-0410-afba-a002956477a4

---
+++ apps/omar/test/unit/RasterEntryMetadataTests.groovy
@@ -0,0 +1,15 @@
+import grails.test.*
+
+class RasterEntryMetadataTests extends GrailsUnitTestCase {
+    protected void setUp() {
+        super.setUp()
+    }
+
+    protected void tearDown() {
+        super.tearDown()
+    }
+
+    void testSomething() {
+
+    }
+}
