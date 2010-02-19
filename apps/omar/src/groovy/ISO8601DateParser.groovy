4a96e3bce94b9938f208b2efa92ef1c76a1da4b0
gpotts <gpotts@b630af17-092b-0410-afba-a002956477a4>
2010-02-19 02:04:33 +0000

    Added some date parsing
    git-svn-id: http://svn.osgeo.org/ossim/omar@16612 b630af17-092b-0410-afba-a002956477a4

---
+++ apps/omar/src/groovy/ISO8601DateParser.groovy
@@ -0,0 +1,196 @@
+import java.text.SimpleDateFormat
+import javax.xml.datatype.Duration
+class ISO8601DateParser
+{
+  static def convertStringToSimpleDate(String dateString, String pattern)
+  {
+    def simpleDateFormat = new SimpleDateFormat(pattern)
+    def result = (Date)null
+
+     try
+     {
+       result = simpleDateFormat.parse(dateString)
+     }
+     catch(java.lang.Exception e)
+     {
+       result = (Date)null
+     }
+
+    return result
+  }
+  static def convertStringToDuration(String durationString)
+  {
+    def result = (OmarDuration)null;
+
+    if(durationString.size()<1)
+    {
+      return result
+    }
+
+    if(durationString.charAt(0) == 'P')
+    {
+      def currentValue = ""
+      def testString = ""
+      def timeFlag = false;
+      result = new OmarDuration();
+      (1..<durationString.size()).each{
+        char c = durationString.charAt(it);
+        switch(c)
+        {
+          case 'Y':
+            result.years = Integer.valueOf(testString)
+            testString = ""
+            break
+          case 'M':
+            if(timeFlag)
+            {
+              result.minutes =  Integer.valueOf(testString)
+            }
+            else
+            {
+              result.months =  Integer.valueOf(testString)
+            }
+            testString = ""
+            break
+          case 'D':
+            result.days = Integer.valueOf(testString)
+            break
+          case 'H':
+            result.hours = Integer.valueOf(testString)
+            break
+          case 'S':
+            result.seconds = Integer.valueOf(testString)
+            break
+          case 'T':
+            timeFlag = true
+            break
+          default:
+            testString += c
+        }
+      }
+    }
+
+    return result
+  }
+  static def convertStringToSimpleDate(String dateString)
+  {
+
+    Date result = convertStringToSimpleDate(dateString, "yyyyMMdd")
+
+    if(result)
+    {
+      return result;
+    }
+    result = convertStringToSimpleDate(dateString, "yyyy-MM-dd'T'hh:mm:ssZ")
+    if(result)
+    {
+      return result;
+    }
+    result = convertStringToSimpleDate(dateString, "yyyy-MM-dd'T'hh:mm:ss")
+    if(result)
+    {
+      return result;
+    }
+    result = convertStringToSimpleDate(dateString, "yyyy-MM-dd hh:mm:ss")
+    if(result)
+    {
+      return result;
+    }
+    result = convertStringToSimpleDate(dateString, "yyyy-MM-dd'T'hh:mm")
+    if(result)
+    {
+      return result;
+    }
+    result = convertStringToSimpleDate(dateString, "yyyy-MM-dd hh:mm")
+    if(result)
+    {
+      return result;
+    }
+    result = convertStringToSimpleDate(dateString, "yyyy-MM-dd'T'hh")
+    if(result)
+    {
+      return result;
+    }
+    result = convertStringToSimpleDate(dateString, "yyyy-MM-dd hh")
+    if(result)
+    {
+      return result;
+    }
+    result = convertStringToSimpleDate(dateString, "yyyy-MM-dd")
+    if(result)
+    {
+      return result;
+    }
+    result = convertStringToSimpleDate(dateString, "yyyy-mm")
+    if(result)
+    {
+      return result;
+    }
+    result = convertStringToSimpleDate(dateString, "yyyy")
+    
+    return result;
+  }
+  
+
+  static def getDateRange(String iso8601String)
+  {
+    String[] startEndSplit = iso8601String.split('/')
+
+    if(startEndSplit.size() ==0)
+    {
+      return false
+    }
+    Date         startDate = (Date)null
+    Date         endDate   = (Date)null
+    OmarDuration startDuration = convertStringToDuration(startEndSplit[0])
+    OmarDuration endDuration   = convertStringToDuration(startEndSplit[0])
+    if(startEndSplit.size()>0)
+    {
+      startDuration = convertStringToDuration(startEndSplit[0])
+      if(!startDuration)
+      {
+        startDate   = convertStringToSimpleDate(startEndSplit[0])
+      }
+
+      if(startEndSplit.size() > 1)
+      {
+        endDuration = convertStringToDuration(startEndSplit[1])
+        if(!endDuration)
+        {
+          endDate   = convertStringToSimpleDate(startEndSplit[1])
+        }
+      }
+    }
+    if(startDate)
+    {
+      if(endDuration)
+      {
+        def calendar = new GregorianCalendar()
+        calendar.setTime(startDate);
+        endDuration.addTo(calendar);
+        endDate = calendar.getTime();
+      }
+      else if(!endDate)
+      {
+        return [startDate]
+      }
+    }
+    else if(endDate)
+    {
+      if(startDuration)
+      {
+        def calendar = new GregorianCalendar()
+        startDuration.durationSign = -1
+        calendar.setTime(endDate);
+        startDuration.addTo(calendar);
+        startDate = calendar.getTime();
+      }
+      else
+      {
+        return [] // need at least some kind of start
+      }
+    }
+
+    return [startDate, endDate]
+  }
+}
