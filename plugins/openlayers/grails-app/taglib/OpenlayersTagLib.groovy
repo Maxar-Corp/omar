03ab039d4b31b48a9fd4fa2bdc996dfe93fdc091
dlucas <dlucas@b630af17-092b-0410-afba-a002956477a4>
2010-03-03 15:34:30 +0000

    Updated taglib for ie6 switch. ~dlucas
    git-svn-id: http://svn.osgeo.org/ossim/omar@16780 b630af17-092b-0410-afba-a002956477a4

--- plugins/openlayers/grails-app/taglib/OpenlayersTagLib.groovy
+++ plugins/openlayers/grails-app/taglib/OpenlayersTagLib.groovy
@@ -2,6 +2,8 @@ class OpenlayersTagLib
 {
   static namespace = 'openlayers'
 
+  def ie6Boolean = 'false'
+
   def loadJavascript = { attrs ->
     def path = createLinkTo( dir:"${pluginContextPath}/js", file:"OpenLayers.js" )
     def output = "<script type='text/javascript' src='${path}'></script>"
@@ -18,11 +20,24 @@ class OpenlayersTagLib
   }
 
   def loadMapToolBar = { attrs ->
+
+
+    if (ie6Boolean == 'true')
+    {
+
     def theme = attrs['theme'] ?: 'default'
+    def path = createLinkTo( dir:"${pluginContextPath}/css", file:"mapwidget3.css" )
+    def output = "<link rel='stylesheet' href='${path}' type='text/css'/>"
+    out << output
+    }
+    else
+    {
+      def theme = attrs['theme'] ?: 'default'
     def path = createLinkTo( dir:"${pluginContextPath}/css", file:"mapwidget2.css" )
     def output = "<link rel='stylesheet' href='${path}' type='text/css'/>"
-
-    out << output
+      out << output
+    }
   }
 
 }
+
