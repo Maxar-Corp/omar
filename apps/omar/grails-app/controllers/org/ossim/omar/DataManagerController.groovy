4f0243c9d9dd7ca72f0f6309502e6b2b0375dd0b
gpotts <gpotts@b630af17-092b-0410-afba-a002956477a4>
2010-03-12 16:23:17 +0000

    Adding documentation
    git-svn-id: http://svn.osgeo.org/ossim/omar@16862 b630af17-092b-0410-afba-a002956477a4

--- apps/omar/grails-app/controllers/org/ossim/omar/DataManagerController.groovy
+++ apps/omar/grails-app/controllers/org/ossim/omar/DataManagerController.groovy
@@ -1,5 +1,15 @@
 package org.ossim.omar
 
+/**
+ * DataManagerController supplies a simple API to add and remove raster
+ * and video datasets to the OMAR tables.  Currently, only a filenam argument is passed
+ *
+ * All controller methods will use the internal params variable to parse out
+ * the required arguments.  All http parameters are stored as a Java HashMap type
+ * in a variable called params.
+ *
+ * @see DataManagerService
+ */
 class DataManagerController
 {
   def dataManagerService
@@ -7,6 +17,20 @@ class DataManagerController
   def index = {
   }
 
+  /**
+   * renders a response back to the caller to notify if the raster was added successfully
+   * or not.  The response header is populated with the proper response code.
+   * for adding a raster you must use the http method POST or PUT.  If you are going through the
+   * browser then a GET method will bring up a form to type in a file to add to the table.<br>
+   * Example use case:
+   * <br>
+   * <code>
+   *   http://&lt;server&gt;/omar/dataManager/addRaster?filename="&lt;myFileToAdd&gt;"
+   * </code>
+   *
+   * <br>
+   * @param params.filename This is a http parameter of the form filename="&lt;raster to add&gt;"
+   */
   def addRaster = {
     def method = request.method.toUpperCase()
     def filename = params.filename
@@ -34,6 +58,20 @@ class DataManagerController
     }
   }
 
+  /**
+    * renders a response back to the caller to notify if the video was added successfully
+    * or not.  For adding a video you must use the http method POST or PUT. If you are going through the
+   *  browser then a GET method will bring up a form to type in a file to add to the table.<br>
+    * Example use case:
+    * <br>
+    * <code>
+    *   http://&lt;server&gt;/omar/dataManager/addVideo?filename="&lt;myFileToAdd&gt;"
+    * </code>
+    *
+    * <br>
+    * @param params.filename This is a http parameter of the form filename="&lt;video to add&gt;"
+    * @return Proper http response code is rendered back to the caller 
+    */
   def addVideo = {
     def method = request.method.toUpperCase()
     def filename = params.filename
@@ -64,6 +102,20 @@ class DataManagerController
 
   }
 
+  /**
+    * renders a response back to the caller to notify if the raster was removed successfully
+    * or not.  For removing a raster you must use the http method POST or DELETE.  If you are going through the
+    * browser then a GET method will bring up a form to type in a file to delete from the table.<br>
+    * Example use case:
+    * <br>
+    * <code>
+    *   http://&lt;server&gt;/omar/dataManager/removeRaster?filename="&lt;myFileToRemove&gt;"
+    * </code>
+    *
+    * <br>
+    * @param params.filename This is a http parameter of the form filename="&lt;raster to add&gt;"
+    * @return Proper http response code is rendered back to the caller
+    */
   def removeRaster = {
     def method = request.method.toUpperCase()
     def filename = params.filename
@@ -94,6 +146,20 @@ class DataManagerController
     }
   }
 
+  /**
+    * renders a response back to the caller to notify if the video was removed successfully
+    * or not.  For removing a video you must use the http method POST or DELETE.  If you are going through the
+    * browser then a GET method will bring up a form to type in a file to delete from the table.<br>
+    * Example use case:
+    * <br>
+    * <code>
+    *   http://&lt;server&gt;/omar/dataManager/removeVideo?filename="&lt;myFileToRemove&gt;"
+    * </code>
+    *
+    * <br>
+    * @param params.filename This is a http parameter of the form filename="&lt;video to remove&gt;"
+    * @return Proper http response code is rendered back to the caller
+    */
   def removeVideo = {
     def method = request.method.toUpperCase()
     def filename = params.filename
