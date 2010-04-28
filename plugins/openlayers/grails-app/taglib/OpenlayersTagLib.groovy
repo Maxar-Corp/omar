d10ecf959faf875e17fbd2565355d849daf10d1b
sbortman <sbortman@b630af17-092b-0410-afba-a002956477a4>
2010-04-28 19:48:27 +0000

    Upgraded to OpenLayers 2.9 Copied some javascript code in here for coord converstion and map widget
    git-svn-id: http://svn.osgeo.org/ossim/omar@17239 b630af17-092b-0410-afba-a002956477a4

--- plugins/openlayers/grails-app/taglib/OpenlayersTagLib.groovy
+++ plugins/openlayers/grails-app/taglib/OpenlayersTagLib.groovy
@@ -3,35 +3,35 @@ class OpenlayersTagLib
   static namespace = 'openlayers'
   def grailsApplication
 
-  def loadJavascript = { attrs ->
-    def path = createLinkTo( dir:"${pluginContextPath}/js", file:"OpenLayers.js" )
+  def loadJavascript = {attrs ->
+    def path = resource(plugin: "openlayers", dir: "js", file: "OpenLayers.js")
     def output = "<script type='text/javascript' src='${path}'></script>"
 
     out << output
   }
 
-  def loadTheme = { attrs ->
+  def loadTheme = {attrs ->
     def theme = attrs['theme'] ?: 'default'
-    def path = createLinkTo( dir:"${pluginContextPath}/js/theme/${theme}", file:"style.css" )
+    def path = resource(plugin: "openlayers", dir: "js/theme/${theme}", file: "style.css")
     def output = "<link rel='stylesheet' href='${path}' type='text/css'/>"
-                 
+
     out << output
   }
 
-  def loadMapToolBar = { attrs ->
-if (grailsApplication.config.wms.supportIE6)
+  def loadMapToolBar = {attrs ->
+    if ( grailsApplication.config.wms.supportIE6 )
     {
-    
-    def theme = attrs['theme'] ?: 'default'
-    def path = createLinkTo( dir:"${pluginContextPath}/css", file:"mapwidget3.css" )
-    def output = "<link rel='stylesheet' href='${path}' type='text/css'/>"
-    out << output
+
+      def theme = attrs['theme'] ?: 'default'
+      def path = resource(plugin: "openlayers", dir: "css", file: "mapwidget3.css")
+      def output = "<link rel='stylesheet' href='${path}' type='text/css'/>"
+      out << output
     }
     else
     {
       def theme = attrs['theme'] ?: 'default'
-    def path = createLinkTo( dir:"${pluginContextPath}/css", file:"mapwidget2.css" )
-    def output = "<link rel='stylesheet' href='${path}' type='text/css'/>"
+      def path = resource(plugin: "openlayers", dir: "css", file: "mapwidget2.css")
+      def output = "<link rel='stylesheet' href='${path}' type='text/css'/>"
       out << output
     }
   }
