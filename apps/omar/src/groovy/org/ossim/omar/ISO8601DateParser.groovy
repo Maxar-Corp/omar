9e5f617ce214df8b139d50fc7a642a3f03a2fd77
gpotts <gpotts@b630af17-092b-0410-afba-a002956477a4>
2010-05-11 18:11:20 +0000

    Added joda time parsing
    git-svn-id: http://svn.osgeo.org/ossim/omar@17342 b630af17-092b-0410-afba-a002956477a4

--- apps/omar/src/groovy/org/ossim/omar/ISO8601DateParser.groovy
+++ apps/omar/src/groovy/org/ossim/omar/ISO8601DateParser.groovy
@@ -1,6 +1,12 @@
 package org.ossim.omar
 
 import java.text.SimpleDateFormat
+import org.joda.time.DateTime
+import org.joda.time.Period
+import org.joda.time.format.ISODateTimeFormat
+import org.joda.time.format.ISOPeriodFormat
+import org.joda.time.format.PeriodFormatter
+import org.joda.time.format.DateTimeFormatter
 
 class ISO8601DateParser
 {
@@ -157,5 +163,219 @@ class ISO8601DateParser
     }
 
     return [startDate, endDate]
+  }
+
+
+
+   /**
+    * This uses the joda time library to parse a period formatted string.
+    * We will limit it to the standard ISO format - PyYmMwWdDThHmMsS
+    *
+    * @param iso8601Period
+    * @return null if unable to produce a period and a valid period object otherwise
+    *
+    */
+  static def parsePeriod(String iso8601Period)
+  {
+    org.joda.time.Period result = null;
+    PeriodFormatter periodFormatter = ISOPeriodFormat.standard();
+    // Try the stadnard period format of the form
+    // The standard ISO format - PyYmMwWdDThHmMsS
+    try
+    {
+       result = periodFormatter.parsePeriod(iso8601Period); 
+    }
+    catch(Exception e)
+    {
+      result = null;
+    }
+
+    return result;
+  }
+ 
+  /**
+   * This uses the joda time library to parse a IOS8601 date time string.  We first look for the
+   * hard coded forms such as:
+   * yyyyMMdd'T'HHmmss.SSSZ
+   * yyyyMMdd'T'HHmmssZ
+   * yyyy-MM-dd'T'HH:mm:ssZZ
+   * yyyy-MM-dd'T'HH:mm:ss.SSSZZ
+   * yyyyMMdd
+   *
+   * If they fail we allocate a generalized joida time parser that looks
+   * for the following described patterns:
+   *
+   * datetime          = time | date-opt-time
+   * time              = 'T' time-element [offset]
+   * date-opt-time     = date-element ['T' [time-element] [offset]]
+   * date-element      = std-date-element | ord-date-element | week-date-element
+   * std-date-element  = yyyy ['-' MM ['-' dd]]
+   * ord-date-element  = yyyy ['-' DDD]
+   * week-date-element = xxxx '-W' ww ['-' e]
+   * time-element      = HH [minute-element] | [fraction]
+   * minute-element    = ':' mm [second-element] | [fraction]
+   * second-element    = ':' ss [fraction]
+   * fraction          = ('.' | ',') digit+
+   * offset            = 'Z' | (('+' | '-') HH [':' mm [':' ss [('.' | ',') SSS]]])
+   *
+   * @param iso8601DateTime
+   * @return null if unable to produce a DateTime else a valid DateTime object
+   *
+   */
+  static def parseDateTime(String iso8601DateTime)
+  {
+    org.joda.time.DateTime result = null;
+
+    // Try the basic date time of the form
+    // yyyyMMdd'T'HHmmss.SSSZ
+    DateTimeFormatter formatter = org.joda.time.format.ISODateTimeFormat.basicDateTime();
+    try
+    {
+       result = formatter.parseDateTime(iso8601DateTime);
+    }
+    catch (Exception e)
+    {
+       result = null;
+    }
+
+    if(!result)
+    {
+      // now lets try the form
+      // yyyyMMdd'T'HHmmssZ
+      formatter = org.joda.time.format.ISODateTimeFormat.basicDateTimeNoMillis();
+      try
+      {
+         result = formatter.parseDateTime(iso8601DateTime);
+      }
+      catch (Exception e)
+      {
+         result = null;
+      }
+    }
+
+    if(!result)
+    {
+      // now lets try the form
+      // yyyy-MM-dd'T'HH:mm:ssZZ
+      formatter = org.joda.time.format.ISODateTimeFormat.dateTimeNoMillis();
+      try
+      {
+         result = formatter.parseDateTime(iso8601DateTime);
+      }
+      catch (Exception e)
+      {
+         result = null;
+      }
+    }
+    if(!result)
+    {
+      // now lets try the form
+      // yyyy-MM-dd'T'HH:mm:ss.SSSZZ
+      formatter = org.joda.time.format.ISODateTimeFormat.dateTime();
+      try
+      {
+         result = formatter.parseDateTime(iso8601DateTime);
+      }
+      catch (Exception e)
+      {
+         result = null;
+      }
+    }
+
+    if(!result)
+    {
+      // Try to parse for format yyyyMMdd
+      formatter = org.joda.time.format.ISODateTimeFormat.basicDate();
+      try
+      {
+        result = formatter.parseDateTime(iso8601DateTime);
+      }
+      catch (Exception e)
+      {
+        result = null;
+      }
+    }
+
+    // last resort we will let joda do a generic
+    // date or time parsing and return the proper object
+    //
+    if(!result)
+    {
+      formatter = org.joda.time.format.ISODateTimeFormat.dateTimeParser();
+      try
+      {
+        result = formatter.parseDateTime(iso8601DateTime);
+      }
+      catch (Exception e)
+      {
+        result = null;
+      }
+    }
+    return result;
+  }
+
+
+   /**
+    * Sets up a WMS interval parser and uses the parsePeriod and parseDateTime
+    * parsers to constrcut the intervals.  We currently do not support periodicity
+    * but we do support intervals and interval lists.  For example:
+    * P1Y/1999,2000/P10Y
+    *
+    * would produce 2 intervals the first
+    * would be from jan 1 1998 through jan 1 1999
+    * and the second
+    * would be from january 1 2000 -  january 1 2010
+    *
+    * @param wmsIntervals
+    * @return a list of intervals.  If the intervals were invalid or none
+    *         specified an empty list would be returned. 
+    */
+  static def parseWMSIntervals(String wmsIntervals)
+  {
+    def intervals = wmsIntervals?.split(',');
+    def intervalResult = [];
+    intervals?.each{intervalValue->
+      def d1 = null;
+      def d2 = null;
+      def range =intervalValue.split("/");
+      if(range)
+      {
+        String dateString = range[0].trim();
+        d1 = parsePeriod(dateString);
+        if(!d1)
+        {
+          d1 = parseDateTime(dateString);
+        }
+        if(range.size() > 1)
+        {
+          dateString =  range[1].trim();
+          d2 = parsePeriod(dateString);
+          if(!d2)
+          {
+            d2 = parseDateTime(dateString);
+          }
+
+        }
+        else
+        {
+          d2 = d1;
+        }
+
+        if(d1 && d2)
+        {
+          try
+          {
+            def interval = new org.joda.time.Interval(d1, d2);
+            intervalResult.add(interval);
+          }
+          catch(Exception e)
+          {
+          }
+        }
+        d1 = null;
+        d2 = null;
+      }
+    }
+    return intervalResult;
   }
 }
