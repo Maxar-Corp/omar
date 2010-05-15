b3f28e6b8b2a87e0848d23ad83a750100ea71a46
sbortman <sbortman@b630af17-092b-0410-afba-a002956477a4>
2010-05-15 00:33:52 +0000

    Added JTS support and the ability to map JTS types to the PostGIS Geometry columns using Hibernate Spatial.
    git-svn-id: http://svn.osgeo.org/ossim/omar@17360 b630af17-092b-0410-afba-a002956477a4

--- plugins/postgis/PostgisGrailsPlugin.groovy
+++ plugins/postgis/PostgisGrailsPlugin.groovy
@@ -1,41 +1,45 @@
-class PostgisGrailsPlugin {
-    def version = 0.10
-    def dependsOn = [:]
 
-    def author = "Scott Bortman"
-    def authorEmail = "scott.bortman@gmail.com"
-    def title = "PostGIS"
-    def description = '''\
+class PostgisGrailsPlugin
+{
+  def version = 0.11
+  def dependsOn = [:]
+
+  def author = "Scott Bortman"
+  def authorEmail = "scott.bortman@gmail.com"
+  def title = "PostGIS"
+  def description = '''\
     Adding support for PostGIS datatypes in domain classes
     '''
 
-    // URL to the plugin's documentation
-    def documentation = "http://grails.org/Postgis+Plugin"
+  // URL to the plugin's documentation
+  def documentation = "http://grails.org/Postgis+Plugin"
 
-    def doWithSpring = {
-        // TODO Implement runtime spring config (optional)
-    }
-   
-    def doWithApplicationContext = { applicationContext ->
-        // TODO Implement post initialization spring config (optional)		
-    }
+  def doWithSpring = {
+    // TODO Implement runtime spring config (optional)
+    customEditorRegistrar(org.ossim.postgis.CustomEditorRegistrar)
+    pointEditor(com.vividsolutions.jts.geom.PointEditor)
+  }
 
-    def doWithWebDescriptor = { xml ->
-        // TODO Implement additions to web.xml (optional)
-    }
-	                                      
-    def doWithDynamicMethods = { ctx ->
-        // TODO Implement registering dynamic methods to classes (optional)
-    }
-	
-    def onChange = { event ->
-        // TODO Implement code that is executed when any artefact that this plugin is
-        // watching is modified and reloaded. The event contains: event.source,
-        // event.application, event.manager, event.ctx, and event.plugin.
-    }
+  def doWithApplicationContext = { applicationContext ->
+    // TODO Implement post initialization spring config (optional)
+  }
 
-    def onConfigChange = { event ->
-        // TODO Implement code that is executed when the project configuration changes.
-        // The event is the same as for 'onChange'.
-    }
+  def doWithWebDescriptor = { xml ->
+    // TODO Implement additions to web.xml (optional)
+  }
+
+  def doWithDynamicMethods = { ctx ->
+    // TODO Implement registering dynamic methods to classes (optional)
+  }
+
+  def onChange = { event ->
+    // TODO Implement code that is executed when any artefact that this plugin is
+    // watching is modified and reloaded. The event contains: event.source,
+    // event.application, event.manager, event.ctx, and event.plugin.
+  }
+
+  def onConfigChange = { event ->
+    // TODO Implement code that is executed when the project configuration changes.
+    // The event is the same as for 'onChange'.
+  }
 }
