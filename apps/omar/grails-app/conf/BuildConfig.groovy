ba1c20875a21196893087b7ea98d879e34229da0
sbortman <sbortman@b630af17-092b-0410-afba-a002956477a4>
2010-05-15 20:05:12 +0000

    Added the grails repository stuff back
    git-svn-id: http://svn.osgeo.org/ossim/omar@17368 b630af17-092b-0410-afba-a002956477a4

--- apps/omar/grails-app/conf/BuildConfig.groovy
+++ apps/omar/grails-app/conf/BuildConfig.groovy
@@ -1,34 +1,35 @@
-//grails.project.class.dir = "target/classes"
-//grails.project.test.class.dir = "target/test-classes"
-//grails.project.test.reports.dir	= "target/test-reports"
-////grails.project.war.file = "target/${appName}-${appVersion}.war"
-//grails.project.dependency.resolution = {
-//    // inherit Grails' default dependencies
-//    inherits( "global" ) {
-//        // uncomment to disable ehcache
-//        // excludes 'ehcache'
-//    }
-//    log "warn" // log level of Ivy resolver, either 'error', 'warn', 'info', 'debug' or 'verbose'
-//    repositories {
-//        grailsPlugins()
-//        grailsHome()
-//
-//        // uncomment the below to enable remote dependency resolution
-//        // from public Maven repositories
-//        //mavenLocal()
-//        //mavenCentral()
-//        //mavenRepo "http://snapshots.repository.codehaus.org"
-//        //mavenRepo "http://repository.codehaus.org"
-//        //mavenRepo "http://download.java.net/maven/2/"
-//        //mavenRepo "http://repository.jboss.com/maven2/"
-//    }
-//    dependencies {
-//        // specify dependencies here under either 'build', 'compile', 'runtime', 'test' or 'provided' scopes eg.
-//
-//        // runtime 'mysql:mysql-connector-java:5.1.5'
-//    }
-//
-//}
+grails.project.class.dir = "target/classes"
+grails.project.test.class.dir = "target/test-classes"
+grails.project.test.reports.dir	= "target/test-reports"
+//grails.project.war.file = "target/${appName}-${appVersion}.war"
+grails.project.dependency.resolution = {
+    // inherit Grails' default dependencies
+    inherits( "global" ) {
+        // uncomment to disable ehcache
+        // excludes 'ehcache'
+    }
+    log "warn" // log level of Ivy resolver, either 'error', 'warn', 'info', 'debug' or 'verbose'
+    repositories {        
+        grailsPlugins()
+        grailsHome()
+        grailsCentral()
+
+        // uncomment the below to enable remote dependency resolution
+        // from public Maven repositories
+        //mavenLocal()
+        //mavenCentral()
+        //mavenRepo "http://snapshots.repository.codehaus.org"
+        //mavenRepo "http://repository.codehaus.org"
+        //mavenRepo "http://download.java.net/maven/2/"
+        //mavenRepo "http://repository.jboss.com/maven2/"
+    }
+    dependencies {
+        // specify dependencies here under either 'build', 'compile', 'runtime', 'test' or 'provided' scopes eg.
+
+        // runtime 'mysql:mysql-connector-java:5.1.5'
+    }
+
+}
 
 grails.plugin.location.postgis='../../plugins/postgis'
 grails.plugin.location.openlayers='../../plugins/openlayers'
@@ -38,4 +39,3 @@ grails.plugin.location.'omar-video'='../../plugins/omar-video'
 grails.plugin.location.'omar-raster`'='../../plugins/omar-raster'
 grails.plugin.location.'omar-core'='../../plugins/omar-core'
 grails.plugin.location.'omar-stager'='../../plugins/omar-stager'
-
