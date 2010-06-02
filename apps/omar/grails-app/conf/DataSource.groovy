e94db0d5b665de0a2f7b96d6cbe9c8b64e908d27
gpotts <gpotts@b630af17-092b-0410-afba-a002956477a4>
2010-06-02 13:26:47 +0000

    Put the names back temporarily
    git-svn-id: http://svn.osgeo.org/ossim/omar@17510 b630af17-092b-0410-afba-a002956477a4

--- apps/omar/grails-app/conf/DataSource.groovy
+++ apps/omar/grails-app/conf/DataSource.groovy
@@ -22,20 +22,20 @@ environments {
     dataSource {
       dbCreate = "create-drop" // one of 'create', 'create-drop','update'
       //dbCreate = "update"
-      url = "jdbc:postgresql_postGIS:omardb-dev"
+      url = "jdbc:postgresql_postGIS:omardb-2.0-dev"
     }
   }
   test {
     dataSource {
       dbCreate = "update"
       //url = "jdbc:postgresql_postGIS:omardb-2.0-test"
-      url = "jdbc:postgresql_postGIS:omardb-test"
+      url = "jdbc:postgresql_postGIS:omardb-2.0-test"
     }
   }
   production {
     dataSource {
       dbCreate = "update"
-      url = "jdbc:postgresql_postGIS:omardb-prod"
+      url = "jdbc:postgresql_postGIS:omardb-2.0-prod"
     }
   }
 }
