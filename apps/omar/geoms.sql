5fee22ef30d662feeb9655c7414781ea62479932
gpotts <gpotts@b630af17-092b-0410-afba-a002956477a4>
2010-06-02 02:08:14 +0000

    ground geom is now part of RasterEntry
    git-svn-id: http://svn.osgeo.org/ossim/omar@17500 b630af17-092b-0410-afba-a002956477a4

--- apps/omar/geoms.sql
+++ apps/omar/geoms.sql
@@ -1,6 +1,6 @@
-ALTER TABLE raster_entry_metadata DROP COLUMN ground_geom;
-SELECT AddGeometryColumn( 'raster_entry_metadata', 'ground_geom', 4326, 'POLYGON', 2 );
-CREATE INDEX raster_entry_metadata_ground_geom_idx ON raster_entry_metadata USING GIST ( ground_geom GIST_GEOMETRY_OPS );
+ALTER TABLE raster_entry DROP COLUMN ground_geom;
+SELECT AddGeometryColumn( 'raster_entry', 'ground_geom', 4326, 'POLYGON', 2 );
+CREATE INDEX raster_entry_ground_geom_idx ON raster_entry USING GIST ( ground_geom GIST_GEOMETRY_OPS );
 
 ALTER TABLE video_data_set_metadata DROP COLUMN ground_geom;
 SELECT AddGeometryColumn( 'video_data_set_metadata', 'ground_geom', 4326, 'GEOMETRY', 2 );
