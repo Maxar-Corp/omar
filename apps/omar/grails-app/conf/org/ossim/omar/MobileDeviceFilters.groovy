78337a5b401c42f9a0912e53a71610e5fe81c8f5
dlucas <dlucas@b630af17-092b-0410-afba-a002956477a4>
2010-06-11 23:54:56 +0000

    Filter for mobile device support.
    git-svn-id: http://svn.osgeo.org/ossim/omar@17571 b630af17-092b-0410-afba-a002956477a4

---
+++ apps/omar/grails-app/conf/org/ossim/omar/MobileDeviceFilters.groovy
@@ -0,0 +1,28 @@
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: dlucas
+ * Date: Jun 11, 2010
+ * Time: 3:12:22 PM
+ * To change this template use File | Settings | File Templates.
+ */
+
+package org.ossim.omar
+
+class MobileDeviceFilters
+{
+  def filters = {
+       detectMobile (controller:'*', action:'*') {
+         before = {
+           if (request.getHeader('user-agent') =~ /(?i)ipad/) {
+             request['isMobile'] = true
+           } else {
+             request['isMobile'] = false
+           }
+           return true // keep processing other filters and the action
+         }
+         after = { }
+         afterView = { }
+       }
+  }
+}
