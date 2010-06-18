01d16c7729886215eb40653bf3cd260d03e86f0b
dlucas <dlucas@b630af17-092b-0410-afba-a002956477a4>
2010-06-18 12:54:41 +0000

    git-svn-id: http://svn.osgeo.org/ossim/omar@17595 b630af17-092b-0410-afba-a002956477a4

--- plugins/omar-core/web-app/js/mapwidget.js
+++ plugins/omar-core/web-app/js/mapwidget.js
@@ -6,7 +6,9 @@ function MapWidget()
     {
         map = new OpenLayers.Map("map", {controls: []});
 
-        map.addControl(new OpenLayers.Control.LayerSwitcher());
+        //map.addControl(new OpenLayers.Control.LayerSwitcher());
+          map.addControl(new OpenLayers.Control.LayerSwitcher({'div':OpenLayers.Util.getElement('layerswitcher')}));
+
         map.addControl(new OpenLayers.Control.Scale());
         map.addControl(new OpenLayers.Control.ScaleLine());
 
