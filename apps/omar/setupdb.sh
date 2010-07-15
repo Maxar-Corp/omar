6a052f27a5978fe205c266425bd0daba3585528d
sbortman <sbortman@b630af17-092b-0410-afba-a002956477a4>
2010-07-15 13:11:02 +0000

    Temporary fix (hack) until I fix a bug in run-sql-file script in postgis plugin
    git-svn-id: http://svn.osgeo.org/ossim/omar@17754 b630af17-092b-0410-afba-a002956477a4

--- apps/omar/setupdb.sh
+++ apps/omar/setupdb.sh
@@ -15,6 +15,10 @@ esac
 grails ${mode} drop-postgis-database
 grails ${mode} create-postgis-database
 grails ${mode} schema-export ./ddl.sql
-grails ${mode} run-sql-file ddl.sql
-grails ${mode} run-sql-file geoms.sql
 
+
+#grails ${mode} run-sql-file ddl.sql
+#grails ${mode} run-sql-file geoms.sql
+
+psql84 -U postgres -d omardb-2.0-${mode} -f ddl.sql
+psql84 -U postgres -d omardb-2.0-${mode} -f geoms.sql
