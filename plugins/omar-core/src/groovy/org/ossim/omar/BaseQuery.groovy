01370636dd16d430a15b08fae93f44a48a05321a
gpotts <gpotts@b630af17-092b-0410-afba-a002956477a4>
2010-08-04 14:28:47 +0000

    Added a base query class
    git-svn-id: http://svn.osgeo.org/ossim/omar@17832 b630af17-092b-0410-afba-a002956477a4

---
+++ plugins/omar-core/src/groovy/org/ossim/omar/BaseQuery.groovy
@@ -0,0 +1,145 @@
+package org.ossim.omar
+
+import com.vividsolutions.jts.geom.Coordinate
+import com.vividsolutions.jts.geom.Geometry
+import com.vividsolutions.jts.geom.GeometryFactory
+import com.vividsolutions.jts.geom.PrecisionModel
+import com.vividsolutions.jts.io.WKTReader
+import org.hibernate.criterion.Criterion
+import org.hibernatespatial.criterion.SpatialFilter
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: gpotts
+ * Date: Aug 3, 2010
+ * Time: 12:31:03 PM
+ * To change this template use File | Settings | File Templates.
+ */
+class BaseQuery
+{
+  def grailsApplication
+
+  public static final String RADIUS_SEARCH = "RADIUS"
+  public static final String BBOX_SEARCH = "BBOX"
+  String searchMethod = BBOX_SEARCH
+
+  String aoiMaxLat
+  String aoiMinLon
+  String aoiMinLat
+  String aoiMaxLon
+  String centerLon
+  String centerLat
+
+  String viewMaxLat
+  String viewMinLon
+  String viewMinLat
+  String viewMaxLon
+
+  String aoiRadius
+
+  Date startDate
+  Date endDate
+ 
+  List<String> searchTagNames = ["", "", "", "", "", "", "", ""]
+  List<String> searchTagValues = ["", "", "", "", "", "", "", ""]
+
+  Criterion createIntersection(String geomColumnName = "groundGeom")
+  {
+    def intersects = null
+
+    Geometry groundGeom = getGroundGeom()
+
+    if ( groundGeom )
+    {
+      //intersects = new IntersectsExpression(geomColumnName, groundGeom)
+      intersects = new SpatialFilter(geomColumnName, groundGeom)
+    }
+
+    return intersects
+  }
+  def getGroundGeom()
+  {
+    def srs = "4326"
+    def wkt = null
+    def bounds = null
+
+    switch ( searchMethod )
+    {
+    case BBOX_SEARCH:
+      def minLat, minLon, maxLat, maxLon
+      if ( aoiMaxLat && aoiMinLon && aoiMinLat && aoiMaxLon )
+      {
+        minLat = aoiMinLat
+        minLon = aoiMinLon
+        maxLat = aoiMaxLat
+        maxLon = aoiMaxLon
+      }
+      else
+      {
+        minLat = viewMinLat
+        minLon = viewMinLon
+        maxLat = viewMaxLat
+        maxLon = viewMaxLon
+      }
+      // only do a bounds if one exists
+      //
+      if ( minLat && maxLat && minLon && maxLon )
+      {
+        def coordinateConversionService = new CoordinateConversionService()
+
+        maxLat = coordinateConversionService.convertToDecimalDegrees(maxLat)
+        maxLon = coordinateConversionService.convertToDecimalDegrees(maxLon)
+        minLat = coordinateConversionService.convertToDecimalDegrees(minLat)
+        minLon = coordinateConversionService.convertToDecimalDegrees(minLon)
+
+        //wkt = Geometry.createPolygon(
+        //    minLon,
+        //    minLat,
+        //    maxLon,
+        //    maxLat
+        //)
+
+        def geometryFactory = new GeometryFactory(new PrecisionModel(PrecisionModel.FLOATING), 4326)
+
+        minLon = Double.parseDouble(minLon)
+        minLat = Double.parseDouble(minLat)
+        maxLon = Double.parseDouble(maxLon)
+        maxLat = Double.parseDouble(maxLat)
+
+        def coords = [
+                new Coordinate(minLon, minLat), new Coordinate(minLon, maxLat), new Coordinate(maxLon, maxLat), new Coordinate(maxLon, minLat), new Coordinate(minLon, minLat)
+        ] as Coordinate[]
+
+        def polygon = geometryFactory.createPolygon(geometryFactory.createLinearRing(coords), null)
+
+
+        wkt = polygon.toText()
+      }
+
+      break
+
+    case RADIUS_SEARCH:
+      if ( centerLon && centerLat && aoiRadius )
+      {
+        def coordinateConversionService = new CoordinateConversionService()
+
+        centerLat = coordinateConversionService.convertToDecimalDegrees(centerLat)
+        centerLon = coordinateConversionService.convertToDecimalDegrees(centerLon)
+
+        wkt = coordinateConversionService.computePointRadiusWKT(centerLon, centerLat, aoiRadius)
+      }
+      break
+    }
+
+    if ( wkt )
+    {
+      //println wkt
+      //bounds = Geometry.fromString("SRID=${srs};${wkt}")
+      bounds = new WKTReader().read(wkt)
+      bounds?.setSRID(Integer.parseInt(srs))
+      //println bounds
+    }
+
+    return bounds
+  }
+}
