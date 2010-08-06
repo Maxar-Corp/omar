6ac86f4b10aeb01afda84521aa83cb3ba5b2bb4b
gpotts <gpotts@b630af17-092b-0410-afba-a002956477a4>
2010-08-06 11:44:33 +0000

    Added GRAILS_OPTS as well
    git-svn-id: http://svn.osgeo.org/ossim/omar@17852 b630af17-092b-0410-afba-a002956477a4

--- apps/omar/run.sh
+++ apps/omar/run.sh
@@ -3,7 +3,8 @@
 #export JAVA_OPTS="-Xmx1024m -Xms256m -Djava.awt.headless=true -Dsun.rmi.dgc.client.gcInterval=10000 -Dsun.rmi.dgc.server.gcInterval=10000 -XX:+PrintGCDetails -verbose:gc"
 #export JAVA_OPTS="-Xmx1024m -Xms256m -Djava.awt.headless=true -Xcheck:jni -server -verbose:jni"
 #export JAVA_OPTS="-Xmx1024m -Xms256m -Xmn512m -ParallelGCThreads=4 -XX:+UseParallelOldGC -Djava.awt.headless=true"
-#export JAVA_OPTS="-server -Xcomp -Xbatch -Xss1m -Xms256m -Xmx1024m -XX:+UseConcMarkSweepGC -XX:+CMSIncrementalMode -XX:NewSize=128m -XX:MaxPermSize=128M -XX:CMSInitiatingOccupancyFraction=80" 
+#export JAVA_OPTS="-server -Djava.awt.headless=true -XX:+HeapDumpOnOutOfMemoryError -XX:MaxGCMinorPauseMillis=500 -XX:MaxGCPauseMillis=100 -XX:MaxNewSize=1500m -Xcomp -Xbatch -Xmx3000m -Xms2048m -XX:NewSize=1500m  -XX:+UseConcMarkSweepGC -XX:+CMSIncrementalMode  -XX:MaxPermSize=512M -XX:CMSInitiatingOccupancyFraction=80 -XX:+CMSClassUnloadingEnabled -XX:+UseGCOverheadLimit" 
+export GRAILS_OPTS=${JAVA_OPTS} 
 #export JAVA_OPTS="-server -Xms256m -Xmx1024m -Djava.awt.headless=true  -XX:MaxPermSize=256m -XX:+CMSClassUnloadingEnabled -XX:+UseGCOverheadLimit"
 
 export JAVA_OPTS="-server -Xms256m -Xmx1024m -Djava.awt.headless=true  -XX:MaxPermSize=256m -XX:+CMSClassUnloadingEnabled -XX:+UseGCOverheadLimit"
