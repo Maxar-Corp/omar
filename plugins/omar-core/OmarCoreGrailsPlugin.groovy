6496fd44d23e40607d50f91636a268fc6977b633
sbortman <sbortman@b630af17-092b-0410-afba-a002956477a4>
2010-09-22 20:23:50 +0000

    Added GZip filter to try and help speeding up loading of page by compressing the javascript and css files.
    git-svn-id: http://svn.osgeo.org/ossim/omar@18117 b630af17-092b-0410-afba-a002956477a4

--- plugins/omar-core/OmarCoreGrailsPlugin.groovy
+++ plugins/omar-core/OmarCoreGrailsPlugin.groovy
@@ -1,50 +1,67 @@
-class OmarCoreGrailsPlugin {
-    // the plugin version
-    def version = "0.1"
-    // the version or versions of Grails the plugin is designed for
-    def grailsVersion = "1.2.2 > *"
-    // the other plugins this plugin depends on
-    def dependsOn = [:]
-    // resources that are excluded from plugin packaging
-    def pluginExcludes = [
-            "grails-app/views/error.gsp"
-    ]
+class OmarCoreGrailsPlugin
+{
+  // the plugin version
+  def version = "0.1"
+  // the version or versions of Grails the plugin is designed for
+  def grailsVersion = "1.2.2 > *"
+  // the other plugins this plugin depends on
+  def dependsOn = [:]
+  // resources that are excluded from plugin packaging
+  def pluginExcludes = [
+          "grails-app/views/error.gsp"
+  ]
 
-    // TODO Fill in these fields
-    def author = "Your name"
-    def authorEmail = ""
-    def title = "Plugin summary/headline"
-    def description = '''\\
-Brief description of the plugin.
+  // TODO Fill in these fields
+  def author = "Scott Bortman"
+  def authorEmail = "sbortman@radiantblue.com"
+  def title = "OMAR core"
+  def description = '''\\
+This plugin contains OMAR code that can be shared or accessed from other OMAR plugins. 
 '''
 
-    // URL to the plugin's documentation
-    def documentation = "http://grails.org/plugin/omar-core"
+  // URL to the plugin's documentation
+  def documentation = "http://grails.org/plugin/omar-core"
 
-    def doWithWebDescriptor = { xml ->
-        // TODO Implement additions to web.xml (optional), this event occurs before 
-    }
+  def doWithWebDescriptor = { xml ->
+    def filters = xml.filter
+    def lastFilter = filters[-1]
 
-    def doWithSpring = {
-        // TODO Implement runtime spring config (optional)
+    lastFilter + {
+      filter {
+        'filter-name'('gzipFilter')
+        'filter-class'(com.jspbook.GZIPFilter.name)
+      }
+      'filter-mapping' {
+        'filter-name'('gzipFilter')
+        'url-pattern'('*.js')
+      }
+      'filter-mapping' {
+        'filter-name'('gzipFilter')
+        'url-pattern'('*.css')
+      }
     }
+  }
 
-    def doWithDynamicMethods = { ctx ->
-        // TODO Implement registering dynamic methods to classes (optional)
-    }
+  def doWithSpring = {
+    // TODO Implement runtime spring config (optional)
+  }
 
-    def doWithApplicationContext = { applicationContext ->
-        // TODO Implement post initialization spring config (optional)
-    }
+  def doWithDynamicMethods = { ctx ->
+    // TODO Implement registering dynamic methods to classes (optional)
+  }
 
-    def onChange = { event ->
-        // TODO Implement code that is executed when any artefact that this plugin is
-        // watching is modified and reloaded. The event contains: event.source,
-        // event.application, event.manager, event.ctx, and event.plugin.
-    }
+  def doWithApplicationContext = { applicationContext ->
+    // TODO Implement post initialization spring config (optional)
+  }
 
-    def onConfigChange = { event ->
-        // TODO Implement code that is executed when the project configuration changes.
-        // The event is the same as for 'onChange'.
-    }
+  def onChange = { event ->
+    // TODO Implement code that is executed when any artefact that this plugin is
+    // watching is modified and reloaded. The event contains: event.source,
+    // event.application, event.manager, event.ctx, and event.plugin.
+  }
+
+  def onConfigChange = { event ->
+    // TODO Implement code that is executed when the project configuration changes.
+    // The event is the same as for 'onChange'.
+  }
 }
