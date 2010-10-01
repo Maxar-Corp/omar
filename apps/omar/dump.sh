e9d2da781b258b319f02bf06497889b8e4fd16a5
gpotts <gpotts@b630af17-092b-0410-afba-a002956477a4>
2010-10-01 19:12:30 +0000

    Added dump.sh fix
    git-svn-id: http://svn.osgeo.org/ossim/omar@18208 b630af17-092b-0410-afba-a002956477a4

--- apps/omar/dump.sh
+++ apps/omar/dump.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 export INPUTDB=$1
-OUTPUTFILE="${INPUTDB}-data.sql"
+OUTPUTFILE=$2
 TABLES="auth_user auth_user_id_seq geometry_columns raster_data_set raster_data_set_id_seq raster_entry raster_entry_file raster_entry_file_id_seq raster_entry_id_seq raster_entry_search_tag raster_entry_search_tag_id_seq raster_file raster_file_id_seq repository repository_id_seq requestmap requestmap_id_seq role role_auth_user role_id_seq stager_queue_item stager_queue_item_id_seq video_data_set video_data_set_id_seq video_data_set_search_tag video_data_set_search_tag_id_seq video_file video_file_id_seq"
 
 
