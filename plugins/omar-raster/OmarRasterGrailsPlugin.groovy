68ef54be4f46dea9aa160bb2bcba5b43376ad26b
dlucas <dlucas@b630af17-092b-0410-afba-a002956477a4>
2010-10-11 20:28:04 +0000

    git-svn-id: http://svn.osgeo.org/ossim/omar@18248 b630af17-092b-0410-afba-a002956477a4

--- plugins/omar-raster/OmarRasterGrailsPlugin.groovy
+++ plugins/omar-raster/OmarRasterGrailsPlugin.groovy
@@ -1,65 +1,87 @@
 class OmarRasterGrailsPlugin {
-    // the plugin version
-    def version = "0.3"
-    // the version or versions of Grails the plugin is designed for
-    def grailsVersion = "1.2.2 > *"
-    // the other plugins this plugin depends on
-    def dependsOn = [:]
-    // resources that are excluded from plugin packaging
-    def pluginExcludes = [
-            "grails-app/views/error.gsp"
-    ]
+  // the plugin version
+  def version = "0.3"
+  // the version or versions of Grails the plugin is designed for
+  def grailsVersion = "1.2.2 > *"
+  // the other plugins this plugin depends on
+  def dependsOn = [:]
+  // resources that are excluded from plugin packaging
+  def pluginExcludes = [
+          "grails-app/views/error.gsp"
+  ]
 
-    // TODO Fill in these fields
-    def author = "Scott Bortman"
-    def authorEmail = "sbortman@radiantblue.com"
-    def title = "OMAR Raster"
-    def description = '''\\
+  // TODO Fill in these fields
+  def author = "Scott Bortman"
+  def authorEmail = "sbortman@radiantblue.com"
+  def title = "OMAR Raster"
+  def description = '''\\
 OMAR Raster support
 '''
 
-    // URL to the plugin's documentation
-    def documentation = "http://grails.org/plugin/omar-raster"
+  // URL to the plugin's documentation
+  def documentation = "http://grails.org/plugin/omar-raster"
 
-    def doWithWebDescriptor = { xml ->
-        def filters = xml.filter
-        def lastFilter = filters[-1]
+  def doWithWebDescriptor = { xml ->
+    def filters = xml.filter
+    def lastFilter = filters[-1]
 
-        lastFilter + {
-          'filter-mapping' {
-            'filter-name'('gzipFilter')
-            'url-pattern'('/rasterEntry/search')
-          }
-        }
-        lastFilter + {
-          'filter-mapping' {
-            'filter-name'('gzipFilter')
-            'url-pattern'('/rasterEntry/results')
-          }
-        }
+    lastFilter + {
+      'filter-mapping' {
+        'filter-name'('gzipFilter')
+        'url-pattern'('/rasterEntry/search')
+      }
+    }
+    lastFilter + {
+      'filter-mapping' {
+        'filter-name'('gzipFilter')
+        'url-pattern'('/rasterEntry/results')
+      }
     }
 
-    def doWithSpring = {
-        // TODO Implement runtime spring config (optional)
-        rasterInfoParser(org.ossim.omar.RasterInfoParser)
+    lastFilter + {
+      'filter-mapping' {
+        'filter-name'('gzipFilter')
+        'url-pattern'('/rasterEntry/search_mobile')
+      }
     }
 
-    def doWithDynamicMethods = { ctx ->
-        // TODO Implement registering dynamic methods to classes (optional)
+    lastFilter + {
+      'filter-mapping' {
+        'filter-name'('gzipFilter')
+        'url-pattern'('/rasterEntry/results_mobile')
+      }
+    }
+    lastFilter + {
+      'filter-mapping' {
+        'filter-name'('gzipFilter')
+        'url-pattern'('/rasterEntry/list_mobile')
+      }
     }
 
-    def doWithApplicationContext = { applicationContext ->
-        // TODO Implement post initialization spring config (optional)
-    }
 
-    def onChange = { event ->
-        // TODO Implement code that is executed when any artefact that this plugin is
-        // watching is modified and reloaded. The event contains: event.source,
-        // event.application, event.manager, event.ctx, and event.plugin.
-    }
+  }
 
-    def onConfigChange = { event ->
-        // TODO Implement code that is executed when the project configuration changes.
-        // The event is the same as for 'onChange'.
-    }
+  def doWithSpring = {
+    // TODO Implement runtime spring config (optional)
+    rasterInfoParser(org.ossim.omar.RasterInfoParser)
+  }
+
+  def doWithDynamicMethods = { ctx ->
+    // TODO Implement registering dynamic methods to classes (optional)
+  }
+
+  def doWithApplicationContext = { applicationContext ->
+    // TODO Implement post initialization spring config (optional)
+  }
+
+  def onChange = { event ->
+    // TODO Implement code that is executed when any artefact that this plugin is
+    // watching is modified and reloaded. The event contains: event.source,
+    // event.application, event.manager, event.ctx, and event.plugin.
+  }
+
+  def onConfigChange = { event ->
+    // TODO Implement code that is executed when the project configuration changes.
+    // The event is the same as for 'onChange'.
+  }
 }
