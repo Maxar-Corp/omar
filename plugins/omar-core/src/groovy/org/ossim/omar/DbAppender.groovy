bd8b93b86f9ccf29131f05200044aa5388f0b2b8
gpotts <gpotts@b630af17-092b-0410-afba-a002956477a4>
2010-11-01 13:16:46 +0000

    Added a general solution for DbAppender for logging to a table in a database
    git-svn-id: http://svn.osgeo.org/ossim/omar@18355 b630af17-092b-0410-afba-a002956477a4

---
+++ plugins/omar-core/src/groovy/org/ossim/omar/DbAppender.groovy
@@ -0,0 +1,43 @@
+package org.ossim.omar
+
+//import org.codehaus.groovy.grails.commons.*
+//import grails.converters.JSON
+public class DbAppender extends org.apache.log4j.AppenderSkeleton
+
+{
+  protected String sqlStatement = ""
+  def sql = null
+  def modifyParametersClosure
+  def shell
+  void append(org.apache.log4j.spi.LoggingEvent event)
+  {
+    if(!sql)
+    {
+      sql = org.codehaus.groovy.grails.commons.ApplicationHolder.application.mainContext.getBean("sql")
+      shell = new GroovyShell();
+    }
+    def params = shell.evaluate (event.message)
+    def newParams = modifyParametersClosure(params)
+    try
+    {
+      sql.execute(sqlStatement, newParams)
+    }
+    catch(Exception e)
+    {
+       println e
+    }
+  }
+  public void activateOptions()
+  {
+    println "DbAppender:activateOptions: *********************"
+    this.closed=false
+  }
+  boolean requiresLayout()
+  {
+    false
+  }
+  void close()
+  {
+    this.closed = true;
+  }
+}
