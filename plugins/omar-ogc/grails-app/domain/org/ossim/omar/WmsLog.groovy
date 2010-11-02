7d7ee7acd215bec948cdd763100788416c617936
gpotts <gpotts@b630af17-092b-0410-afba-a002956477a4>
2010-11-02 13:01:51 +0000

    Added WmsLog object for logging wms requests
    git-svn-id: http://svn.osgeo.org/ossim/omar@18370 b630af17-092b-0410-afba-a002956477a4

---
+++ plugins/omar-ogc/grails-app/domain/org/ossim/omar/WmsLog.groovy
@@ -0,0 +1,54 @@
+package org.ossim.omar
+
+import com.vividsolutions.jts.geom.Geometry
+
+class WmsLog {
+
+  Long   width
+  Long   height
+  String layers
+  String styles
+  String format
+  String request
+  String bbox
+  Double internalTime
+  Double renderTime
+  Double totalTime
+  Date   startDate
+  Date   endDate
+  String userName
+  String ip
+  String url
+  Double meanGsd
+  Geometry geometry
+  static constraints =
+  {
+    width(nullable:true)
+    height(nullable:true)
+    layers(nullable:true)
+    styles(nullable:true)
+    format(nullable:true)
+    request(nullable:true)
+    bbox(nullable:true)
+    internalTime(nullable:true)
+    renderTime(nullable:true)
+    totalTime(nullable:true)
+    startDate(nullable:true)
+    endDate(nullable:true)
+    userName(nullable:true)
+    ip(nullable:true)
+    url(nullable:true)
+    meanGsd(nullable:true)
+    geometry(nullable:true)
+  }
+  static mapping = {
+    version false
+    columns
+    {
+      url type:'text'
+      layers type: 'text'
+      geometry type: org.hibernatespatial.GeometryUserType
+    }
+  }
+
+}
