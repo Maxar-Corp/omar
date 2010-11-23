f130b344b7c9777d19e03405d27fbf9003a589ef
jason4151 <jason4151@b630af17-092b-0410-afba-a002956477a4>
2010-11-23 19:07:42 +0000

    Added working LDAP configuration. Currently commented out though.
    git-svn-id: http://svn.osgeo.org/ossim/omar@18448 b630af17-092b-0410-afba-a002956477a4

--- apps/omar/grails-app/conf/SecurityConfig.groovy
+++ apps/omar/grails-app/conf/SecurityConfig.groovy
@@ -5,15 +5,22 @@ security {
 	// change to true to use OpenID authentication
 	useOpenId = false
 
-	// change to true and specify parameters to use LDAP/ActiveDirectory authentication
-	useLdap = false
-	//ldapServer = 'ldap://localhost:389' // 'ldap://ad.example.com', 'ldap://monkeymachine:389/dc=acegisecurity,dc=org'
-	//ldapManagerDn = 'cn=admin,dc=example,dc=com'
-	//ldapManagerPassword = 'secret'
-	//ldapSearchBase = 'dc=example,dc=com' // 'ou=users,dc=example,dc=com'
-	//ldapSearchFilter = '(uid={0})' //, '(mailNickname={0})'
-	//ldapGroupSearchBase = 'ou=groups,dc=example,dc=com'
-	//ldapGroupSearchFilter = 'uniquemember={0}'
+	/* 
+	// LDAP Configuration
+	
+	providerNames = ['daoAuthenticationProvider',
+	                             'ldapAuthProvider']
+	useLdap = true
+	ldapRetrieveDatabaseRoles = true
+	ldapRetrieveGroupRoles = false
+	ldapServer = 'ldap://sles11-ldap-server'
+	ldapManagerDn = 'cn=Administrator,dc=otd,dc=radiantblue,dc=com'
+	ldapManagerPassword = 'omarldap'
+	ldapSearchBase = 'ou=people,dc=otd,dc=radiantblue,dc=com'
+	ldapSearchFilter = '(uid={0})'
+	ldapGroupSearchBase = 'ou=groups,dc=otd,dc=radiantblue,dc=com'
+	ldapGroupSearchFilter = 'uniquemember={0}'
+	*/
 
 	algorithm = 'MD5' 
 	//use Base64 text ( true or false )
