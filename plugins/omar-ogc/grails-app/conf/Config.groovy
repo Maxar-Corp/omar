e2d1c62eca00f32fc68d08dfd16ad7a2a42fe78a
gpotts <gpotts@b630af17-092b-0410-afba-a002956477a4>
2010-11-29 16:56:53 +0000

    Updated for removing some of the configuration for logging
    git-svn-id: http://svn.osgeo.org/ossim/omar@18452 b630af17-092b-0410-afba-a002956477a4

--- plugins/omar-ogc/grails-app/conf/Config.groovy
+++ plugins/omar-ogc/grails-app/conf/Config.groovy
@@ -2,58 +2,12 @@
  
 log4j = {
   // WMS logging appender
-  appender new org.ossim.omar.DbAppender(  name: "wmsLoggingAppender",
-          threshold: org.apache.log4j.Level.INFO,
-          modifyParametersClosure: {map->
-            def newMap = [:]
-            map.each{k,v->
-              try{
-                switch(k)
-                {
-                  case "width":
-                  case "height":
-                    newMap."${k}" = v as Long
-                    break
-                  case "internal_time":
-                  case "render_time":
-                  case "total_time":
-                  case "mean_gsd":
-                    newMap."${k}" = v as Double
-                    break
-                  case "start_date":
-                  case "end_date":
-                    def dateTime  = org.ossim.omar.ISO8601DateParser.parseDateTime(v)
-                    newMap."${k}" =  new java.sql.Timestamp(dateTime.millis)
-                    break
-                  default:
-                    newMap."${k}" = "${v}" as String
-                }
-              }
-              catch(Exception e)
-              {
-                println "PROBLEMS with ${k}"
-                println "ERROR = ${e}"
-                newMap."${k}" = v
-              }
-            }
-            newMap
-          },
-          sqlStatement: """INSERT INTO wms_log(version, width, height, layers, style,
-                                               format, request, internal_time, render_time, total_time,
-                                               start_date, end_date, user_name, ip, url, mean_gsd,
-                                               geometry) VALUES
-                           (0, :width, :height, :layers, :style,
-                            :format, :request, :internal_time, :render_time, :total_time,
-                            :start_date, :end_date, :user_name, :ip, :url, :mean_gsd,
-                            ST_GeomFromText(:geometry, 4326))"""
-          )
     // Example of changing the log pattern for the default console
     // appender:
     //
     //appenders {
     //    console name:'stdout', layout:pattern(conversionPattern: '%c{2} %m%n')
     //}
-    info wmsLoggingAppender: 'grails.app.service.org.ossim.omar.WmsLogService', additivity: true
  
     error  'org.codehaus.groovy.grails.web.servlet',  //  controllers
            'org.codehaus.groovy.grails.web.pages', //  GSP
