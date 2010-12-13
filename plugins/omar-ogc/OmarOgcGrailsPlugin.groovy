318f0301a2eb2fe957fba2fb835630d4c21326d0
sbortman <sbortman@b630af17-092b-0410-afba-a002956477a4>
2010-12-13 15:54:02 +0000

    Added description info the plugin
    git-svn-id: http://svn.osgeo.org/ossim/omar@18551 b630af17-092b-0410-afba-a002956477a4

--- plugins/omar-ogc/OmarOgcGrailsPlugin.groovy
+++ plugins/omar-ogc/OmarOgcGrailsPlugin.groovy
@@ -1,50 +1,51 @@
-class OmarOgcGrailsPlugin {
-    // the plugin version
-    def version = "0.1"
-    // the version or versions of Grails the plugin is designed for
-    def grailsVersion = "1.3.5 > *"
-    // the other plugins this plugin depends on
-    def dependsOn = [:]
-    // resources that are excluded from plugin packaging
-    def pluginExcludes = [
-            "grails-app/views/error.gsp"
-    ]
+class OmarOgcGrailsPlugin
+{
+  // the plugin version
+  def version = "0.1"
+  // the version or versions of Grails the plugin is designed for
+  def grailsVersion = "1.3.5 > *"
+  // the other plugins this plugin depends on
+  def dependsOn = [:]
+  // resources that are excluded from plugin packaging
+  def pluginExcludes = [
+          "grails-app/views/error.gsp"
+  ]
 
-    // TODO Fill in these fields
-    def author = "Your name"
-    def authorEmail = ""
-    def title = "Plugin summary/headline"
-    def description = '''\\
-Brief description of the plugin.
+  // TODO Fill in these fields
+  def author = "Scott Bortman"
+  def authorEmail = "sbortman@radiantblue.com"
+  def title = "OMAR OGC"
+  def description = '''\\
+Adds OGC functionality to OMAR
 '''
 
-    // URL to the plugin's documentation
-    def documentation = "http://grails.org/plugin/omar-ogc"
+  // URL to the plugin's documentation
+  def documentation = "http://grails.org/plugin/omar-ogc"
 
-    def doWithWebDescriptor = { xml ->
-        // TODO Implement additions to web.xml (optional), this event occurs before 
-    }
+  def doWithWebDescriptor = { xml ->
+    // TODO Implement additions to web.xml (optional), this event occurs before
+  }
 
-    def doWithSpring = {
-        // TODO Implement runtime spring config (optional)
-    }
+  def doWithSpring = {
+    // TODO Implement runtime spring config (optional)
+  }
 
-    def doWithDynamicMethods = { ctx ->
-        // TODO Implement registering dynamic methods to classes (optional)
-    }
+  def doWithDynamicMethods = { ctx ->
+    // TODO Implement registering dynamic methods to classes (optional)
+  }
 
-    def doWithApplicationContext = { applicationContext ->
-        // TODO Implement post initialization spring config (optional)
-    }
+  def doWithApplicationContext = { applicationContext ->
+    // TODO Implement post initialization spring config (optional)
+  }
 
-    def onChange = { event ->
-        // TODO Implement code that is executed when any artefact that this plugin is
-        // watching is modified and reloaded. The event contains: event.source,
-        // event.application, event.manager, event.ctx, and event.plugin.
-    }
+  def onChange = { event ->
+    // TODO Implement code that is executed when any artefact that this plugin is
+    // watching is modified and reloaded. The event contains: event.source,
+    // event.application, event.manager, event.ctx, and event.plugin.
+  }
 
-    def onConfigChange = { event ->
-        // TODO Implement code that is executed when the project configuration changes.
-        // The event is the same as for 'onChange'.
-    }
+  def onConfigChange = { event ->
+    // TODO Implement code that is executed when the project configuration changes.
+    // The event is the same as for 'onChange'.
+  }
 }
