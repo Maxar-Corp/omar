35066f14c52cba2d03e545097be4f9fa56f8be2d
gpotts <gpotts@b630af17-092b-0410-afba-a002956477a4>
2011-01-25 13:33:06 +0000

    Added default argument support for javascript
    git-svn-id: http://svn.osgeo.org/ossim/omar@18716 b630af17-092b-0410-afba-a002956477a4

--- apps/omar/web-app/js/application.js
+++ apps/omar/web-app/js/application.js
@@ -11,3 +11,15 @@ if (Ajax && (Ajax != null)) {
 	  }
 	});
 }
+
+Function.prototype.defaults = function()
+{
+  var _f = this;
+  var _a = Array(_f.length-arguments.length).concat(
+           Array.prototype.slice.apply(arguments));
+  return function()
+  {
+    return _f.apply(_f, Array.prototype.slice.apply(arguments).concat(
+      _a.slice(arguments.length, _a.length)));
+  }
+}
