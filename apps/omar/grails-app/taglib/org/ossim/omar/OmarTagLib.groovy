e3953854158eb804c3ad8d0da06e816d6cdbd681
gpotts <gpotts@b630af17-092b-0410-afba-a002956477a4>
2011-01-26 14:07:28 +0000

    Fixing the tag lib for allowing us to modify the security settings
    git-svn-id: http://svn.osgeo.org/ossim/omar@18730 b630af17-092b-0410-afba-a002956477a4

--- apps/omar/grails-app/taglib/org/ossim/omar/OmarTagLib.groovy
+++ apps/omar/grails-app/taglib/org/ossim/omar/OmarTagLib.groovy
@@ -3,35 +3,17 @@ class OmarTagLib
 {
   static namespace = "omar"
 
-  def securityClassificationBanner = {
-    def color
-    def description
-
-
-
-    switch ( grailsApplication.config.security.level )
-    {
-      case "UNCLASS":
-        color = "green"
-        description = "Unclassified"
-        break
-      case "SECRET":
-        color = "red"
-        description = "Secret // NOFORN"
-
-        break
-      case "TOPSECRET":
-        color = "yellow"
-        description = "Top Secret"
-        break
-    }
-
-    if ( color && description )
-    {
-      out << """<div align="center" style="background: ${color}; color: black; font-size:1.5em; font-weight:bold">
-        ${description}
-      </div>"""
-    }
-  }
-
+  def securityClassificationBanner = {attrs->
+    def level = grailsApplication.config.security.level
+    def levelMap = grailsApplication.config.security."${level}"
+    def fontSize    = attrs.fontSize?:"1.5em"
+    def fontWeight  = attrs.fontWeight?:"bold"
+    def id          = attrs.id
+    def style       = attrs.style
+    if(!style) style =  "background: ${levelMap.color}; color: black; font-size:${fontSize}; font-weight:${fontWeight}"
+    if(id) id = "id=${id}"
+    out << """<div ${id} align="center" style="${style}">
+       ${levelMap.description}
+     </div>"""
+   }
 }
