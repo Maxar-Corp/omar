d14790afe458b0f5fa07c1fc9633a2da9f97dce2
gpotts <gpotts@b630af17-092b-0410-afba-a002956477a4>
2011-01-27 20:06:03 +0000

    Fixed the config for the geoscript
    git-svn-id: http://svn.osgeo.org/ossim/omar@18758 b630af17-092b-0410-afba-a002956477a4

--- plugins/omar-video/grails-app/conf/BuildConfig.groovy
+++ plugins/omar-video/grails-app/conf/BuildConfig.groovy
@@ -32,7 +32,7 @@ grails.project.dependency.resolution = {
 
 grails.plugin.location.postgis="../../plugins/postgis"
 grails.plugin.location.openlayers="../../plugins/openlayers"
+grails.plugin.location.'geoscript'="../../plugins/geoscript"
 grails.plugin.location.'omar-oms'="../../plugins/omar-oms"
 grails.plugin.location.'omar-core'="../../plugins/omar-core"
 grails.plugin.location.'omar-stager'="../../plugins/omar-stager"
-grails.plugin.location.'geoscript'="../../plugins/geoscript"
