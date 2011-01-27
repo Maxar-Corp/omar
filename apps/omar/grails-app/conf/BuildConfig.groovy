a69934d477910bd5419fcb0239c48723cabad8ff
gpotts <gpotts@b630af17-092b-0410-afba-a002956477a4>
2011-01-27 19:51:47 +0000

    Moved plugins to above
    git-svn-id: http://svn.osgeo.org/ossim/omar@18757 b630af17-092b-0410-afba-a002956477a4

--- apps/omar/grails-app/conf/BuildConfig.groovy
+++ apps/omar/grails-app/conf/BuildConfig.groovy
@@ -32,6 +32,8 @@ grails.project.dependency.resolution = {
 }
 
 grails.plugin.location.postgis = "../../plugins/postgis"
+grails.plugin.location.openlayers = "../../plugins/openlayers"
+grails.plugin.location.geoscript = "../../plugins/geoscript"
 grails.plugin.location.'omar-core' = "../../plugins/omar-core"
 grails.plugin.location.'omar-oms' = "../../plugins/omar-oms"
 grails.plugin.location.'omar-ogc' = "../../plugins/omar-ogc"
@@ -39,7 +41,5 @@ grails.plugin.location.'omar-stager' = "../../plugins/omar-stager"
 grails.plugin.location.'omar-raster' = "../../plugins/omar-raster"
 grails.plugin.location.'omar-video' = "../../plugins/omar-video"
 grails.plugin.location.'omar-security' = "../../plugins/omar-security"
-grails.plugin.location.openlayers = "../../plugins/openlayers"
-grails.plugin.location.geoscript = "../../plugins/geoscript"
 //grails.plugin.location.'omar-mobile'="../../plugins/omar-mobile"
 
