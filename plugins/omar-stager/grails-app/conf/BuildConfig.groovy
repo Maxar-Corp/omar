e77bf43579b199078999483acf28a88ad15cc5fb
gpotts <gpotts@b630af17-092b-0410-afba-a002956477a4>
2011-01-27 16:21:52 +0000

    Added dependency for build
    git-svn-id: http://svn.osgeo.org/ossim/omar@18753 b630af17-092b-0410-afba-a002956477a4

--- plugins/omar-stager/grails-app/conf/BuildConfig.groovy
+++ plugins/omar-stager/grails-app/conf/BuildConfig.groovy
@@ -30,6 +30,7 @@ grails.project.dependency.resolution = {
 
 }
 
+grails.plugin.location.postgis="../../plugins/postgis"
+grails.plugin.location.geoscript="../../plugins/geoscript"
 grails.plugin.location.'omar-core'="../../plugins/omar-core"
 grails.plugin.location.'omar-oms'="../../plugins/omar-oms"
-grails.plugin.location.postgis="../../plugins/postgis"
