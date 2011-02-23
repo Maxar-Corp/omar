76be0662a613bbe21fa9efb662c46b724fc0b743
dlucas <dlucas@b630af17-092b-0410-afba-a002956477a4>
2011-02-23 01:44:53 +0000

    Added phone the the domain class.
    git-svn-id: http://svn.osgeo.org/ossim/omar@18946 b630af17-092b-0410-afba-a002956477a4

--- plugins/omar-core/grails-app/domain/org/ossim/omar/Report.groovy
+++ plugins/omar-core/grails-app/domain/org/ossim/omar/Report.groovy
@@ -3,12 +3,14 @@ package org.ossim.omar
 class Report {
   String name
   String email
+  String phone
   Date createdDate
   String report
 
   static constraints = {
     name(blank:false)
     email(blank:false)
+    phone(blank:true, nullable:true)
     createdDate()
     report(maxSize:1000, blank:false)
   }
