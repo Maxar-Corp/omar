d37cb50e169df2863a249c0ff3cb64a754e3f587
gpotts <gpotts@b630af17-092b-0410-afba-a002956477a4>
2011-03-01 19:35:56 +0000

    Adding support to create chains on the fly for processing chips
    git-svn-id: http://svn.osgeo.org/ossim/omar@18986 b630af17-092b-0410-afba-a002956477a4

--- plugins/omar-ogc/grails-app/services/org/ossim/omar/WebCoverageService.groovy
+++ plugins/omar-ogc/grails-app/services/org/ossim/omar/WebCoverageService.groovy
@@ -11,6 +11,7 @@ class WebCoverageService implements InitializingBean{
     static transactional = true
     def grailsApplication
     def temporaryDirectory
+	def rasterChainService
     def getCoverage(def wcsRequest) {
 
         def coverageList = wcsRequest?.coverage?.split(",")
@@ -31,11 +32,7 @@ class WebCoverageService implements InitializingBean{
             wmsQuery.sort  = wmsQuery.sort?:"acquisitionDate"
             wmsQuery.order = wmsQuery.order?:"desc"
         }
-        def quickLookFlagString = wcsRequest?.quicklook ?: "false"
-        def interpolation = wcsRequest.interpolation?wcsRequest.interpolation:"nearest neighbor"
-        def sharpenMode = wcsRequest?.sharpen_mode?: ""
         def crs = wcsRequest?.response_crs?wcsRequest?.response_crs:wcsRequest?.crs
-        def nullFlip = wcsRequest?.null_flip ?: null
         def requestFormat = wcsRequest?.format?.toLowerCase()
         def sharpenWidth = wcsRequest?.sharpen_width ?: null
         def sharpenSigma = wcsRequest?.sharpen_sigma ?: null
@@ -45,6 +42,7 @@ class WebCoverageService implements InitializingBean{
         def bounds = wcsRequest.bounds
         def wmsView = new WmsView()
         def defaultOutputName = "coverage"
+		def wcsParams = wcsRequest.toMap()
         if(!wmsView.setProjection(crs))
         {
             log.error("Unsupported projection ${crs}")
@@ -62,7 +60,7 @@ class WebCoverageService implements InitializingBean{
         }
         def rasterEntries = wmsQuery.getRasterEntriesAsList();
         def objectPrefixIdx = 0
-        if(wmsQuery.layers)
+        if(rasterEntries)
         {
             rasterEntries = rasterEntries?.reverse()
         }
@@ -75,149 +73,17 @@ class WebCoverageService implements InitializingBean{
         y            -= (bounds.height*0.5);
         def w         = bounds.width
         def h         = bounds.height
+		wcsParams.viewGeom = wmsView.getImageGeometry()
         rasterEntries.each{rasterEntry->
-            def histogramFile = new File(rasterEntry.getFileFromObjects("histogram")?.name)
-            def overviewFile  = new File(rasterEntry.getFileFromObjects("overview")?.name)
-            objectPrefixIdx = 0
-            kwlString       = "type: ossimImageChain\n"
-            def chain           = new joms.oms.Chain();
-            // CONSTRUCT HANDLER
-            //
-            kwlString          += "object${objectPrefixIdx}.type:${rasterEntry.className?rasterEntry.className:'ossimImageHandler'}\n"
-            kwlString          += "object${objectPrefixIdx}.entry:${rasterEntry.entryId}\n"
-            kwlString          += "object${objectPrefixIdx}.filename:${rasterEntry.rasterDataSet.getFileFromObjects("main").name}\n"
-            kwlString          += "object${objectPrefixIdx}.width:${rasterEntry.width}\n"
-            kwlString          += "object${objectPrefixIdx}.height:${rasterEntry.height}\n"
-            if(overviewFile.exists())
-            {
-                kwlString          += "object${objectPrefixIdx}.overview_file:${overviewFile}\n"
-            }
-
-            ++objectPrefixIdx
-
-            // CONSTRUCT BAND SELECTION IF NEEDED
-            //
-            if(bands)
-            {
-                def bandArray = bands.split(",")
-                def validBands = true
-                def maxBands = rasterEntry.numberOfBands as Integer
-                // validate that the ban list is within the desired ranges
-                // the http request is 0 based.
-                if(bandArray.size() < 1)
-                {
-                    validBands = false
-                }
-                else
-                {
-                    bandArray.each{
-                        try
-                        {
-                            if(Integer.parseInt(it) >= maxBands)
-                            {
-                                validBands = false;
-                            }
-                        }
-                        catch(Exception e)
-                        {
-                           validBands = false
-                        }
-                    }
-                }
-                if(validBands)
-                {
-                    // the keywordlist in ossim takes a list of integers surrounded
-                    // by parenthesis
-                    //
-                    kwlString += "object${objectPrefixIdx}.type:ossimBandSelector\n"
-                    kwlString += "object${objectPrefixIdx}.bands:(${bands})\n"
-                    ++objectPrefixIdx
-                }
-                else
-                {
-                    log.error("Invalid band selection (${bands}) for image ${rasterEntry.id}")
-                }
-            }
-            if(nullFlip)
-            {
-                kwlString += "object${objectPrefixIdx}.type:ossimNullPixelFlip\n"
-                ++objectPrefixIdx
-            }
-            // CONSTRUCT HISTOGRAM STRETCHING IF NEEDED
-            //
-            if(stretchMode&&stretchModeRegion)
-            {
-                if(stretchModeRegion == "global"  )
-                {
-                    if(histogramFile.exists())
-                    {
-                        kwlString += "object${objectPrefixIdx}.type:ossimHistogramRemapper\n"
-                        kwlString += "object${objectPrefixIdx}.histogram_filename:${histogramFile}\n"
-                        kwlString += "object${objectPrefixIdx}.stretch_mode:${stretchMode}\n"
-                        ++objectPrefixIdx
-                    }
-                    else
-                    {
-                        log.error("Histogram file does not exist and will ignore the stretch: ${histogramFile}")
-                    }
-                }
-               //  kwlString          += "object${objectPrefixIdx}.type:ossimHistogramRemapper\n"
-            }
-            // CONSTRUCT SHARPENING IF NEEDED
-            //
-            if(sharpenMode)
-            {
-                switch(sharpenMode)
-                {
-                    case "light":
-                        sharpenSigma = 0.5
-                        sharpenWidth = 3
-                        break
-                    case "heavy":
-                        sharpenSigma = 1.0
-                        sharpenWidth = 5.0
-                        break
-                    default:
-                        break
-                }
-
-            }
-            if(sharpenSigma&&sharpenWidth)
-            {
-                kwlString += "object${objectPrefixIdx}.type:ossimImageSharpenFilter\n"
-                kwlString += "object${objectPrefixIdx}.kernel_sigma:${sharpenSigma}\n"
-                kwlString += "object${objectPrefixIdx}.kernel_width:${sharpenWidth}\n"
-                ++objectPrefixIdx
-            }
-            //CONSTRUCT IMAGE CACHE
-            //
-            kwlString          += "object${objectPrefixIdx}.type:ossimCacheTileSource\n"
-
-            ++objectPrefixIdx
-            //CONSTRUCT RENDERER
-            //
-            kwlString          += "object${objectPrefixIdx}.type:ossimImageRenderer\n"
-            kwlString          += "object${objectPrefixIdx}.max_levels_to_compute:0\n"
-            kwlString          += "object${objectPrefixIdx}.resampler.magnify_type:  ${interpolation}\n"
-            kwlString          += "object${objectPrefixIdx}.resampler.minify_type:  ${interpolation}\n"
-
-            ++objectPrefixIdx
-            //CONSTRUCT VIEW CACHE
-            //
-            kwlString          += "object${objectPrefixIdx}.type:ossimCacheTileSource\n"
-            kwlString          += "object${objectPrefixIdx}.tile_size_xy:(64,64)\n"
-            ++objectPrefixIdx
-
-
-            chain.loadChainKwlString(kwlString)
-            Util.setAllViewGeometries(chain.getChain(), wmsView.getImageGeometry().get(), false);
+			def chain = rasterChainService.createRasterEntryChain(rasterEntry, wcsParams)
+           // Util.setAllViewGeometries(chain.getChain(), wmsView.getImageGeometry().get(), false);
             //chain.print()
             if(chain.getChain()!=null)
             {
                 srcChains.add(chain)
             }
         }
-
+		wcsParams.viewGeom = null
 
         // now establish mosaic and cut to match the output dimensions
         kwlString = "type:ossimImageChain\n"
@@ -249,66 +115,37 @@ class WebCoverageService implements InitializingBean{
             kwlString += "object${objectPrefixIdx}.type:ossimScalarRemapper\n"
         }
 
+		
         def mosaic = new joms.oms.Chain();
         mosaic.loadChainKwlString(kwlString)
         srcChains.each{srcChain->
             mosaic.connectMyInputTo(srcChain)
         }
-        def ext = null
-        def contentType = null
-        kwlString = ""
-        switch(requestFormat)
+		
+		def writer = rasterChainService.createWriterChain([format:wcsParams.format, 
+			                                               temporaryDirectory:"${temporaryDirectory}",
+														   filenamePrefix:"wcs"])
+		def contentType = ""
+        if(writer.chain&&(writer.chain.getChain() != null))
         {
-            case ~/.*jpeg.*/:
-                kwlString  += "type:ossimJpegWriter\n"
-                contentType = "image/jpeg"
-                ext = ".jpg"
-                break
-            case ~/.*tiff.*/:
-                kwlString    += "type:ossimTiffWriter\n"
-                kwlString    += "image_type:tiff_tiled\n"
-                contentType  = "image/tiff"
-                ext          = ".tif"
-                break
-            case ~/.*jp2.*/:
-                kwlString   += "type:ossimKakaduJp2Writer\n"
-                contentType  = "image/jp2"
-                ext = ".jp2"
-                break
-            case ~/.*png.*/:
-                kwlString   += "type:ossimPngWriter\n"
-                contentType  = "image/png"
-                ext = ".png"
-                break
-            default:
-                log.error("Unsupported FORMAT=${requestFormat}")
-                break
-        }
-        def outputFileName = "${defaultOutputName}${ext}"
-        File tempFile = File.createTempFile("wcs", ext, temporaryDirectory?new File(temporaryDirectory):null);
-        // now establish a writer
-        //
-        kwlString += "filename:${tempFile}\n"
-        def writer = new joms.oms.Chain();
-        writer.loadChainKwlString(kwlString)
-        if(writer.getChain() != null)
-        {
-            writer.connectMyInputTo(mosaic)
-            writer.executeChain()
+            writer.chain.connectMyInputTo(mosaic)
+            writer.chain.executeChain()
+			contentType = writer.contentType
         }
         else
         {
-            log.error("Unable to create a writer with the format ${requestFormat}")
+            log.error("Unable to create a writer with the format ${wcsParams.format}")
         }
-        writer.deleteChain()
+        writer.chain.deleteChain()
         mosaic.deleteChain()
-        writer = null
+        writer.chain = null
         mosaic = null
         srcChains.each{srcChain->
             srcChain.deleteChain()
         }
         srcChains = null
-        [file:tempFile, outputName: "${outputFileName}", contentType: "${contentType}"]
+		
+        [file:writer.file, outputName: "${defaultOutputName}", contentType: "${contentType}"]
     }
     public void afterPropertiesSet()
     {
