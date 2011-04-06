134ccf2168d83f62664d63d336344a41c7c62c40
gpotts <gpotts@b630af17-092b-0410-afba-a002956477a4>
2011-04-06 12:32:39 +0000

    Initial import for super overlay service
    git-svn-id: http://svn.osgeo.org/ossim/omar@19274 b630af17-092b-0410-afba-a002956477a4

---
+++ plugins/omar-superoverlay/grails-app/services/org/ossim/omar/SuperOverlayService.groovy
@@ -0,0 +1,60 @@
+package org.ossim.omar
+
+class SuperOverlayService {
+
+    static transactional = true
+
+    def canSplit(def tileBounds, def tileSize, def metersPerDegree, def fullResMetersPerPixel)
+    {
+        def deltax = (tileBounds.maxx-tileBounds.minx)
+        def deltay = (tileBounds.maxy-tileBounds.miny)
+        def maxDelta = deltax>deltay?deltay:deltax
+        def maxTileSize = tileSize.width>tileSize.height?tileSize.width:tileSize.height
+        def metersPerPixel = (maxDelta*metersPerDegree)/maxTileSize
+        println "${metersPerPixel} ? ${fullResMetersPerPixel}"
+
+        // keep splitting if we can zoom further
+        metersPerPixel > fullResMetersPerPixel
+    }
+    def tileBound(def params, def fullResBbox)
+    {
+        def level = params.level?params.level as Integer:0
+        def row   = params.row?params.row as Integer:0
+        def col   = params.col?params.col as Integer:0
+        def minx  = fullResBbox.minx
+        def maxx  = fullResBbox.maxx
+        def miny  = fullResBbox.miny
+        def maxy  = fullResBbox.maxy
+        def deltax = (maxx-minx)/(2**level)
+        def deltay = (maxy-miny)/(2**level)
+
+        def llx = minx + deltax*col
+        def lly = miny + deltay*row
+
+        [minx:llx, miny:lly, maxx:(llx+deltax), maxy:(lly+deltay)]
+    }
+    def generateSubTiles(def params, def fullResBbox)
+    {
+        def level = params.level as Integer
+        def row   = params.row   as Integer
+        def col   = params.col   as Integer
+        def nrow = row*2
+        def ncol = col*2
+        def minx  = fullResBbox.minx
+        def maxx  = fullResBbox.maxx
+        def miny  = fullResBbox.miny
+        def maxy  = fullResBbox.maxy
+        ++level
+        def deltax = (maxx-minx)/(2**level)
+        def deltay = (maxy-miny)/(2**level)
+
+        def llx = minx + deltax*ncol
+        def lly = miny + deltay*nrow
+
+        [[minx:llx, miny:lly, maxx:(llx+deltax), maxy:(lly+deltay), level:level, col:ncol, row:nrow],
+         [minx:llx+deltax, miny:lly, maxx:(llx+2.0*deltax), maxy:(lly+deltay), level:level, col:(ncol+1), row:nrow],
+         [minx:llx+deltax, miny:(lly+deltay), maxx:(llx+2.0*deltax), maxy:(lly+2.0*deltay), level:level, col:(ncol+1), row:(nrow+1)],
+         [minx:llx, miny:lly+deltay, maxx:(llx+deltax), maxy:(lly+2.0*deltay), level:level, col:ncol, row:(nrow+1)]
+        ]
+    }
+}
