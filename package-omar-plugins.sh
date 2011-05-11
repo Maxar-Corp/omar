ded345d07a2c34ed658457f0cfbff78e00f85172
sbortman <sbortman@b630af17-092b-0410-afba-a002956477a4>
2011-05-11 18:42:50 +0000

    Helper script to package the omar plugins
    git-svn-id: http://svn.osgeo.org/ossim/omar@19569 b630af17-092b-0410-afba-a002956477a4

---
+++ package-omar-plugins.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+# Cleanup old
+rm -rf ~/.ivy2/cache
+rm -rf ~/.grails
+rm -rf ~/.m2/repository
+
+# Setup ivy cache
+cd ~
+mkdir -p ~/.ivy2/cache
+cd ~/.ivy2/cache
+tar xvfz $OMAR_HOME/plugins/ivyCache.tgz
+
+# Setup maven cache
+cd ~ 
+mkdir -p .m2/repository
+cp -R $OMAR_HOME/plugins/m2Repository/* . 
+
+# Package OMAR plugins
+cd $OMAR_DEV_HOME/plugins
+for x in geoscript postgis openlayers omar-oms omar-core omar-security-spring omar-ogc omar-stager omar-video omar-raster omar-superoverlay omar-rss; do
+	cd $x
+	grails clean
+	grails package-plugin 
+	cd ..
+done
+
+# install the mail plugin into OMAR
+cd $OMAR_HOME
+grails install-plugin plugins/grails-mail-1.0-SNAPSHOT.zip
