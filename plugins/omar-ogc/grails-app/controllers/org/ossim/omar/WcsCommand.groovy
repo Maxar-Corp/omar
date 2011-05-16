6c977ca7c4e690c2758e2fdb8a598a77663a4223
gpotts <gpotts@b630af17-092b-0410-afba-a002956477a4>
2011-05-16 19:23:01 +0000

    Adding Command validators for wcs actation
    git-svn-id: http://svn.osgeo.org/ossim/omar@19587 b630af17-092b-0410-afba-a002956477a4

---
+++ plugins/omar-ogc/grails-app/controllers/org/ossim/omar/WcsCommand.groovy
@@ -0,0 +1,291 @@
+package org.ossim.omar
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: gpotts
+ * Date: 5/16/11
+ * Time: 11:02 AM
+ * To change this template use File | Settings | File Templates.
+ */
+class WcsCommand {
+    static final def OUTPUT_FORMATS = ["jpeg","image/jpeg", "png", "image/png", "png_uint8",
+            "image/tiff", "geotiff", "geotiff_uint8", "geojp2_uint8",
+            "geojp2"]
+    String bbox
+    String width
+    String height
+    String depth
+    String resx
+    String resy
+    String resz
+    String format
+    String coverage
+    String crs
+    String response_crs
+    String service
+    String version
+    String request
+    String stretch_mode
+    String stretch_mode_region
+    String sharpen_mode
+    String sharpen_width
+    String sharpen_sigma
+    String quicklook
+    String null_flip
+    String exception
+    String bands
+    String time
+    String interpolation
+    String filter
+    String max
+	String brightness
+	String contrast
+
+    static constraints = {
+        bbox(validator:{val,obj->
+            def message = true
+            if(obj.request?.toLowerCase() == "getcoverage")
+            {
+                if(!val)
+                {
+                    message = "BBOX parameter not found.  This is a required parameter."
+                }
+                else
+                {
+                    def box = val.split(",");
+                    if(box.length != 4)
+                    {
+                        message = " BBOX must be formatted with 4 parameters separated by commas and matching the form minx,miny,maxx,maxy in \n"
+                        message += "the units of the crs code"
+                    }
+                }
+            }
+            else
+            {
+
+            }
+            message
+          }
+        )
+        width(validator:{val, obj->
+            def message = true
+            if(obj.request?.toLowerCase() == "getcoverage")
+            {
+              if((!val) && ((!obj.resx)&&(!obj.resy)))
+              {
+                  message = "WIDTH not specified.  You are required to sepcify WIDTH, HEIGHT pair or RESX, RESY pair"
+              }
+            }
+            message
+        })
+        height(validator:{val, obj->
+            def message = true
+            if(obj.request?.toLowerCase() == "getcoverage")
+            {
+              if((!val) && ((!obj.resx)&&(!obj.resy)))
+              {
+                  message = "HEIGHT not specified.  You are required to sepcify WIDTH, HEIGHT pair or RESX, RESY pair"
+              }
+            }
+            message
+        })
+//        depth(validator:{val, obj->
+//            def message = true
+//            if(obj.request?.toLowerCase() == "getcoverage")
+//            {
+//
+//            }
+//            message
+//        })
+        resx(validator:{val, obj->
+            def message = true
+            if(obj.request?.toLowerCase() == "getcoverage")
+            {
+              if((!val) && ((!obj.width)&&(!obj.height)))
+              {
+                  message = "RESX not specified.  You are required to sepcify WIDTH, HEIGHT pair or RESX, RESY pair"
+              }
+            }
+            message
+        })
+        resy(validator:{val, obj->
+            def message = true
+            if(obj.request?.toLowerCase() == "getcoverage")
+            {
+              if((!val) && ((!obj.width)&&(!obj.height)))
+              {
+                  message = "RESY not specified.  You are required to sepcify WIDTH, HEIGHT pair or RESX, RESY pair"
+              }
+            }
+            message
+        })
+//        resz(validator:{val, obj->
+//            def message = true
+//            if(obj.request?.toLowerCase() == "getcoverage")
+//            {
+//
+//            }
+//            message
+//        })
+        format(validator:{val,obj->
+            def message = true
+            if(obj.request?.toLowerCase() == "getcoverage")
+            {
+                if(!val)
+                {
+                   message = "FORMAT parameter not found.  This is a required parameter."
+                }
+                else if(!(val.toLowerCase() in  OUTPUT_FORMATS))
+                {
+                    message = "FORMAT ${val} not supported.  Values can only be ${OUTPUT_FORMATS.join(', ')}"
+                }
+            }
+
+            message
+        })
+        coverage(validator:{val, obj->
+            def message = true
+            if(obj.request?.toLowerCase() == "getcoverage")
+            {
+              if(!val)
+              {
+                  message = "COVERAGE parameter not found.  This is a required parameter."
+              }
+            }
+            message
+        })
+        crs(validator:{val, obj->
+            def message = true
+            if(obj.request?.toLowerCase() == "getcoverage")
+            {
+                if(!val)
+                {
+                    message = "CRS parameter not found.  This is a required parameter."
+                }
+                else
+                {
+                    if(val.toLowerCase().trim() != "epsg:4326")
+                    {
+                        message = "Invalid CRS parameter.  We only support value of EPSG:4326"
+                    }
+                }
+            }
+            message
+        })
+        response_crs(validator:{val, obj->
+            def message = true
+            if(obj.request?.toLowerCase() == "getcoverage")
+            {
+                if(val)
+                {
+                    message = "RESPONSE_CRS is specified but is currently not supported.  We only support the parameter CRS."
+                }
+            }
+            message
+        })
+        service(validator:{val, obj->
+            true
+        })
+        version(validator:{val, obj->
+            true
+        })
+        request(validator:{val,obj->
+            def message = true
+
+            if(!val)
+            {
+                message = "REQUEST parameter not found.  Values can be getcoverage, getcapabilities, or describecoverage"
+            }
+            else if(!(val.toLowerCase() in ["getcoverage", "getcapabilities", "describecoverage"]))
+            {
+                message = "Invalid value ${val}, values can be getcoverage, getcapabilities, or describecoverage"
+            }
+            message
+        })
+
+    }
+    def toMap()
+    {
+       return [bbox: bbox, bands:bands, width: width as Integer, height: height as Integer,
+               format: format, resx:resx as Double, resy: resy as Double,
+               coverage: coverage, crs: crs, response_crs:response_crs, service: service,
+               version: version, request: request, stretch_mode: stretch_mode, interpolation:interpolation,
+               stretch_mode_region: stretch_mode_region, sharpen_mode: sharpen_mode,
+               sharpen_width: sharpen_width as Double, sharpen_sigma: sharpen_sigma as Double,
+               time: time, null_flip: null_flip, exception: exception, filter:filter,
+               quicklook: quicklook, max:max, brightness:brightness, contrast:contrast].sort { it.key }
+    }
+    String[] getDates()
+    {
+      return (time) ? time.split(",") : []
+    }
+    def getBounds()
+    {
+        def result = null
+        if(bbox)
+        {
+           def splitBbox = bbox.split(",")
+            try{
+                def minx = splitBbox[0] as Double
+                def miny = splitBbox[1] as Double
+                def maxx = splitBbox[2] as Double
+                def maxy = splitBbox[3] as Double
+                def w = width
+                def h = height
+                if(!(w&&h)&&resx&&resy)
+                {
+                    def rx = resx as Double
+                    def ry = resy as Double
+                    w = (maxx-minx)/rx
+                    h = (maxy-miny)/ry
+                }
+                result = [minx:minx,
+                          miny:miny,
+                          maxx:maxx,
+                          maxy:maxy,
+                          width:w as Integer,
+                          height:h as Integer]
+            }
+            catch(Exception e)
+            {
+              result = null
+            }
+        }
+        result
+    }
+    def getDateRange()
+    {
+      def result = []
+      def dates = this.dates
+
+      if ( dates )
+      {
+        (0..<dates.size()).each {
+          def range = ISO8601DateParser.getDateRange(dates[it])
+
+          if ( range.size() > 0 )
+          {
+            result.add(range[0])
+            if ( range.size() > 1 )
+            {
+              result.add(range[1])
+            }
+          }
+        }
+      }
+
+      return result
+    }
+    def createErrorString()
+    {
+        def errorString = ""
+        errors?.each{err->
+//            errorString +=  (err.fieldError?.arguments[0].toUpperCase() +
+//                             ": " + err.getFieldError("${err.fieldError.arguments[0]}")?.code +
+//                             "\n")
+            errorString +=  (err.getFieldError("${err.fieldError.arguments[0]}")?.code + "\n")
+        }
+
+        errorString
+    }
+}
