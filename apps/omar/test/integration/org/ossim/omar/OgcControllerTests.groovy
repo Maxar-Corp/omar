b169bea75bc494d54f51731591e57a69460e189b
gpotts <gpotts@b630af17-092b-0410-afba-a002956477a4>
2011-05-17 19:44:06 +0000

    Added integration testing for the WcsCommand invalid tests
    git-svn-id: http://svn.osgeo.org/ossim/omar@19596 b630af17-092b-0410-afba-a002956477a4

--- apps/omar/test/integration/org/ossim/omar/OgcControllerTests.groovy
+++ apps/omar/test/integration/org/ossim/omar/OgcControllerTests.groovy
@@ -1,15 +1,21 @@
 package org.ossim.omar
 
 import grails.test.*
-
+import joms.oms.DataInfo
+import grails.orm.HibernateCriteriaBuilder
 class OgcControllerTests extends ControllerUnitTestCase {
+    OgcController ogcController
+
     protected void setUp() {
         super.setUp()
+        ogcController = new OgcController()
+        mockController(OgcController)
     }
 
     protected void tearDown() {
         super.tearDown()
     }
+
 
     void testInvalidWcsCommand() {
         def cmd = new WcsCommand()
@@ -20,18 +26,13 @@ class OgcControllerTests extends ControllerUnitTestCase {
         cmd.coverage="raster_entry"
         cmd.crs="EPSG:4326"
         cmd.request="GetCoverage"
-        def cntr = new OgcController()
-        cntr.wcs(cmd)
-        // validate initial paramter set
-        //
-        //assertEquals false, cmd.hasErrors()
 
         cmd.bbox = null
-        cntr.wcs(cmd)
-        assertEquals true, cntr.response.contentAsString.contains("BBOX parameter not found")
+        ogcController.wcs(cmd)
+        assertEquals true, ogcController.response.contentAsString.contains("BBOX parameter not found")
         cmd.bbox = ""
-        cntr.wcs(cmd)
-        assertEquals true, cntr.response.contentAsString.contains("BBOX parameter not found")
+        ogcController.wcs(cmd)
+        assertEquals true, ogcController.response.contentAsString.contains("BBOX parameter not found")
         cmd.bbox="-180,-90,180,90"
 
         // now test combinations of resx, resy and width, height
@@ -39,79 +40,79 @@ class OgcControllerTests extends ControllerUnitTestCase {
         cmd.height = null
         cmd.resx   = null
         cmd.resy   = null
-        cntr.wcs(cmd)
-        def contentString = cntr.response.contentAsString
+        ogcController.wcs(cmd)
+        def contentString = ogcController.response.contentAsString
         assertEquals true, (contentString.contains("WIDTH parameter not found") ||
                      contentString.contains("HEIGHT parameter not found") ||
                      contentString.contains("RESX parameter not found")||
                      contentString.contains("RESY parameter not found"))
 
         cmd.width = "256"
-        cntr.wcs(cmd)
-        assertEquals true, cntr.response.contentAsString.contains("HEIGHT parameter not found")
+        ogcController.wcs(cmd)
+        assertEquals true, ogcController.response.contentAsString.contains("HEIGHT parameter not found")
         cmd.width = null
         cmd.height = "256"
-        cntr.wcs(cmd)
-        assertEquals true, cntr.response.contentAsString.contains("WIDTH parameter not found")
+        ogcController.wcs(cmd)
+        assertEquals true, ogcController.response.contentAsString.contains("WIDTH parameter not found")
         cmd.width  = null
         cmd.height = null
         cmd.resx   = "0.001"
         cmd.resy   = null
-        cntr.wcs(cmd)
-        assertEquals true, cntr.response.contentAsString.contains("RESY parameter not found")
+        ogcController.wcs(cmd)
+        assertEquals true, ogcController.response.contentAsString.contains("RESY parameter not found")
         cmd.resx   = ""
         cmd.resy   = "0.001"
-        cntr.wcs(cmd)
-        assertEquals true, cntr.response.contentAsString.contains("RESX parameter not found")
+        ogcController.wcs(cmd)
+        assertEquals true, ogcController.response.contentAsString.contains("RESX parameter not found")
         cmd.width  = "256"
         cmd.height = "256"
         cmd.resx   = null
         cmd.resy   = null
 
         cmd.format="" // invalidate format
-        cntr.wcs(cmd)
-        assertEquals true, cntr.response.contentAsString.contains("FORMAT parameter not found")
+        ogcController.wcs(cmd)
+        assertEquals true, ogcController.response.contentAsString.contains("FORMAT parameter not found")
         cmd.format=null // invalidate format
-        cntr.wcs(cmd)
-        assertEquals true, cntr.response.contentAsString.contains("FORMAT parameter not found")
+        ogcController.wcs(cmd)
+        assertEquals true, ogcController.response.contentAsString.contains("FORMAT parameter not found")
         cmd.format="image/df" // invalidate format
-        cntr.wcs(cmd)
-        assertEquals true, cntr.response.contentAsString.contains("FORMAT parameter image/df not supported")
+        ogcController.wcs(cmd)
+        assertEquals true, ogcController.response.contentAsString.contains("FORMAT parameter image/df not supported")
         cmd.format="image/jpeg" // give valid value
 
         cmd.coverage = "" // invalidate coverage
-        cntr.wcs(cmd)
-        assertEquals true, cntr.response.contentAsString.contains("COVERAGE parameter not found")
+        ogcController.wcs(cmd)
+        assertEquals true, ogcController.response.contentAsString.contains("COVERAGE parameter not found")
         cmd.coverage = null // invalidate coverage
-        cntr.wcs(cmd)
-        assertEquals true, cntr.response.contentAsString.contains("COVERAGE parameter not found")
+        ogcController.wcs(cmd)
+        assertEquals true, ogcController.response.contentAsString.contains("COVERAGE parameter not found")
         cmd.coverage = "raster_entry" // give a valid value again
 
         cmd.crs=""
-        cntr.wcs(cmd)
-        assertEquals true, cntr.response.contentAsString.contains("CRS parameter not found")
+        ogcController.wcs(cmd)
+        assertEquals true, ogcController.response.contentAsString.contains("CRS parameter not found")
         cmd.crs=null
-        cntr.wcs(cmd)
-        assertEquals true, cntr.response.contentAsString.contains("CRS parameter not found")
+        ogcController.wcs(cmd)
+        assertEquals true, ogcController.response.contentAsString.contains("CRS parameter not found")
         cmd.crs="EPSG:3232"
-        cntr.wcs(cmd)
-        assertEquals true, cntr.response.contentAsString.contains("CRS parameter EPSG:3232")
+        ogcController.wcs(cmd)
+        assertEquals true, ogcController.response.contentAsString.contains("CRS parameter EPSG:3232")
         cmd.crs="EPSG:4326"
 
         cmd.response_crs="EPSG:3232"
-        cntr.wcs(cmd)
-        assertEquals true, cntr.response.contentAsString.contains("RESPONSE_CRS is specified and is not equal")
+        ogcController.wcs(cmd)
+        assertEquals true, ogcController.response.contentAsString.contains("RESPONSE_CRS is specified and is not equal")
         cmd.response_crs=""
 
         cmd.request=""
-        cntr.wcs(cmd)
-        assertEquals true, cntr.response.contentAsString.contains("REQUEST parameter not found")
+        ogcController.wcs(cmd)
+        assertEquals true, ogcController.response.contentAsString.contains("REQUEST parameter not found")
         cmd.request=null
-        cntr.wcs(cmd)
-        assertEquals true, cntr.response.contentAsString.contains("REQUEST parameter not found")
+        ogcController.wcs(cmd)
+        assertEquals true, ogcController.response.contentAsString.contains("REQUEST parameter not found")
         cmd.request="nogood"
-        cntr.wcs(cmd)
-        assertEquals true, cntr.response.contentAsString.contains("REQUEST parameter nogood is invalid")
+        ogcController.wcs(cmd)
+        assertEquals true, ogcController.response.contentAsString.contains("REQUEST parameter nogood is invalid")
         cmd.request="GetCoverage"
     }
 }
