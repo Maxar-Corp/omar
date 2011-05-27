bf13cf43e9ce180a1a8945940dbf8ad2586e5e71
sbortman <sbortman@b630af17-092b-0410-afba-a002956477a4>
2011-05-27 16:31:36 +0000

    Declaring dependencies on following plugins:
    	spring-security-core
    	spring-security-ldap
    	mail
    
    Upgrading spring-security-core from 1.1.2 -> 1.1.3
    Upgrading spring-security-ldap from 1.0.3 -> 1.0.5
    
    
    
    git-svn-id: http://svn.osgeo.org/ossim/omar@19673 b630af17-092b-0410-afba-a002956477a4

--- plugins/omar-security-spring/OmarSecuritySpringGrailsPlugin.groovy
+++ plugins/omar-security-spring/OmarSecuritySpringGrailsPlugin.groovy
@@ -5,7 +5,11 @@ class OmarSecuritySpringGrailsPlugin
   // the version or versions of Grails the plugin is designed for
   def grailsVersion = "1.3.7 > *"
   // the other plugins this plugin depends on
-  def dependsOn = [:]
+  def dependsOn = [
+          'springSecurityCore': '1.1.3 > *',
+          'springSecurityLdap': '1.0.5 > *',
+          'mail': '1.0-SNAPSHOT'
+  ]
   // resources that are excluded from plugin packaging
   def pluginExcludes = [
           "grails-app/views/error.gsp"
