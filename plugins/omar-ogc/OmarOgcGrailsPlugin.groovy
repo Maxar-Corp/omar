da45e5aa26b20ec73ebc7d08975e46efafbe4c03
sbortman <sbortman@b630af17-092b-0410-afba-a002956477a4>
2011-05-27 18:42:09 +0000

    Added dependency to csv plugin Upraded csv plugin from 0.1 to 0.3
    git-svn-id: http://svn.osgeo.org/ossim/omar@19678 b630af17-092b-0410-afba-a002956477a4

--- plugins/omar-ogc/OmarOgcGrailsPlugin.groovy
+++ plugins/omar-ogc/OmarOgcGrailsPlugin.groovy
@@ -5,7 +5,9 @@ class OmarOgcGrailsPlugin
   // the version or versions of Grails the plugin is designed for
   def grailsVersion = "1.3.5 > *"
   // the other plugins this plugin depends on
-  def dependsOn = [:]
+  def dependsOn = [
+     csv: "0.3 > *"
+   ]
   // resources that are excluded from plugin packaging
   def pluginExcludes = [
           "grails-app/views/error.gsp"
