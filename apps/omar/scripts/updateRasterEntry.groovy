e5a071c49ae07d50e39cf1cb1d41631eb315cd8a
sbortman <sbortman@b630af17-092b-0410-afba-a002956477a4>
2011-10-12 19:50:15 +0000

    Added a script to cycle through all the raster entries and apply some kind of update.
    git-svn-id: http://svn.osgeo.org/ossim/omar@20137 b630af17-092b-0410-afba-a002956477a4

---
+++ apps/omar/scripts/updateRasterEntry.groovy
@@ -0,0 +1,39 @@
+import org.ossim.omar.RasterEntry
+
+def count = RasterEntry.count()
+
+def max = 1000
+def offset = 0
+
+def start = System.currentTimeMillis()
+
+while ( offset < count )
+{
+    println offset
+    
+    def x = {
+        maxResults(max)
+        firstResult(offset)
+    }
+    
+    def results = RasterEntry.createCriteria().list(x)
+    
+    RasterEntry.withTransaction {
+        for ( def record in results ) 
+        {
+            def azimuthAngle = record.azimuthAngle
+            
+            if ( record.azimuthAngle )
+            {
+                record.azimuthAngle = (record.azimuthAngle + 90) % 360d
+                record.save()
+            }                
+        }            
+    }
+    
+    offset += max   
+}
+
+def stop = System.currentTimeMillis()
+
+println "elapsed: ${stop-start}ms"
