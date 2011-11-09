6064e7419a8645ffebfe89f817dbf8dc49f0adba
sbortman <sbortman@b630af17-092b-0410-afba-a002956477a4>
2011-11-09 00:29:59 +0000

    Upgraded postgis jar files Removed old code from postgis plugin that isn't (or shouldn't be) used anymore
    git-svn-id: http://svn.osgeo.org/ossim/omar@20233 b630af17-092b-0410-afba-a002956477a4

--- plugins/omar-ogc/grails-app/services/org/ossim/omar/WmsLogService.groovy
+++ plugins/omar-ogc/grails-app/services/org/ossim/omar/WmsLogService.groovy
@@ -1,68 +1,70 @@
 package org.ossim.omar
 
-import grails.converters.JSON
-import org.ossim.postgis.Geometry
 import joms.oms.ossimGpt
 import joms.oms.ossimDpt
 import org.joda.time.DateTime
 import org.joda.time.DateTimeZone
+import geoscript.geom.Bounds
 
-class WmsLogService {
+class WmsLogService
+{
   static transactional = true
   static metersPerDegree = null
+
   def fixLogParamsForRouting(def params)
   {
     def paramsSave = [:]//new HashMap(params)
 
-    params.each{k, v->
-      if(params."${k}" != null)
+    params.each {k, v ->
+      if ( params."${k}" != null )
       {
         paramsSave."${k}" = v
       }
     }
 
-    def startDate  = new DateTime(paramsSave.startDate?.time).toDateTime(DateTimeZone.UTC)
-    def endDate    = new DateTime(paramsSave.endDate?.time).toDateTime(DateTimeZone.UTC)
+    def startDate = new DateTime(paramsSave.startDate?.time).toDateTime(DateTimeZone.UTC)
+    def endDate = new DateTime(paramsSave.endDate?.time).toDateTime(DateTimeZone.UTC)
     paramsSave."startDate" = startDate.toString()
-    paramsSave."endDate"   = endDate.toString()
+    paramsSave."endDate" = endDate.toString()
 
     def bboxSplit = params.bbox?.split(',')
-    if(bboxSplit?.size() == 4)
+    if ( bboxSplit?.size() == 4 )
     {
       double minX = bboxSplit[0] as double;
       double minY = bboxSplit[1] as double;
       double maxX = bboxSplit[2] as double;
       double maxY = bboxSplit[3] as double;
 
-      paramsSave."geometry" = Geometry.createPolygon(minX, minY, maxX, maxY)
+      paramsSave."geometry" = new Bounds(minX, minY, maxX, maxY).polygon.wkt
 
-       // we are assuming SRS 4326.
+      // we are assuming SRS 4326.
       // need to support others later
       try
       {
-        if(!metersPerDegree)
+        if ( !metersPerDegree )
         {
           metersPerDegree = (new ossimGpt(0.0, 0.0)).metersPerDegree().y
         }
-        def metersY = (maxY-minY)*metersPerDegree;
-        def metersX = (maxX-minX)*metersPerDegree;
-        if(paramsSave.width&&paramsSave.height)
+        def metersY = (maxY - minY) * metersPerDegree;
+        def metersX = (maxX - minX) * metersPerDegree;
+        if ( paramsSave.width && paramsSave.height )
         {
-           paramsSave."meanGsd" = ((metersX/(paramsSave.width as Double))+
-                                  ((metersY/(paramsSave.height as Double))))*0.5
+          paramsSave."meanGsd" = ((metersX / (paramsSave.width as Double)) +
+                  ((metersY / (paramsSave.height as Double)))) * 0.5
         }
       }
-      catch(Exception e)
+      catch (Exception e)
       {
         log.error(e)
       }
     }
 
     paramsSave
-   }
+  }
+
   def logParams(def params)
   {
-     def paramsSave = fixLogParamsForRouting(params)
-     log.info(paramsSave as grails.converters.JSON)
+    def paramsSave = fixLogParamsForRouting(params)
+    log.info(paramsSave as grails.converters.JSON)
   }
 }
