ef4e63b87c2db44b2bf6818c2fe11852e500e304
sbortman <sbortman@b630af17-092b-0410-afba-a002956477a4>
2011-11-10 18:48:43 +0000

    Forgot these as part of last checkin
    git-svn-id: http://svn.osgeo.org/ossim/omar@20250 b630af17-092b-0410-afba-a002956477a4

--- apps/omar/grails-app/taglib/org/ossim/omar/OmarTagLib.groovy
+++ apps/omar/grails-app/taglib/org/ossim/omar/OmarTagLib.groovy
@@ -1,9 +1,6 @@
 package org.ossim.omar
 
-import de.andreasschmitt.richui.taglib.renderer.Renderer
-import de.andreasschmitt.richui.taglib.renderer.RenderException
 
-import java.text.SimpleDateFormat
 
 
 class OmarTagLib
@@ -23,52 +20,5 @@ class OmarTagLib
     out << """<div ${id} align="center" style="${style}">
        ${levelMap.description}
      </div>"""
-  }
-
-  Renderer dateChooserRenderer
-
-  static acceptedFormats = ["dd.MM.yyyy", "dd-MM-yyyy", "dd/MM/yyyy", "MM/dd/yyyy", "yyyy-MM-dd"]
-
-  def dateChooser = {attrs ->
-
-    dateChooserRenderer = new org.ossim.omar.DateChooserRenderer()
-
-    if ( attrs.name )
-    {
-
-      if ( !attrs?.format || !attrs.format in acceptedFormats )
-      {
-        attrs.format = "dd.MM.yyyy"
-      }
-      try
-      {
-        String date = ""
-        if ( attrs?.value )
-        {
-          date = new SimpleDateFormat(attrs.format).format(attrs.value)
-        }
-      }
-      catch (Exception e)
-      {
-        log.error("Error parsing date", e)
-        attrs.remove("value")
-        attrs.value = new SimpleDateFormat(attrs.format).format(new Date())
-      }
-
-      if ( !attrs?.locale )
-      {
-        attrs.locale = request?.locale?.language
-      }
-
-      //Render output
-      try
-      {
-        out << dateChooserRenderer.renderTag(attrs)
-      }
-      catch (RenderException e)
-      {
-        log.error(e)
-      }
-    }
   }
 }
