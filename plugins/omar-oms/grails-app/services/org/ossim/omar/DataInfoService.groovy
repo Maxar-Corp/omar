4c6e49db46f6084e6ea50cb4140ef830566a45ec
sbortman <sbortman@b630af17-092b-0410-afba-a002956477a4>
2011-11-18 23:05:28 +0000

    Moved the calls to DataInfo to a service.
    git-svn-id: http://svn.osgeo.org/ossim/omar@20273 b630af17-092b-0410-afba-a002956477a4

---
+++ plugins/omar-oms/grails-app/services/org/ossim/omar/DataInfoService.groovy
@@ -0,0 +1,24 @@
+package org.ossim.omar
+
+import joms.oms.DataInfo
+
+class DataInfoService
+{
+
+  static transactional = false
+
+  String getInfo(String filename)
+  {
+    def dataInfo = new DataInfo()
+    def canOpen = dataInfo.open(params.filename)
+    def xml = null
+    if ( canOpen )
+    {
+      xml = dataInfo.getInfo()?.trim()
+    }
+    dataInfo.close()
+    //def xml = StagerUtil.getInfo(filename)
+
+    return xml
+  }
+}
