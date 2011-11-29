b55d5cb9f5edf78f9305c7c57aa1b992d17ea43d
sbortman <sbortman@b630af17-092b-0410-afba-a002956477a4>
2011-11-29 01:59:11 +0000

    Added client support for WMS & WFS to geoscript.
    git-svn-id: http://svn.osgeo.org/ossim/omar@20301 b630af17-092b-0410-afba-a002956477a4

---
+++ plugins/geoscript/src/groovy/geoscript/workspace/WFS.groovy
@@ -0,0 +1,80 @@
+package geoscript.workspace
+
+import org.geotools.data.wfs.*
+
+/**
+ * A WFS Workspace.
+ * http://docs.geotools.org/latest/javadocs/org/geotools/data/wfs/WFSDataStore.html
+ * http://docs.geotools.org/latest/javadocs/org/geotools/data/wfs/WFSDataStoreFactory.html
+ * @author Jared Erickson
+ */
+class WFS extends Workspace
+{
+
+  /**
+   * Create a new WFS Workspace with a WFS get capabilities URL and options.
+   * Options are optional and include (protocol, username, password, encoding,
+   * timeout, bufferSize, tryGzip, lenient, maxFeatures)
+   * @param url The get capabilities URL
+   * @param options A Map of optional options
+   */
+  WFS(String url, Map options = [:])
+  {
+    super(new WFSDataStoreFactory().createDataStore(createParams(url, options)))
+  }
+
+  /**
+   * Get the format
+   * @return The workspace format name
+   */
+  String getFormat()
+  {
+    return "WFS"
+  }
+
+  /**
+   * The String representation
+   * @return A String representation
+   */
+  String toString()
+  {
+    return "WFS (" + ds.getCapabilitiesURL() + ")"
+  }
+
+  /**
+   * Create a Map of parameters for the WFSDataStoreFactory.
+   * @param url The get capabilities url
+   * @param options A Map of options
+   * @return A Map with WFSDataStoreFactory keys and values
+   */
+  private static Map createParams(String url, Map options)
+  {
+    Map params = [:]
+    params.put("WFSDataStoreFactory:GET_CAPABILITIES_URL", url)
+    if ( options.containsKey("protocol") )
+    {
+      params.put("WFSDataStoreFactory:PROTOCOL", options.get("protocol"))
+    }
+    if ( options.containsKey("username") )
+    {
+      params.put("WFSDataStoreFactory:USERNAME", options.get("username"))
+    }
+    if ( options.containsKey("password") )
+    {
+      params.put("WFSDataStoreFactory:PASSWORD", options.get("password"))
+    }
+    if ( options.containsKey("encoding") )
+    {
+      params.put("WFSDataStoreFactory:ENCODING", options.get("encoding"))
+    }
+    params.put("WFSDataStoreFactory:TIMEOUT", options.get("timeout", 3000))
+    params.put("WFSDataStoreFactory:BUFFER_SIZE", options.get("bufferSize", 10))
+    params.put("WFSDataStoreFactory:TRY_GZIP", options.get("tryGzip", true))
+    params.put("WFSDataStoreFactory:LENIENT", options.get("lenient", true))
+    if ( options.containsKey("maxFeatures") )
+    {
+      params.put("WFSDataStoreFactory:MAXFEATURES", options.get("maxFeatures"))
+    }
+    return params
+  }
+}
