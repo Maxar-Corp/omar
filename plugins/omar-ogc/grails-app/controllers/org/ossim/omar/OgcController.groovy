25678674562eacc3a1f7e5a7a4d7ded5adfd710e
kjnaquin <kjnaquin@b630af17-092b-0410-afba-a002956477a4>
2011-12-09 04:26:05 +0000

    I added a chip action to hadle chipping with rotation.
    git-svn-id: http://svn.osgeo.org/ossim/omar@20343 b630af17-092b-0410-afba-a002956477a4

--- plugins/omar-ogc/grails-app/controllers/org/ossim/omar/OgcController.groovy
+++ plugins/omar-ogc/grails-app/controllers/org/ossim/omar/OgcController.groovy
@@ -13,4 +13,41 @@ class OgcController
     log.warn("OgcController getTile is deprecated and image space operations should go through ../icp/getTile\ninstead of /ogc/getTile")
     redirect(controller: "icp", action: "getTile", params: params)
   }
+
+        def imageUtilService
+
+
+    def chip =
+    {
+        def url = "http://localhost"+ "${createLink(controller: "ogc", action: "wms")}" + "?"
+        url += "request=GetMap&"
+        url += "layers=" + params.layers + "&"
+        url += "bbox=" + params.bbox + "&"
+        url += "interpolation=" + params.interpolation + "&"
+        url += "brightness=" + params.brightness + "&"
+        url += "contrast=" + params.contrast + "&"
+        url += "sharpen_mode=" + params.sharpen_mode + "&"
+        url += "stretch_mode=" + params.stretch_mode + "&"
+        url += "stretch_mode_region=" + params.stretch_mode_region + "&"
+        url += "bands=" + params.bands + "&"
+        url += "transparent=true&"
+        url += "srs=epsg:4326&"
+        url += "width=" + params.width + "&"
+        url += "height=" + params.height + "&"
+        url += "format=image/png"
+
+        def inputImage = imageUtilService.readImage(url.toURL())
+        def outputImage = imageUtilService.rotateImage(inputImage, params.double('imageHeight'), params.double('imageWidth'), params.double('angle'))
+        def format = params.format ?: "jpeg"
+        def ostream = response.outputStream
+
+        def filename = File.createTempFile("foo", ".${format}")
+
+        response.setHeader("Content-disposition", "attachment; filename=${filename}");
+        response.contentType = "image/${format}"
+        imageUtilService.writeImageToStream(outputImage, format, ostream)
+
+        ostream.close()
+    }
+
 }
