92389e85f1f02e4405840d465f9b12f06c5e30a5
sbortman <sbortman@b630af17-092b-0410-afba-a002956477a4>
2012-01-30 13:28:59 +0000

    Forgot to add this file as part of the last checkin.   If figures as this was the ONE file that makes all work.   Doh!
    git-svn-id: http://svn.osgeo.org/ossim/omar@20509 b630af17-092b-0410-afba-a002956477a4

---
+++ apps/omar/grails-app/conf/ApplicationResources.groovy
@@ -0,0 +1,141 @@
+modules = {
+  singleColumn {
+    resource url: [dir: 'css', file: 'main.css']
+    resource url: [dir: 'css', file: 'omar-2.0.css']
+
+    resource url: [plugin: 'yui', dir: 'js/yui/reset-fonts-grids', file: 'reset-fonts-grids.css']
+    resource url: [plugin: 'yui', dir: 'js/yui/assets/skins/sam', file: 'skin.css']
+
+    resource url: [dir: 'js', file: 'application.js']
+    resource url: [plugin: 'yui', dir: 'js/yui/yahoo-dom-event', file: 'yahoo-dom-event.js']
+    resource url: [dir: 'js', file: 'datechooser.js']
+    resource url: [plugin: 'yui', dir: 'js/yui/calendar', file: 'calendar-min.js']
+    resource url: [plugin: 'yui', dir: 'js/yui/element', file: 'element-min.js']
+    resource url: [plugin: 'yui', dir: 'js/yui/tabview', file: 'tabview-min.js']
+  }
+
+  homeLayout {
+    resource url: [dir: 'css', file: 'main.css']
+    resource url: [dir: 'css', file: 'omar-2.0.css']
+
+    resource url: [plugin: 'yui', dir: 'js/yui/reset-fonts-grids', file: 'reset-fonts-grids.css']
+    resource url: [plugin: 'yui', dir: 'js/yui/assets/skins/sam', file: 'skin.css']
+
+    resource url: [plugin: "yui", dir: "js/yui/yahoo-dom-event", file: "yahoo-dom-event.js"]
+    resource url: [plugin: "yui", dir: "js/yui/element", file: "element-min.js"]
+
+    resource url: [dir: 'js', file: 'application.js']
+  }
+
+  yuiLayout {
+    resource url: [dir: 'css', file: 'main.css']
+    resource url: [dir: 'css', file: 'omar-2.0.css']
+    resource url: [plugin: 'yui', dir: 'js/yui/reset-fonts-grids', file: 'reset-fonts-grids.css']
+    resource url: [plugin: 'yui', dir: 'js/yui/assets/skins/sam', file: 'skin.css']
+
+    resource url: [dir: 'js', file: 'application.js']
+    resource url: [plugin: 'yui', dir: 'js/yui/yahoo-dom-event', file: 'yahoo-dom-event.js']
+    resource url: [plugin: 'yui', dir: 'js/yui/element', file: 'element-min.js']
+    resource url: [plugin: 'yui', dir: 'js/yui/layout/', file: 'layout-min.js']
+    resource url: [plugin: 'yui', dir: 'js/yui/container/', file: 'container_core-min.js']
+    resource url: [plugin: 'yui', dir: 'js/yui/menu/', file: 'menu-min.js']
+    resource url: [plugin: 'yui', dir: 'js/yui/tabview/', file: 'tabview-min.js']
+    resource url: [plugin: 'yui', dir: 'js/yui/calendar', file: 'calendar-min.js']
+    resource url: [dir: 'js', file: 'datechooser.js']
+
+
+    resource url: [plugin: 'openlayers', dir: 'css', file: 'mapwidget2.css']
+    resource url: [plugin: 'openlayers', dir: 'js/theme/default', file: 'style.css']
+
+    resource url: [plugin: 'openlayers', dir: 'js', file: 'OpenLayers.js']
+
+    resource url: [plugin: 'omar-core', dir: 'js', file: 'omar.js']
+    resource url: [plugin: 'omar-core', dir: 'js', file: 'coord.js']
+  }
+
+  resultsView {
+    resource url: [dir: 'css', file: 'main.css']
+    resource url: [dir: 'css', file: 'omar-2.0.css']
+
+    resource url: [plugin: 'yui', dir: 'js/yui/reset-fonts-grids', file: 'reset-fonts-grids.css']
+    resource url: [plugin: 'yui', dir: 'js/yui/assets/skins/sam', file: 'skin.css']
+
+    resource url: [dir: 'js', file: 'application.js']
+    resource url: [plugin: 'yui', dir: 'js/yui/yahoo-dom-event', file: 'yahoo-dom-event.js']
+    resource url: [dir: 'js', file: 'datechooser.js']
+    resource url: [plugin: 'yui', dir: 'js/yui/calendar', file: 'calendar-min.js']
+    resource url: [plugin: 'yui', dir: 'js/yui/element', file: 'element-min.js']
+    resource url: [plugin: 'yui', dir: 'js/yui/tabview/', file: 'tabview-min.js']
+    resource url: [plugin: 'yui', dir: 'js/yui/container/', file: 'container_core-min.js']
+    resource url: [plugin: 'yui', dir: 'js/yui/menu/', file: 'menu-min.js']
+    resource url: [plugin: 'yui', dir: 'js/yui/dragdrop', file: 'dragdrop-min.js']
+
+    resource url: [plugin: "omar-core", dir: "js/prototype", file: "prototype.js"]
+  }
+
+  rasterViewsStatic {
+    resource url: [dir: 'css', file: 'main.css']
+    resource url: [dir: 'css', file: 'omar-2.0.css']
+    resource url: [plugin: 'yui', dir: 'js/yui/reset-fonts-grids', file: 'reset-fonts-grids.css']
+    resource url: [plugin: 'yui', dir: 'js/yui/assets/skins/sam', file: 'skin.css']
+
+    resource url: [dir: 'js', file: 'application.js']
+    resource url: [plugin: 'yui', dir: 'js/yui/yahoo-dom-event', file: 'yahoo-dom-event.js']
+    resource url: [dir: 'js', file: 'datechooser.js']
+    resource url: [plugin: 'yui', dir: 'js/yui/calendar', file: 'calendar-min.js']
+    resource url: [plugin: 'yui', dir: 'js/yui/element', file: 'element-min.js']
+    resource url: [plugin: 'yui', dir: 'js/yui/tabview/', file: 'tabview-min.js']
+    resource url: [plugin: 'yui', dir: 'js/yui/container/', file: 'container_core-min.js']
+    resource url: [plugin: 'yui', dir: 'js/yui/menu/', file: 'menu-min.js']
+    resource url: [plugin: 'yui', dir: 'js/yui/dragdrop', file: 'dragdrop-min.js']
+    resource url: [plugin: 'yui', dir: 'js/yui/slider', file: 'slider-min.js']
+
+    resource url: [plugin: 'openlayers', dir: 'css', file: 'mapwidget2.css']
+    resource url: [plugin: 'openlayers', dir: 'js/theme/default', file: 'style.css']
+
+    resource url: [plugin: 'openlayers', dir: 'js', file: 'OpenLayers.js']
+
+    resource url: [plugin: 'omar-core', dir: 'js', file: 'coordinateConversion.js']
+    resource url: [plugin: 'omar-core', dir: 'js', file: 'mapwidget.js']
+  }
+
+  multiLayerLayout {
+    resource url: [dir: 'css', file: 'main.css']
+    resource url: [dir: 'css', file: 'omar-2.0.css']
+
+    resource url: [plugin: 'yui', dir: 'js/yui/reset-fonts-grids', file: 'reset-fonts-grids.css']
+    resource url: [plugin: 'yui', dir: 'js/yui/assets/skins/sam', file: 'skin.css']
+
+    resource url: [dir: 'js', file: 'application.js']
+    resource url: [plugin: 'yui', dir: 'js/yui/yahoo-dom-event', file: 'yahoo-dom-event.js']
+    resource url: [dir: 'js', file: 'datechooser.js']
+    resource url: [plugin: 'yui', dir: 'js/yui/calendar', file: 'calendar-min.js']
+    resource url: [plugin: 'yui', dir: 'js/yui/element', file: 'element-min.js']
+    resource url: [plugin: 'yui', dir: 'js/yui/tabview/', file: 'tabview-min.js']
+    resource url: [plugin: 'yui', dir: 'js/yui/container/', file: 'container_core-min.js']
+    resource url: [plugin: 'yui', dir: 'js/yui/menu/', file: 'menu-min.js']
+    resource url: [plugin: 'yui', dir: 'js/yui/dragdrop', file: 'dragdrop-min.js']
+
+    resource url: [plugin: 'openlayers', dir: 'css', file: 'mapwidget2.css']
+    resource url: [plugin: 'openlayers', dir: 'js/theme/default', file: 'style.css']
+
+    resource url: [plugin: 'openlayers', dir: 'js', file: 'OpenLayers.js']
+
+    resource url: [plugin: 'omar-core', dir: 'js', file: 'coordinateConversion.js']
+    resource url: [plugin: 'omar-core', dir: 'js', file: 'mapwidget.js']
+  }
+
+  generatedViews {
+    resource url: [dir: 'css', file: 'main.css']
+    resource url: [dir: 'css', file: 'omar-2.0.css']
+
+    resource url: [plugin: 'yui', dir: 'js/yui/reset-fonts-grids', file: 'reset-fonts-grids.css']
+    resource url: [plugin: 'yui', dir: 'js/yui/assets/skins/sam', file: 'skin.css']
+
+    resource url: [dir: 'js', file: 'application.js']
+    resource url: [plugin: 'yui', dir: 'js/yui/yahoo-dom-event', file: 'yahoo-dom-event.js']
+    resource url: [plugin: 'yui', dir: 'js/yui/element', file: 'element-min.js']
+    resource url: [plugin: 'yui', dir: 'js/yui/layout', file: 'layout-min.js']
+
+  }
+}
