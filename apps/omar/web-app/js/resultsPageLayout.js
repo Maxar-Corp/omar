921c1a4fc79aae1207aca4a3c4b4b847b2b3047f
sbortman <sbortman@b630af17-092b-0410-afba-a002956477a4>
2012-01-31 15:06:30 +0000

    Doh!   Forgot to add these to SVN.   Thanks Dave!
    git-svn-id: http://svn.osgeo.org/ossim/omar@20520 b630af17-092b-0410-afba-a002956477a4

---
+++ apps/omar/web-app/js/resultsPageLayout.js
@@ -0,0 +1,33 @@
+/**
+ * Created by IntelliJ IDEA.
+ * User: sbortman
+ * Date: 1/30/12
+ * Time: 10:48 AM
+ * To change this template use File | Settings | File Templates.
+ */
+
+
+(function ()
+{
+    var Dom = YAHOO.util.Dom,
+            Event = YAHOO.util.Event;
+
+    Event.onDOMReady( function ()
+    {
+        var layout = new YAHOO.widget.Layout( {
+            units:[
+                { position:'top', height:50, body:'top1' },
+                { position:'bottom', height:25, body:'bottom1' },
+                //{ position:'left', header:'Left', width:200, resize:true, body:'left1', gutter:'5px', collapse:true, scroll:true },
+                //{ position:'right', header:'Right', width:300, resize:true, body:'right1', gutter:'5px', collapse:true, scroll:true },
+                { position:'center', body:'center1' }
+            ]
+        } );
+        layout.on( 'render', function ()
+        {
+            init();
+            Dom.setStyle( document.body, 'visibility', 'visible' );
+        } );
+        layout.render();
+    } );
+})();
