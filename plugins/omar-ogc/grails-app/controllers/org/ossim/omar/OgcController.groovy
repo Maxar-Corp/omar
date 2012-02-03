eabfbd31bc7b22ccc14522a8dc85cd0c4d0507ae
sbortman <sbortman@b630af17-092b-0410-afba-a002956477a4>
2012-02-03 21:43:30 +0000

    MultiLayer is now using LayoutManager.
    git-svn-id: http://svn.osgeo.org/ossim/omar@20536 b630af17-092b-0410-afba-a002956477a4

--- plugins/omar-ogc/grails-app/controllers/org/ossim/omar/OgcController.groovy
+++ plugins/omar-ogc/grails-app/controllers/org/ossim/omar/OgcController.groovy
@@ -14,58 +14,57 @@ class OgcController
     redirect(controller: "icp", action: "getTile", params: params)
   }
 
-        def imageUtilService
+  def imageUtilService
 
 
-    def chip =
+  def chip = {
+    def url = ""
+    if ( params.type == "tile" )
     {
-	def url = ""
-	if (params.type == "tile")
-	{
-		url = "http://localhost"
-		url += "${createLink(controller: 'icp', action: 'getTileOpenLayers')}" + "?"
-		url += "res=" + params.res + "&"
-		url += "x=" + params.x + "&"
-		url += "y=" + params.y + "&"
-		url += "z=" + params.z + "&"
-		url += "tileWidth=" + params.tileWidth + "&"
-		url += "tileHeight=" + params.tileHeight + "&"
-		url += "id=" + params.id + "&"
- 
-	}
-	else
-	{
-		println("cheese")
-	}
-       // def url = "http://localhost"+ "${createLink(controller: "ogc", action: "wms")}" + "?"
-       // url += "request=GetMap&"
-       // url += "layers=" + params.layers + "&"
-       // url += "bbox=" + params.bbox + "&"
-    	 url += "interpolation=" + params.interpolation + "&"
-       	 url += "brightness=" + params.brightness + "&"
-       	 url += "contrast=" + params.contrast + "&"
-       	 url += "sharpen_mode=" + params.sharpen_mode + "&"
-       	 url += "stretch_mode=" + params.stretch_mode + "&"
-       	url += "stretch_mode_region=" + params.stretch_mode_region + "&"
-        url += "bands=" + params.bands + "&"
-       // url += "transparent=true&"
-       // url += "srs=epsg:4326&"
-       // url += "width=" + params.width + "&"
-       // url += "height=" + params.height + "&"
-       // url += "format=image/png"	
+      url = "http://localhost"
+      url += "${createLink(controller: 'icp', action: 'getTileOpenLayers')}" + "?"
+      url += "res=" + params.res + "&"
+      url += "x=" + params.x + "&"
+      url += "y=" + params.y + "&"
+      url += "z=" + params.z + "&"
+      url += "tileWidth=" + params.tileWidth + "&"
+      url += "tileHeight=" + params.tileHeight + "&"
+      url += "id=" + params.id + "&"
 
-     	def inputImage = imageUtilService.readImage(url.toURL())
-        def outputImage = imageUtilService.rotateImage(inputImage, params.double('imageHeight'), params.double('imageWidth'), params.double('angle'))
-        def format = params.format ?: "jpeg"
-        def ostream = response.outputStream
-
-        def filename = File.createTempFile("foo", ".${format}")
-
-        response.setHeader("Content-disposition", "attachment; filename=${filename}");
-        response.contentType = "image/${format}"
-        imageUtilService.writeImageToStream(outputImage, format, ostream)
-
-        ostream.close()
     }
+    else
+    {
+      println("cheese")
+    }
+    // def url = "http://localhost"+ "${createLink(controller: "ogc", action: "wms")}" + "?"
+    // url += "request=GetMap&"
+    // url += "layers=" + params.layers + "&"
+    // url += "bbox=" + params.bbox + "&"
+    url += "interpolation=" + params.interpolation + "&"
+    url += "brightness=" + params.brightness + "&"
+    url += "contrast=" + params.contrast + "&"
+    url += "sharpen_mode=" + params.sharpen_mode + "&"
+    url += "stretch_mode=" + params.stretch_mode + "&"
+    url += "stretch_mode_region=" + params.stretch_mode_region + "&"
+    url += "bands=" + params.bands + "&"
+    // url += "transparent=true&"
+    // url += "srs=epsg:4326&"
+    // url += "width=" + params.width + "&"
+    // url += "height=" + params.height + "&"
+    // url += "format=image/png"
+
+    def inputImage = imageUtilService.readImage(url.toURL())
+    def outputImage = imageUtilService.rotateImage(inputImage, params.double('imageHeight'), params.double('imageWidth'), params.double('angle'))
+    def format = params.format ?: "jpeg"
+    def ostream = response.outputStream
+
+    def filename = File.createTempFile("foo", ".${format}")
+
+    response.setHeader("Content-disposition", "attachment; filename=${filename}");
+    response.contentType = "image/${format}"
+    imageUtilService.writeImageToStream(outputImage, format, ostream)
+
+    ostream.close()
+  }
 
 }
