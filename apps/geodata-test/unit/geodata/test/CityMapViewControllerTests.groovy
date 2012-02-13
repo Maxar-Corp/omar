8353acb75c6a844c1997b146d389c95452acc461
sbortman <sbortman@b630af17-092b-0410-afba-a002956477a4>
2012-02-13 20:27:03 +0000

    Initial import of test project for adding new plugin code.
    git-svn-id: http://svn.osgeo.org/ossim/omar@20562 b630af17-092b-0410-afba-a002956477a4

---
+++ apps/geodata-test/unit/geodata/test/CityMapViewControllerTests.groovy
@@ -0,0 +1,17 @@
+package geodata.test
+
+import grails.test.*
+
+class CityMapViewControllerTests extends ControllerUnitTestCase {
+    protected void setUp() {
+        super.setUp()
+    }
+
+    protected void tearDown() {
+        super.tearDown()
+    }
+
+    void testSomething() {
+
+    }
+}
