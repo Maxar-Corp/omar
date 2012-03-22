cc6017b446b9e88ecfc293ce8762d08525f13c21
sbortman <sbortman@b630af17-092b-0410-afba-a002956477a4>
2012-03-22 00:53:59 +0000

    Made this script interactive. Still need to have maven installed though
    git-svn-id: http://svn.osgeo.org/ossim/omar@20702 b630af17-092b-0410-afba-a002956477a4

--- apps/omar/plugins/install-m2-jar.sh
+++ apps/omar/plugins/install-m2-jar.sh
@@ -1,10 +1,15 @@
 #!/bin/sh
 
+read -p "Path to source jar: "  PATH_TO_JAR
+read -p "Group ID: "  GROUP_ID
+read -p "Artefact ID: " ARTEFACT_ID
+read -p "Version: " VERSION
 
-PATH_TO_JAR=$OMAR_HOME/plugins/spring-test-3.0.5.RELEASE.jar
-GROUP_ID=org.springframework
-ARTEFACT_ID=spring-test
-VERSION=3.0.5.RELEASE
+
+#PATH_TO_JAR=$OMAR_HOME/plugins/spring-test-3.0.5.RELEASE.jar
+#GROUP_ID=org.springframework
+#ARTEFACT_ID=spring-test
+#VERSION=3.0.5.RELEASE
 PACKAGING=jar
 
 mvn install:install-file -Dfile=$PATH_TO_JAR -DgroupId=$GROUP_ID \
