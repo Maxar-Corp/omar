99430a9e95edf6c673f894278bf1357a5db8a5a4
dhicks <dhicks@b630af17-092b-0410-afba-a002956477a4>
2012-04-17 18:52:11 +0000

    added projection with error propagation (PQE)
    git-svn-id: http://svn.osgeo.org/ossim/omar@20740 b630af17-092b-0410-afba-a002956477a4

--- plugins/omar-oms/grails-app/services/org/ossim/omar/oms/ProjectionService.groovy
+++ plugins/omar-oms/grails-app/services/org/ossim/omar/oms/ProjectionService.groovy
@@ -1,23 +1,88 @@
 package org.ossim.omar.oms
 
-import joms.oms.ossimSensorModel
+import joms.oms.ImageModel
 import joms.oms.ossimDpt
 import joms.oms.ossimGpt
-import joms.oms.ossimString
+import joms.oms.Init
 
 class ProjectionService
 {
 
   static transactional = false
 
-  def imageSpaceToWorld(String filename, long line, long sample)
-  {
-    def sensorModel = ossimSensorModel.createSensorModel(new ossimString(filename))
-    def imagePoint = new ossimDpt(line, sample)
-    def groundPoint = new ossimGpt()
+    // Projection with no error propagation
+    def imageSpaceToGroundSpace(String filename, double samp, double line, Integer entryId)
+    {
+        Init.instance().initialize()
 
-    sensorModel.lineSampleToWorld(imagePoint, groundPoint)
+        def imageSpaceModel = new ImageModel()
+        def imagePoint = new ossimDpt(samp, line)
+        def groundPoint = new ossimGpt()
 
-    return [lat: groundPoint.latd(), lon: groundPoint.lond(), height: groundPoint.height()]
-  }
+        if ( imageSpaceModel.setModelFromFile(filename, entryId) )
+        {
+            // Perform projection
+            imageSpaceModel.imageToGround(imagePoint, groundPoint, entryId)
+        }
+        imageSpaceModel.destroy()
+        imageSpaceModel.delete()
+
+        return [groundPoint.latd(),
+                groundPoint.lond(),
+                groundPoint.height()
+        ]
+
+    }
+
+
+    // Projection with RPC error propagation
+    //   Error prop input...
+    //      probLev probability level (.5,.9,.95)
+    //      angInc  angular increment (deg) for image space ellipse points
+    //  Error prop output...
+    //      ellSamp array of sample coordinates
+    //      ellLine array of line coordinates
+    //  Returns...
+    //      intersected position lat, lon, hgt (deg)
+    //      pqeArray [0]   CE
+    //               [1]   LE
+    //               [2]   ellipse SMA
+    //               [3]   ellipse SMI
+    //               [4]   ellipse SMA azimuth (rad)
+    //               [5]   number of image ellipse points
+    def imageSpaceToGroundSpace(String filename, double samp, double line, Integer entryId,
+                                double probLev, double angInc, int [] ellSamp, int [] ellLine)
+    {
+        Init.instance().initialize()
+
+        def imageSpaceModel = new ImageModel()
+        def imagePoint = new ossimDpt(samp, line)
+        def groundPoint = new ossimGpt()
+        boolean errorPropAvailable = false
+        double [] pqeArray = new double[6]
+
+        if ( imageSpaceModel.setModelFromFile(filename, entryId) )
+        {
+            // Perform projection
+            imageSpaceModel.imageToGround(imagePoint, groundPoint, entryId)
+
+            // Perform error propagation
+            errorPropAvailable =
+                imageSpaceModel.imageToGroundErrorPropagation(groundPoint, probLev, angInc, pqeArray, ellSamp, ellLine)
+        }
+        imageSpaceModel.destroy()
+        imageSpaceModel.delete()
+
+        return [groundPoint.latd(),
+                groundPoint.lond(),
+                groundPoint.height(),
+                pqeArray[0],
+                pqeArray[1],
+                pqeArray[2],
+                pqeArray[3],
+                pqeArray[4],
+                pqeArray[5]
+        ]
+
+    }
 }
