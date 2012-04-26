891de943043c853fddccccc080ad4bf96ded2af0
gpotts <gpotts@b630af17-092b-0410-afba-a002956477a4>
2012-04-26 23:42:30 +0000

    Added new resources for image space dependencies
    git-svn-id: http://svn.osgeo.org/ossim/omar@20777 b630af17-092b-0410-afba-a002956477a4

--- apps/omar/grails-app/conf/ApplicationResources.groovy
+++ apps/omar/grails-app/conf/ApplicationResources.groovy
@@ -16,10 +16,10 @@ modules = {
 
   searchPageLayout {
 
-    resource url: [dir: 'css', file: 'main.css']
+   resource url: [dir: 'css', file: 'main.css']
     resource url: [dir: 'css', file: 'omar-2.0.css']
 
-    resource url: [plugin: 'yui', dir: "js/yui/reset-fonts-grids", file: "reset-fonts-grids.css"]
+       resource url: [plugin: 'yui', dir: "js/yui/reset-fonts-grids", file: "reset-fonts-grids.css"]
     resource url: [plugin: 'yui', dir: "js/yui/assets/skins/sam", file: "skin.css"]
 
     resource url: [plugin: 'yui', dir: "js/yui/yahoo-dom-event", file: "yahoo-dom-event.js"]
@@ -32,20 +32,20 @@ modules = {
     resource url: [plugin: 'yui', dir: 'js/yui/menu/', file: 'menu-min.js']
     resource url: [plugin: 'yui', dir: 'js/yui/tabview/', file: 'tabview-min.js']
     resource url: [plugin: 'yui', dir: 'js/yui/calendar', file: 'calendar-min.js']
-	
     resource url: [plugin: 'openlayers', dir: 'css', file: 'mapwidget2.css']
     resource url: [plugin: 'openlayers', dir: 'js/theme/default', file: 'style.css']
     resource url: [plugin: 'openlayers', dir: 'js', file: 'OpenLayers.js']
 
-    resource url: [plugin: "omar-core", dir: "js/prototype", file: "prototype.js"]
 
+resource url: [plugin: "omar-core", dir: "js/prototype", file: "prototype.js"]
     resource url: [plugin: 'omar-core', dir: 'js', file: 'omar.js']
     resource url: [plugin: 'omar-core', dir: 'js', file: 'coord.js']
 	
-	resource url: [plugin: "omar-core", dir: "js/prototype", file: "prototype.js"]
-	
-    resource url: [dir: 'js', file: 'application.js']
-    resource url: [dir: 'js', file: 'datechooser.js']
+	//resource url: [plugin: "omar-core", dir: "js/prototype", file: "prototype.js"]
+
+    resource url: [dir: 'js', file: 'OMAR.js']
+      resource url: [dir: 'js', file: 'application.js']
+      resource url: [dir: 'js', file: 'datechooser.js']
 
     resource url: [dir: 'css', file: 'searchPageLayout.css']
     resource url: [dir: 'js', file: 'searchPageLayout.js']
@@ -145,7 +145,12 @@ modules = {
     resource url: [dir: 'css', file: 'main.css']
     resource url: [dir: 'css', file: 'omar-2.0.css']
 
-    resource url: [plugin: 'yui', dir: "js/yui/reset-fonts-grids", file: "reset-fonts-grids.css"]
+      resource url: [dir: 'js/cssSandpaper', file: 'EventHelpers.js']
+      resource url: [dir: 'js/cssSandpaper', file: 'cssQuery-p.js']
+      resource url: [dir: 'js/cssSandpaper/jcoglan.com', file: 'sylvester.js']
+      resource url: [dir: 'js/cssSandpaper', file: 'cssSandpaper.js']
+
+   resource url: [plugin: 'yui', dir: "js/yui/reset-fonts-grids", file: "reset-fonts-grids.css"]
     resource url: [plugin: 'yui', dir: "js/yui/assets/skins/sam", file: "skin.css"]
 
     resource url: [plugin: 'yui', dir: "js/yui/yahoo-dom-event", file: "yahoo-dom-event.js"]
@@ -165,18 +170,18 @@ modules = {
     resource url: [plugin: 'openlayers', dir: 'js/theme/default', file: 'style.css']
     resource url: [plugin: 'openlayers', dir: 'js', file: 'OpenLayers.js']
 
+    resource url: [dir: 'js', file: 'OMAR.js']
 
     resource url: [plugin: 'omar-core', dir: 'js', file: 'mapwidget.js']
     resource url: [plugin: 'omar-core', dir: 'js', file: 'coord.js']
 
     resource url: [dir: 'js', file: 'application.js']
-    //resource url: [dir: 'js', file: 'datechooser.js']
+    resource url: [dir: 'js', file: 'matrix.js']
+    resource url: [dir: 'js', file: 'OpenLayersImageManipulator.js']
+   //resource url: [dir: 'js', file: 'datechooser.js']
 
     resource url: [dir: 'css', file: 'groundSpacePageLayout.css']
     resource url: [dir: 'js', file: 'groundSpacePageLayout.js']
-
-
-
   }
 
   generatedViews {
@@ -200,7 +205,11 @@ modules = {
     resource url: [dir: 'css', file: 'main.css']
     resource url: [dir: 'css', file: 'omar-2.0.css']
 
-    resource url: [plugin: 'yui', dir: "js/yui/reset-fonts-grids", file: "reset-fonts-grids.css"]
+      resource url: [dir: 'js/cssSandpaper', file: 'EventHelpers.js']
+      resource url: [dir: 'js/cssSandpaper', file: 'cssQuery-p.js']
+      resource url: [dir: 'js/cssSandpaper/jcoglan.com', file: 'sylvester.js']
+      resource url: [dir: 'js/cssSandpaper', file: 'cssSandpaper.js']
+   resource url: [plugin: 'yui', dir: "js/yui/reset-fonts-grids", file: "reset-fonts-grids.css"]
     resource url: [plugin: 'yui', dir: "js/yui/assets/skins/sam", file: "skin.css"]
 
     resource url: [plugin: 'yui', dir: "js/yui/yahoo-dom-event", file: "yahoo-dom-event.js"]
@@ -214,6 +223,7 @@ modules = {
     resource url: [plugin: 'yui', dir: 'js/yui/tabview', file: 'tabview-min.js']
     //resource url: [plugin: 'yui', dir: 'js/yui/calendar', file: 'calendar-min.js']
     resource url: [plugin: 'yui', dir: 'js/yui/slider', file: 'slider-min.js']
+    resource url: [plugin: 'yui', dir: 'js/yui/json', file: 'json-min.js']
 
 
     resource url: [plugin: 'openlayers', dir: 'css', file: 'mapwidget2.css']
@@ -221,10 +231,13 @@ modules = {
     resource url: [plugin: 'openlayers', dir: 'js', file: 'OpenLayers.js']
 
 
-    //resource url: [plugin: 'omar-core', dir: 'js', file: 'mapwidget.js']
+    resource url: [dir: 'js', file: 'OMAR.js']
+  //resource url: [plugin: 'omar-core', dir: 'js', file: 'mapwidget.js']
     resource url: [plugin: 'omar-core', dir: 'js', file: 'coord.js']
 
     resource url: [dir: 'js', file: 'application.js']
+    resource url: [dir: 'js', file: 'matrix.js']
+    //resource url: [dir: 'js', file: 'OpenLayersImageManipulator.js']
     //resource url: [dir: 'js', file: 'datechooser.js']
 
     resource url: [dir: 'css', file: 'imageSpacePageLayout.css']
