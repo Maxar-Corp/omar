0a9bace366f8f997ecbfc13743fb383ddff95525
dhicks <dhicks@b630af17-092b-0410-afba-a002956477a4>
2012-05-04 14:19:07 +0000

    created
    git-svn-id: http://svn.osgeo.org/ossim/omar@20842 b630af17-092b-0410-afba-a002956477a4

---
+++ plugins/omar-oms/grails-app/services/org/ossim/omar/oms/GeodeticEvaluationService.groovy
@@ -0,0 +1,35 @@
+package org.ossim.omar.oms
+
+import joms.oms.GeodeticEvaluator
+import joms.oms.ossimGpt
+import joms.oms.Init
+
+
+class GeodeticEvaluationService {
+
+    static transactional = false
+
+    /**
+     * @brief Determine MSL height
+     * @param groundPointList
+     * @return
+     */
+    def getHgtMSL(def groundPointList)
+    {
+        Init.instance().initialize()
+
+        def result = [];
+        def geodeticEvaluatorModel = new GeodeticEvaluator()
+        def groundPoint = new ossimGpt()
+        groundPointList.each{pt->
+            groundPoint.makeNan()
+            groundPoint.latd = pt.lat as double
+            groundPoint.lond = pt.lon as double
+            if(pt.hgt) {groundPoint.height = pt.hgt}
+            double mslHgt = geodeticEvaluatorModel.getHeightMSL(groundPoint)
+            result.add([mslHgt:mslHgt])
+        }
+
+        result;
+    }
+}
