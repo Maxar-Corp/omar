ca291e27b771d736c149109d113eae585526e489
sbortman <sbortman@b630af17-092b-0410-afba-a002956477a4>
2012-05-07 15:28:39 +0000

    Adding Grails 2.0 styling of pages to default scaffolding. Adding the new filter to the city domain class.
    git-svn-id: http://svn.osgeo.org/ossim/omar@20852 b630af17-092b-0410-afba-a002956477a4

---
+++ apps/geodata-test/grails-app/conf/ApplicationResources.groovy
@@ -0,0 +1,7 @@
+modules = {
+  common {
+    resource url: [dir: 'css', file: 'main.css']
+    resource url: [dir: 'css', file: 'mobile.css']
+    resource url: [dir: 'js', file: 'application.js']
+  }
+}
