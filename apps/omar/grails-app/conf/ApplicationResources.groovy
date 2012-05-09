522b4583842f408b1999f75e8c1324ce31214aa0
sbortman <sbortman@b630af17-092b-0410-afba-a002956477a4>
2012-05-09 19:24:43 +0000

    Moved the page layout to the common ui.   Not sure if it will stay there,  might belong in plugin that contains the Home controller.
    git-svn-id: http://svn.osgeo.org/ossim/omar@20873 b630af17-092b-0410-afba-a002956477a4

--- apps/omar/grails-app/conf/ApplicationResources.groovy
+++ apps/omar/grails-app/conf/ApplicationResources.groovy
@@ -32,6 +32,7 @@ modules = {
     resource url: [plugin: 'yui', dir: 'js/yui/menu/', file: 'menu-min.js']
     resource url: [plugin: 'yui', dir: 'js/yui/tabview/', file: 'tabview-min.js']
     resource url: [plugin: 'yui', dir: 'js/yui/calendar', file: 'calendar-min.js']
+
     resource url: [plugin: 'openlayers', dir: 'css', file: 'mapwidget2.css']
     resource url: [plugin: 'openlayers', dir: 'js/theme/default', file: 'style.css']
     resource url: [plugin: 'openlayers', dir: 'js', file: 'OpenLayers.js']
@@ -239,22 +240,6 @@ modules = {
 
     resource url: [dir: 'css', file: 'mapWidgetLayout.css']
     resource url: [dir: 'js', file: 'mapWidgetLayout.js']
-  }
-
-  homePageLayout {
-    resource url: [dir: 'css', file: 'main.css']
-    resource url: [dir: 'css', file: 'omar-2.0.css']
-
-    resource url: [plugin: 'yui', dir: "js/yui/reset-fonts-grids", file: "reset-fonts-grids.css"]
-    resource url: [plugin: 'yui', dir: "js/yui/assets/skins/sam", file: "skin.css"]
-
-    resource url: [plugin: 'yui', dir: "js/yui/yahoo-dom-event", file: "yahoo-dom-event.js"]
-    resource url: [plugin: 'yui', dir: "js/yui/element", file: "element-min.js"]
-    resource url: [plugin: 'yui', dir: "js/yui/resize", file: "resize-min.js"]
-    resource url: [plugin: 'yui', dir: "js/yui/layout", file: "layout-min.js"]
-
-    resource url: [dir: 'css', file: 'homePageLayout.css']
-    resource url: [dir: 'js', file: 'homePageLayout.js']
   }
 
   showVideoPageLayout {
