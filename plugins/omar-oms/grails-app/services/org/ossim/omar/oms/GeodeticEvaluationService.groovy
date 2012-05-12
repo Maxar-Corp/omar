f5917e64482811bc5e47785a3d824cfc01c709a3
dhicks <dhicks@b630af17-092b-0410-afba-a002956477a4>
2012-05-12 20:46:10 +0000

    added computeEllipsoidalDistAz
    git-svn-id: http://svn.osgeo.org/ossim/omar@20884 b630af17-092b-0410-afba-a002956477a4

--- plugins/omar-oms/grails-app/services/org/ossim/omar/oms/GeodeticEvaluationService.groovy
+++ plugins/omar-oms/grails-app/services/org/ossim/omar/oms/GeodeticEvaluationService.groovy
@@ -2,7 +2,6 @@ package org.ossim.omar.oms
 
 import joms.oms.GeodeticEvaluator
 import joms.oms.ossimGpt
-import joms.oms.Init
 
 
 class GeodeticEvaluationService {
@@ -27,10 +26,37 @@ class GeodeticEvaluationService {
             double mslHgt = geodeticEvaluatorModel.getHeightMSL(groundPoint)
             result.add([mslHgt:mslHgt])
         }
-        geodeticEvaluatorModel.destroy()
         geodeticEvaluatorModel.delete()
+        geodeticEvaluatorModel = null
         groundPoint.delete()
 
         result;
+    }
+
+    /**
+     * @brief Compute distance/azimuth between two points
+     * @param pointPair
+     *  [pt1:[lat:, lon:, hgt:], pt2:[lat:, lon:, hgt:]]
+     * @return [dist:(meters), az12:(deg), az21:(deg)
+     */
+    def computeEllipsoidalDistAz(def pointPair)
+    {
+        def geodeticEvaluatorModel = new GeodeticEvaluator()
+        def gp1 = new ossimGpt(pointPair.pt1.lat as double,
+                pointPair.pt1.lon as double,
+                pointPair.pt1.hgt as double)
+        def gp2 = new ossimGpt(pointPair.pt2.lat as double,
+                pointPair.pt2.lon as double,
+                pointPair.pt2.hgt as double)
+        double [] daArray = new double[3]
+
+        geodeticEvaluatorModel.computeEllipsoidalDistAz(gp1, gp2, daArray)
+
+        geodeticEvaluatorModel.delete()
+        geodeticEvaluatorModel = null
+        gp1.delete()
+        gp2.delete()
+
+        [dist:daArray[0], az12:Math.toDegrees(daArray[1]), az21:Math.toDegrees(daArray[2])];
     }
 }
