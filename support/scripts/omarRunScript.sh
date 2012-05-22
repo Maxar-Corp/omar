203316cf97028731fa86eaab18160a551daab7d5
gpotts <gpotts@b630af17-092b-0410-afba-a002956477a4>
2012-05-22 14:05:36 +0000

    Added new command line scripts
    git-svn-id: http://svn.osgeo.org/ossim/omar@20973 b630af17-092b-0410-afba-a002956477a4

---
+++ support/scripts/omarRunScript.sh
@@ -0,0 +1,311 @@
+#!/usr/bin/env groovy
+environmentVariables =[
+   OMAR_URL:"http://localhost:8080/omar",
+   OMAR_THUMBNAIL_CACHE:"/data/omar/omar-cache",
+   NTHREADS: 4,
+   OMARDB:"omardb-${System.env.OSSIM_RELEASE_VERSION_NUMBER?:System.env.OSSIM_VERSION}-prod",
+   POSTGRES_USER:"postgres",
+   POSTGRES_PASSWORD:"postgres",
+   POST_COMMAND_LINE:"NO",
+   LOG_FILE:"",
+   PID_FILE:"",
+   CLASSPATH:"${System.env.OSSIM_DIST_ROOT}/tomcat/webapps/omar/WEB-INF/lib", 
+   STAGE_FILE_FILTER:"nitf,ntf",
+]
+
+
+import java.lang.management.ManagementFactory
+import groovy.io.FileType
+import groovy.sql.Sql
+
+class OmarRunScript{
+   def env
+   def runScriptFile
+   def runScriptDirectory
+   def envVariables
+   def scriptToRun
+   def scriptArgs
+   def args
+   def outputUsage()
+   {
+      def runnableScripts = []
+    try{
+      def tempFile = runScriptDirectory as File
+         tempFile.traverse( type:FileType.FILES, nameFilter:~/.*.groovy/ ) {
+                 
+           if(it.name != runScriptFile)
+            {
+                  runnableScripts << it.name.replaceFirst(~/\.[^\.]+$/, '')
+            }
+         }
+
+      }
+      catch(def e)
+      {
+         outputLog(e)
+      }
+
+      println """Supported Scripts:
+${runnableScripts.join("\n")}
+
+To run any script please use the command: 
+   ${runScriptFile.name} <script>
+
+To bring up this usage: 
+   ${runScriptFile.name} --help
+
+To bring up help on any script please use the following format: 
+   ${runScriptFile.name} <script> --help
+
+The .groovy extension is not needed and will be appended when running a script file if the script is not present.  
+To get help on any script please run:
+
+   ${runScriptFile.name} <script> --help
+   
+"""
+   }
+   def run(args)
+   {
+      if(!args.length)
+      {
+         return 1
+      }
+      if(args[0] == "--help")
+      {
+         outputUsage();
+         return 0;
+      }
+      this.args = args
+      scriptToRun  = findScriptToRun(new File(args[0]))
+
+      if(!scriptToRun)
+      {
+         outputLog("Unable to find script ${args[0]}")
+         return 1;
+      }
+
+     env.PID_FILE = new File(scriptToRun.toString()+".pid")
+     if(checkPidRunning())
+      {
+         println "Process ${scriptToRun} is already running"
+         return 0;
+      }
+      outputPid();
+      loadJars();
+
+      def shell = new GroovyShell();
+      this.scriptArgs = [];
+      if(args.length>1) (1..args.length-1).each{this.scriptArgs << args[it]}
+      shell.setVariable("parent", this)
+      return shell.evaluate(scriptToRun)
+   }
+
+   def findScriptToRun(script)
+   {
+      def result = script
+      // test extension
+      //
+      result = script.name.lastIndexOf('.') >0?script:new File(script.toString()+".groovy")
+
+      if(!result.exists())
+      {
+         result = new File(runScriptDirectory, result.name)
+         if(!result.exists())
+         {
+            return "";
+         }
+      }
+
+      result
+   }
+   def outputLog(value)
+   {
+       if(env.LOG_FILE)
+       {
+         (env.LOG_FILE as File).withWriterAppend(){it << "${value}\n"}
+       }
+       else
+       {
+         println value;
+       }
+   }
+   def newSqlConnection()
+   {
+      Sql.newInstance([
+       url: "jdbc:postgresql:${env.OMARDB}",
+       user: "${env.POSTGRES_USER}",
+       password: "${env.POSTGRES_PASSWORD}",
+       driver: "org.postgresql.Driver"
+    ]);
+   }
+   def findWar()
+   {
+      def version = System.env.OSSIM_RELEASE_VERSION_NUMBER
+      if(!version)
+      {
+         version =  System.env.OSSIM_VERSION
+      }
+      def test = new File("${System.env.OSSIM_DIST_ROOT}/tomcat/webapps/omar.war ")
+      if(!test.exists())
+      {
+         test = new File("${System.env.OSSIM_INSTALL_PREFIX}/omar/omar.war")
+         if(!test.exists())
+         {
+            test = new File("")
+         }
+      }
+      test.toString()
+   }
+   def loadJars()
+   {
+      // For now until we get local repositories setup we will try to find the extracted WAR file location
+      //
+      if(!env.CLASSPATH)
+      {
+         // try to find class path
+         //
+         def test = new File("${System.env.OMAR_HOME}/target/WEB-INF/lib")
+         if(!test.exists())
+         {
+            def warFile = new File(findWar())
+            if(warFile.exists())
+            {
+               def warFileExtractTo = new File(warFile.parent,"omar");
+               if(!warFileExtractTo.exists())
+               {
+                  outputLog("Extracting WAR ${warFile} to location ${warFileExtractTo}")
+                  def command = "unzip ${warFile} -d ${warFileExtractTo}"
+                  def output  = command.execute();
+
+                  output.consumeProcessOutput()
+                  output.waitFor()
+               }
+               test = new File(warFileExtractTo.toString(), "WEB-INF/lib")
+            }
+         }
+         env.CLASSPATH = test.toString();
+      }
+      def loader = this.class.classLoader.rootLoader
+      def splitClasspath = env.CLASSPATH.split(":");
+      splitClasspath.each{
+         def file = new File(it)
+
+         if(file.name.endsWith(".jar"))
+         {
+            jars.each { loader.addURL(file.toURI().toURL()) }
+         }
+         else
+         {
+            try{
+               file.traverse( type:FileType.FILES, nameFilter:~/.*.jar/ ) {
+                  loader.addURL(it.toURI().toURL())
+               }
+
+            }
+            catch(def e)
+            {
+               outputLog(e)
+            }
+         }
+      }
+   }
+   def outputPid()
+   {
+      def pidString = (ManagementFactory.getRuntimeMXBean().getName() as String);
+
+
+      def pid = pidString?.find(/\d+/)
+
+      (this.env.PID_FILE as File).withWriter() { it << pid }
+      pid
+   }
+
+   def checkPidRunning()
+   {
+      def result=false
+      def PID=""
+      if(env.PID_FILE.exists())
+      {
+         PID = Integer.parseInt(env.PID_FILE.text)
+         def command = "kill -s 0 ${PID}"
+         def output = command.execute();
+
+         output.waitFor()
+         def text = output.err.text //output.consumeProcessOutput()
+
+         if(!text)
+         {
+            result = true
+         }
+      }
+
+      result;
+   }
+
+   def post(def urlParam, def urlEncodedParams)
+   {
+      def result
+      if(env.POST_COMMAND_LINE == "YES")
+      {
+         def command = "curl"
+
+         if(urlEncodedParams)
+         {
+           command += " --data ${urlEncodedParams} ${urlParam}"
+         }
+         else
+         {
+           command += " ${urlParam}"
+         }
+         def output = command.execute();
+
+         result = output.text //output.consumeProcessOutput()
+         output.waitFor()
+      }
+      else
+      {
+         def wr;
+         def url = new URL(urlParam);
+         try{
+            def connection = url.openConnection(); 
+            connection.setDoOutput(true); 
+            connection.setRequestMethod("POST"); 
+            connection.setRequestProperty("Content-Type", "application/x-www-form-urlencoded"); 
+            connection.setRequestProperty("charset", "utf-8");
+
+            if(urlEncodedParams)
+            {
+               wr = new OutputStreamWriter(connection.getOutputStream()); 
+               wr.write(urlEncodedParams); 
+               wr.flush(); 
+            }
+            result = connection.getInputStream().getText("utf-8"); 
+         }
+         catch(def e)
+         {
+            result = e
+         }
+         wr?.close(); 
+      }
+
+      result
+   }
+   def postDataManager(filename, command)
+   {
+
+      this.post("${env.OMAR_URL}/dataManager/${command}",
+            URLEncoder.encode("filename", "UTF-8") + "=" + URLEncoder.encode(filename as String, "UTF-8"))
+   }
+   def postRunning()
+   {
+      this.post("${env.OMAR_URL}/running", null)
+   }
+}
+def runScript = new OmarRunScript();
+
+
+runScript.runScriptFile  = new File(getClass().protectionDomain.codeSource.location.path)
+runScript.runScriptDirectory = runScript.runScriptFile.parent
+runScript.env = environmentVariables
+runScript.run(args);
+
---
+++ support/scripts/stageRaster.groovy
@@ -0,0 +1,268 @@
+import groovy.io.FileType
+import groovy.io.FileVisitResult
+import org.apache.commons.io.FilenameUtils 
+import joms.oms.ossimImageHandlerRegistry
+import joms.oms.Init
+import joms.oms.StringVector
+import joms.oms.DataInfo
+import joms.oms.ImageStager
+import java.util.concurrent.Executors
+import groovy.sql.Sql
+import java.util.concurrent.Callable
+import java.lang.management.ManagementFactory
+import org.joda.time.DateTime 
+import org.joda.time.DateTimeZone
+def outputHelp()
+{
+  println """
+    REQUIRED ENVIRONMENT VARIABLES
+
+      OMARDB            identify the postgres database name holding the omar tables.  Version number might
+                        be different for your distribution but it will be of the form omardb-<VERSION>-prod. 
+                        In this example we will use version 1.8.12:
+                        export OMARDB=omardb-1.8.12-prod
+
+      POSTGRES_USER     identify the postgres user name that has modifcation priviledges.  Example:
+                        export POSTGRES_USER=postgres
+
+      POSTGRES_PASSWORD identify the posgres user password
+                        export POSTGRES_PASSWORD=postgres
+
+      OMAR_URL          identify the OMAR URL.  If running local:
+                        export OMAR_URL=http://localhost/omar
+
+      PID_FILE          idientify the location where you want this script's process ID written to.  The user 
+                        must have write priviledges to the PID_FILE location.
+                        export PID_FILE=/var/run/omar/parallelStage.groovy.pid
+
+      NTHREADS          controls the number of threads used for the staging process.
+                        export NTHREADS=4
+
+   OPTIONAL ENVIRONMENT VARIABLES
+   
+      LOG_FILE          identify where the log file is written.  If blank then to standard output.
+
+      OVERVIEW_OPTIONS  specifies the overview options.  example:
+                        export OVERVIEW_OPTIONS="--compression-type JPEG --compression-quality 75"  
+
+      HISTOGRAM_OPTIONS specifies the histogram option to use. exmaples:
+                        export HISTOGRAM_OPTIONS="--create-histogram-fast"
+                        export HISTOGRAM_OPTIONS="--create-histogram"
+
+      POST_COMMAND_LINE defaults to using java code for posting.  This value can be YES or NO.  if
+                        YES then it will use curl command line application to post to the server.
+                        export POST_COMMAND_LINE=NO
+
+      STAGE_FILE_FILTER Is a comma separated list of extensions without the dot.  Examples:
+                        stage only nitf files:
+                        export STAGE_FILE_FILTER="nitf,ntf"
+
+                        if blank all files that OSSIM core engine used by OMAR will be staged into the system.
+                        It is recommended that you use a filter for only data that you want staged.  
+Staging examples:
+
+    To stage files in a directory called /data/foo we can run the command
+    omarRunScript.sh stageRaster.groovy /data/foo
+   
+    To stage files in a multiple directories called /data/foo /data/foo2 we can run the command
+    omarRunScript.sh stageRaster.groovy /data/foo /data/foo2
+
+  """
+}
+
+if(!parent.scriptArgs.size() ||parent.scriptArgs[0] == "--help")
+{
+  outputHelp()
+  return 0
+}
+
+def outputPid()
+{
+   def pidString = (ManagementFactory.getRuntimeMXBean().getName() as String);
+
+   def pid = pidString?.find(/\d+/)
+
+   (parent.env.PID_FILE as File).withWriter() { it << pid }
+   pid
+}
+def timeStamp()
+{
+  new DateTime(DateTimeZone.UTC)
+}
+def calculateDisplayTimeString(timeDelta)
+{
+  def timeValue = timeDelta /1000.0
+  def timeUnits = "s"
+  if(timeValue >= 60)
+  {
+    timeValue /= 60;
+    timeUnits = "m"
+    if(timeValue >= 60)
+    {
+      timeValue /=60
+      timeUnits = "h"
+
+      if(timeValue >= 24)
+      {
+        timeValue/=24;
+        timeUnits = "d"
+      }
+    }
+  }
+  
+  "${timeValue}${timeUnits}"
+}
+def outputLog(value)
+{
+  if(parent.env.LOG_FILE)
+  {
+    (parent.env.LOG_FILE as File).withWriterAppend(){it << "${value}\n"}
+  }
+  else
+  {
+    println value;
+  }
+}
+
+def parallelStagerPid = outputPid();
+
+Init.instance().initialize();
+extList=[]
+sql = parent.newSqlConnection()
+
+if(!sql)
+{
+   return 1
+}
+
+if(parent.env.STAGE_FILE_FILTER)
+{
+  extList = new String(parent.env.STAGE_FILE_FILTER).split(",")
+}
+else
+{
+  supportedExtensions = new StringVector()
+  ossimImageHandlerRegistry.instance().getSupportedExtensions(supportedExtensions);
+  supportedExtensions.size().times{extList << supportedExtensions.get(it)}
+  supportedExtensions.delete()
+  supportedExtensions=null
+}
+nThreads = parent.env.NTHREADS?:4
+threadPool = Executors.newFixedThreadPool(nThreads);
+futures = []
+
+
+
+def stageAndAddClosure ={file->
+   def imageStager = new ImageStager();
+   if(imageStager.open(file.toString()))
+   {
+      def commandLine = ""; 
+      def noExt = FilenameUtils.removeExtension(file.toString());
+
+      if(!imageStager.hasOverviews())
+      {
+         commandLine = "ossim-img2rr ${parent.env.OVERVIEW_OPTIONS}";
+         if(!imageStager.hasHistograms())
+         {
+            commandLine += " ${parent.env.HISTOGRAM_OPTIONS}"
+         }
+         commandLine += " ${file.absolutePath}"
+      }
+      else if(!imageStager.hasHistograms())
+      {
+         commandLine = "ossim-create-histo ${parent.env.HISTOGRAM_OPTIONS} ${file.absolutePath}";
+      }
+      def exitCode = 0
+      if(commandLine)
+      {
+         def process = commandLine.execute()
+         process.consumeProcessOutput()
+
+         exitCode = process.waitFor()
+      }
+      if(!exitCode)
+      {
+        outputLog("${timeStamp()}: " + parent.postDataManager(file, "addRaster"))
+         //println "${new DateTime(DateTimeZone.UTC)}: " + postDataManager(parent.env.OMAR_URL, file, "addRaster");
+      }
+   }
+   imageStager?.delete();
+   imageStager = null
+}
+
+
+def options = [
+    type: FileType.FILES,
+    preDir: { dir ->
+
+       def status = FileVisitResult.CONTINUE
+
+       if("toc" in extList)
+       {
+         def atoc = new File(dir, "a.toc")
+         def row = sql?.firstRow("SELECT name FROM raster_file where name = ${atoc.toString()}")
+         if ( atoc.exists() &&!row)
+         {
+             //closure(atoc)
+             if(this.threadPool)
+             {
+                 this.futures << this.threadPool.submit({-> stageAndAddClosure atoc } as Callable);
+             }
+             status =  FileVisitResult.SKIP_SUBTREE 
+         } 
+       }
+       return status
+    }, 
+    filter: { file ->  
+        def ext = FilenameUtils.getExtension(file.name).toLowerCase()
+
+         return ((ext in extList)&&(ext!="til"))
+     }
+]
+
+
+def startTime = System.currentTimeMillis()
+outputLog("parallelStager id ${parallelStagerPid} started: ${timeStamp()}" )
+if(parent.scriptArgs.size())
+{
+   // grab dirs
+   //
+   def dirList = []
+   parent.scriptArgs.size().times{
+      dirList << new File(parent.scriptArgs[it])
+   }
+   //def count = 0;
+   dirList.each{dir->
+      dir.traverse( options){file->
+         def row = sql?.firstRow("SELECT name FROM raster_file where name = ${file.toString()}")
+         if(row)
+         {
+         }
+         else
+         {  
+            def dataInfo = new DataInfo();
+
+            if(dataInfo.open(file as String))
+            {
+               futures << threadPool.submit({-> stageAndAddClosure file } as Callable);
+            }
+            dataInfo.delete()
+            dataInfo = null
+         }
+      }
+   }
+
+   // make sure the list is called
+   futures.each{it.get()}
+
+   threadPool.shutdown();
+}
+sql.close();
+
+def stopTime = System.currentTimeMillis()
+def multiplier = 1/(1000*60)
+
+outputLog("parallelStager id ${parallelStagerPid} ended: ${timeStamp()} and took ${calculateDisplayTimeString(stopTime - startTime)}" )
+
+
