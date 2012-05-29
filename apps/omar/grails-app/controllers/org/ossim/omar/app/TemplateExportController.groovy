87342499550514bf6f68f4623ac5a511f54fa816
kjnaquin <kjnaquin@b630af17-092b-0410-afba-a002956477a4>
2012-05-29 02:08:03 +0000

    This is the controller to handle all the template export functions using image magick. Right now it just has two functions.
    git-svn-id: http://svn.osgeo.org/ossim/omar@21015 b630af17-092b-0410-afba-a002956477a4

---
+++ apps/omar/grails-app/controllers/org/ossim/omar/app/TemplateExportController.groovy
@@ -0,0 +1,80 @@
+package org.ossim.omar.app
+
+class TemplateExportController 
+{
+	def grailsApplication
+	def index = 
+        {
+		def acquisitionDate = params.acquisitionDate
+		def countryCode = params.countryCode
+		def imageId = params.imageId
+		def imageURL = params.imageURL
+		def mgrs = params.mgrs
+	
+		render(
+			view:"templateExport.gsp", 
+			model:
+			[
+				acquisitionDate: acquisitionDate, 
+				countryCode: countryCode, 
+				imageId: imageId,
+				imageURL: "http://localhost" + imageURL,
+				mgrs: mgrs
+			]
+		)
+        }
+
+	def export = 
+	{
+		def imageFile = params.imageURL
+		//println "${imageFile}"
+		def logo = params.logo
+		//prinln "${logo}"
+		def line1 = params.line1
+		//println "${line1}"
+		def line2 = params.line2
+		//println "${line2}"
+		def line3 = params.line3
+		//println "${line3}"
+		
+ 		def pathToImageMagick = grailsApplication.config.pathToImageMagick
+                def logoFilesLocation = grailsApplication.config.logoFilesLocation
+                def tempFilesLocation = grailsApplication.config.tempFilesLocation
+                def scriptLocation = grailsApplication.config.templateExportScriptLocation
+               
+		def date = new Date().getTime() 
+		def paramsFile = new File("${tempFilesLocation}${date}" + "paramsFile.txt")
+		paramsFile.write("${pathToImageMagick}\n")
+		paramsFile.append("${imageFile}\n")
+		paramsFile.append("${logo}\n")
+		paramsFile.append("${line1}\n")
+		paramsFile.append("${line2}\n")
+		paramsFile.append("${line3}\n")
+		paramsFile.append("${logoFilesLocation}\n")
+		paramsFile.append("${tempFilesLocation}\n")
+		paramsFile.append("${date}\n")
+		
+		def script = "${scriptLocation}templateExport.pl ${tempFilesLocation}${date}"+ "paramsFile.txt"
+		def scriptProc = script.execute()
+		scriptProc.waitFor()
+		def fileName = scriptProc.text
+		//println "${fileName}"
+		
+		def file = new File("${fileName}")
+		println "cheese"
+		if (file.exists())
+		{
+			response.setContentType("application/octet-stream")
+			response.setHeader("Content-disposition", "filename=${file.name}")
+			response.outputStream << file.bytes
+
+			def removeImageFile = "rm ${file}"
+			def removeImageFileProc = removeImageFile.execute()
+			removeImageFileProc.waitFor()
+		}
+		
+		def removeParamsFile = "rm ${tempFilesLocation}${date}" + "paramsFile.txt"
+		def removeParamsFileProc = removeParamsFile.execute()
+		removeParamsFileProc.waitFor()
+	}
+}
