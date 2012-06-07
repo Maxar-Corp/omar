8c4fac5bb5ccfdab7b9815f3e33c72523c01acc9
gpotts <gpotts@b630af17-092b-0410-afba-a002956477a4>
2012-06-07 17:04:57 +0000

    Fixed query
    git-svn-id: http://svn.osgeo.org/ossim/omar@21093 b630af17-092b-0410-afba-a002956477a4

--- plugins/omar-ogc/src/groovy/org/ossim/omar/ogc/WMSQuery.groovy
+++ plugins/omar-ogc/src/groovy/org/ossim/omar/ogc/WMSQuery.groovy
@@ -1,8 +1,9 @@
 package org.ossim.omar.ogc
 
 import org.hibernate.criterion.Criterion
-import  org.ossim.omar.raster.RasterEntryQuery
-import org.ossim.omar.raster.RasterEntry
+import org.ossim.omar.core.Utility
+import org.ossim.omar.core.ISO8601DateParser
+import org.ossim.omar.core.BaseQuery
 
 /**
  * Created by IntelliJ IDEA.
@@ -11,19 +12,168 @@ import org.ossim.omar.raster.RasterEntry
  * Time: 10:11:18 AM
  * To change this template use File | Settings | File Templates.
  */
-class WMSQuery extends RasterEntryQuery
+class WMSQuery extends BaseQuery
 {
+  def niirs
   def bbox
   def layers
   def max
   WMSQuery()
   {
-    super()
-
-//    filterTypeMap = Utility.createTypeMap(RasterEntry.class) +
-//            Utility.createTypeMap(VideoDataSet.class)
-
+      super()
+      filterTypeMap = Utility.createTypeMap(org.ossim.omar.raster.RasterEntry.class)
   }
+    Criterion createDateRange(String dateColumnName = "acquisitionDate")
+    {
+        def range = null
+
+        if ( startDate && endDate )
+        {
+            range = Restrictions.and(
+                    Restrictions.ge(dateColumnName, startDate),
+                    Restrictions.le(dateColumnName, endDate)
+            )
+        }
+        else
+        {
+            if ( startDate )
+            {
+                range = Restrictions.ge(dateColumnName, startDate)
+            }
+            else if ( endDate )
+            {
+                range = Restrictions.le(dateColumnName, endDate)
+            }
+        }
+
+        return range
+    }
+    def createDateRangeTime(def columnName="acquisitionDate")
+    {
+        def dateColumnName = columnName
+        def disj = null
+
+        if(time)
+        {
+            disj = Restrictions.disjunction();
+
+            def intervals = ISO8601DateParser.parseOgcTimeIntervals(time)
+            if(intervals)
+            {
+                intervals.each{interval->
+                    def startDate = new Date(interval.getStart().getMillis());
+                    def endDate   = new Date(interval.getEnd().getMillis());
+                    if(interval.toDurationMillis() == 0)
+                    {
+                        def range = null
+
+                        if ( startDate && endDate )
+                        {
+                            disj.add(Restrictions.eq(dateColumnName, startDate))
+                        }
+                    }
+                    else
+                    {
+                        disj.add(Restrictions.and(Restrictions.ge(dateColumnName, startDate),
+                                Restrictions.le(dateColumnName, endDate)
+                        )
+                        )
+                    }
+                }
+            }
+            else
+            {
+                disj = null
+            }
+        }
+        disj
+    }
+
+    def createClause()
+    {
+        def baseClause = super.createClause()
+
+        def result = null
+        if(baseClause instanceof org.hibernate.criterion.Conjunction)
+        {
+            result = baseClause
+        }
+        else
+        {
+            result =  Restrictions.conjunction();
+            if(baseClause)
+            {
+                result.add(baseClause)
+            }
+        }
+
+
+        if ( groundGeom )
+        {
+            def criterion = createIntersection("groundGeom")
+            if ( criterion )
+            {
+                result.add(criterion)
+            }
+        }
+
+        if ( startDate || endDate )
+        {
+            def criterion = createDateRange("acquisitionDate")
+            if ( criterion )
+            {
+                result.add(criterion)
+            }
+        }
+        else if(time)
+        {
+            def criterion = createDateRangeTime("acquisitionDate")
+            if ( criterion )
+            {
+                result.add(criterion)
+            }
+        }
+
+        // we will support 2 ways to populate certain fields.  We will support array
+        // or direct.  niirs will be direct field or an array
+        if ( niirs )
+        {
+            result.add(Restrictions.ge("niirs", niirs as double))
+        }
+
+        searchTagNames?.size()?.times {i ->
+            String name = searchTagNames[i]
+            String value = searchTagValues[i]
+
+            if ( name && value )
+            {
+                def results = Utility.parseSearchTag(name, value)
+
+                if ( results["property"] == "otherTagsXml" )
+                {
+                    String tag = results["tag"].trim()
+                    String content = results["content"].trim()
+                    result.add(Restrictions.ilike("otherTagsXml", "%<${tag}>%${content}%</${tag}>%"))
+                }
+                else
+                {
+                    String prop = results["property"]
+                    prop = prop.toLowerCase()
+                    if ( prop == "niirs" && !niirs )
+                    {
+                        result.add(Restrictions.ge("niirs", results['value'] as double))
+                    }
+                    else
+                    {
+                        //result.add(Restrictions.like(results["property"], results['value'], MatchMode.ANYWHERE))
+                        result.add(Restrictions.ilike(results["property"], results['value'], MatchMode.ANYWHERE))
+                    }
+                }
+            }
+        }
+        return result;
+    }
+
   Criterion createIntersection(String geomColumnName = "groundGeom")
   {
     if ( bbox )
@@ -38,6 +188,7 @@ class WMSQuery extends RasterEntryQuery
 
     return super.createIntersection(geomColumnName)
   }
+
   def caseInsensitiveBind(def params)
   {
       super.caseInsensitiveBind(params)
@@ -54,176 +205,4 @@ class WMSQuery extends RasterEntryQuery
 
     baseClause
   }
-/*
-  def createDateRangeRestrictionRaster(def columnName = "acquisitionDate")
-  {
-    def dateColumnName = columnName
-    def disj = Restrictions.disjunction();
-
-    def intervals = ISO8601DateParser.parseOgcTimeIntervals(time)
-    intervals.each {interval ->
-      def startDate = new Date(interval.getStart().getMillis());
-      def endDate = new Date(interval.getEnd().getMillis());
-      if ( interval.toDurationMillis() == 0 )
-      {
-        def range = null
-
-        if ( startDate && endDate )
-        {
-          disj.add(Restrictions.eq(dateColumnName, startDate))
-        }
-      }
-      else
-      {
-        disj.add(Restrictions.and(Restrictions.ge(dateColumnName, startDate),
-                Restrictions.le(dateColumnName, endDate)
-        )
-        )
-      }
-    }
-    return disj
-  }
-
-  def createVideoClause()
-  {
-
-  }
-
-  Criterion createIntersection(String geomColumnName = "groundGeom")
-  {
-    if ( bbox )
-    {
-      def bounds = bbox.split(',')
-      aoiMinLon = bounds[0]
-      aoiMinLat = bounds[1]
-      aoiMaxLon = bounds[2]
-      aoiMaxLat = bounds[3]
-      searchMethod = BBOX_SEARCH
-    }
-
-    return super.createIntersection(geomColumnName)
-  }
-
-  def createRasterClause()
-  {
-    def names = []
-    if ( layers )
-    {
-      layers.split(',').each
-              {
-                names.add(it)
-              }
-    }
-    def baseClause = super.createClause()
-
-    def result = null
-    if ( baseClause instanceof org.hibernate.criterion.Conjunction )
-    {
-      result = baseClause
-    }
-    else
-    {
-      result = Restrictions.conjunction();
-      if ( baseClause )
-      {
-        result.add(baseClause)
-      }
-    }
-
-    def geomIntersect = createIntersection()
-    if ( geomIntersect )
-    {
-      result.add(geomIntersect)
-    }
-    if ( names.size() > 0 )
-    {
-      def disj = Restrictions.disjunction();
-      names.each() {name ->
-        try
-        {
-          def value = java.lang.Long.valueOf(name)
-          disj.add(Restrictions.eq('id', value))
-        }
-        catch (java.lang.Exception e)
-        {
-          disj.add(Restrictions.eq('imageId', name))
-          disj.add(Restrictions.eq('indexId', name))
-        }
-      }
-      result.add(disj)
-    }
-    def dateIntersect = createDateRangeRestrictionRaster()
-    if ( dateIntersect )
-    {
-      result.add(dateIntersect)
-    }
-
-    return result
-  }
-
-  def getRasterEntriesAsList()
-  {
-    def names = layers ? layers.split(",") : []
-    def layersCopy = layers
-    def tempMax = 10
-    try
-    {
-      tempMax = Integer.valueOf(max);
-    }
-    catch (Exception e)
-    {
-      tempMax = 10;
-    }
-    def result = []
-    if ( names.size() > 0 )
-    {
-      // we must query layer results individually
-      names.each {name ->
-        layers = name
-        def tempRasterEntry = RasterEntry.createCriteria().list {
-          maxResults(tempMax)
-          addToCriteria(createRasterClause())
-        }
-        if ( tempRasterEntry )
-        {
-          tempRasterEntry.each {
-            result.add(it)
-            if ( result.size() >= tempMax )
-            {
-              return result
-            }
-          }
-        }
-      }
-    }
-    else
-    {
-      def ordering = null
-      if ( order && sort )
-      {
-        ordering = (order == "asc") ? Order.asc(sort) : Order.desc(sort)
-      }
-      def tempRasterEntry = RasterEntry.createCriteria().list {
-        maxResults(tempMax)
-        addToCriteria(createRasterClause())
-        if ( ordering ) addOrder(ordering)
-      }
-      if ( tempRasterEntry )
-      {
-        tempRasterEntry.each {
-          result.add(it)
-          if ( result.size() >= tempMax )
-          {
-            return result
-          }
-        }
-      }
-      // we will add support for a general query that is similar to the last 10 or last N images.
-    }
-    layers = layersCopy
-
-    return result;
-
-  }
-*/
 }
