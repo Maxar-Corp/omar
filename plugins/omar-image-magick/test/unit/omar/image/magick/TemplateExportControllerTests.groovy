672205a5b727ca1745c6e632701cbb708e57c5b8
kjnaquin <kjnaquin@b630af17-092b-0410-afba-a002956477a4>
2012-07-04 01:54:29 +0000

    This is the plugin the handle all the image magick stuff. First up is the template export.
    git-svn-id: http://svn.osgeo.org/ossim/omar@21221 b630af17-092b-0410-afba-a002956477a4

---
+++ plugins/omar-image-magick/test/unit/omar/image/magick/TemplateExportControllerTests.groovy
@@ -0,0 +1,17 @@
+package omar.image.magick
+
+
+
+import grails.test.mixin.*
+import org.junit.*
+
+/**
+ * See the API for {@link grails.test.mixin.web.ControllerUnitTestMixin} for usage instructions
+ */
+@TestFor(TemplateExportController)
+class TemplateExportControllerTests {
+
+    void testSomething() {
+       fail "Implement me"
+    }
+}
