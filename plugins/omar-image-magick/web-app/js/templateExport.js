0045fc2715ae9c88e9140dda6cbc7d13c2415c94
kjnaquin <kjnaquin@b630af17-092b-0410-afba-a002956477a4>
2012-07-18 21:05:58 +0000

    Took away all the grails tags.
    git-svn-id: http://svn.osgeo.org/ossim/omar@21311 b630af17-092b-0410-afba-a002956477a4

--- plugins/omar-image-magick/web-app/js/templateExport.js
+++ plugins/omar-image-magick/web-app/js/templateExport.js
@@ -1,90 +1,89 @@
-//var countryMap;
+ var countryMap;
 
-//function changeLogo(logo)
-//{
-//	${"logoImage"}.src = "${resource(dir: 'images/', plugin: 'omar-image-magick')}" + logo + ".png?_debugResources=y";
-//}
+function changeLogo(logo)
+{
+	document.getElementById("logoImage").src = document.getElementById("imageResourceLocation").innerHTML + logo + ".png";
+}
 
-//function changeCountry(country)
-//{
-//	countryMap = country;
-//	updateCountryMaps();
-//}
+function changeCountry(country)
+{
+	countryMap = country;
+	updateCountryMaps();
+}
 
-//function exportPreview()
-//{
-//	exportPreviewSetup();
-//	var previewUrl = "${createLink(action: 'exportPreview')}";
-//	var countryCode = ${"country"}.options.selectedIndex;
-//	previewUrl += "?country=" + ${"country"}.options[countryCode].value;
-//	previewUrl += "&imageURL=" + (${"imageURL"}.value).replace(/&/g,"%26");
-//	previewUrl += "&includeOutlineMap=null";
-//	if (${"includeOverviewMap"}.checked)
-//	{
-//		previewUrl += "&includeOverviewMap=on";
-//	}
-//	else
-//	{
-//		previewUrl += "&includeOverviewMap=null";
-//	}
-//	var logoId = ${"logo"}.options.selectedIndex;
-//	previewUrl += "&logo=" + ${"logo"}.options[logoId].value;
-//	previewUrl += "&line1=" + ${"line1"}.value;
-//	previewUrl += "&line2=" + ${"line2"}.value;
-//	previewUrl += "&line3=" + ${"line3"}.value;
-//	previewUrl += "&northArrowAngle=" + ${"northArrowAngle"}.value;
-//	previewUrl += "&securityClassification=" + ${"securityClassification"}.value;
-//	${"preview"}.onload = function() { exportPreviewCleanup(); }
-//	${"preview"}.src = previewUrl;
-//}
+function exportPreview()
+{
+	exportPreviewSetup();
+	var previewUrl = document.getElementById("exportPreviewUrl").innerHTML;
+		var countryCode = document.getElementById("country").options.selectedIndex;
+	previewUrl += "?country=" + document.getElementById("country").options[countryCode].value;
+	previewUrl += "&imageURL=" + (document.getElementById("imageURL").value).replace(/&/g,"%26");
+	previewUrl += "&includeOutlineMap=null";
 
-//function exportPreviewCleanup()
-//{
-//	${"templateStatus"}.style.display = "none";
-//	${"loading"}.style.display = "none";
-//	${"preview"}.style.display = "block";
-//}
+	if (document.getElementById("includeOverviewMap").checked)
+	{
+		previewUrl += "&includeOverviewMap=on";
+	}
+	else
+	{
+		previewUrl += "&includeOverviewMap=null";
+	}
+		var logoId = document.getElementById("logo").options.selectedIndex;
+	previewUrl += "&logo=" + document.getElementById("logo").options[logoId].value;
+	previewUrl += "&line1=" + document.getElementById("line1").value;
+	previewUrl += "&line2=" + document.getElementById("line2").value;
+	previewUrl += "&line3=" + document.getElementById("line3").value;
+	previewUrl += "&northArrowAngle=" + document.getElementById("northArrowAngle").value;
+	previewUrl += "&securityClassification=" + document.getElementById("securityClassification").value;
+	document.getElementById("preview").onload = function() { exportPreviewCleanup(); }
+	document.getElementById("preview").src = previewUrl;
+}
 
-//function exportPreviewSetup()
-//{
-//	${"templateStatus"}.style.display = "block";
-//	${"loading"}.src = "${resource(dir: 'images/', plugin: 'omar-image-magick')}imageLoading.gif?_debugResources=y";
-//	${"loading"}.style.display = "block";
-//	${"preview"}.style.display = "none";
-//}
+function exportPreviewCleanup()
+{
+	document.getElementById("templateStatus").style.display = "none";
+	document.getElementById("loading").style.display = "none";
+	document.getElementById("preview").style.display = "block";
+}
 
-//function init()
-//{
-//	${"templateStatus"}.style.display = "none";
-//	countryMap = "aa";
-//	var countryCode = "${countryCode}".toLowerCase();
-//	for (var i = 0; i < ${"country"}.options.length; i++)
-//	{
-//		if (countryCode == ${"country"}.options[i].value)
-//		{
-//			${"country"}.options.selectedIndex = i;
-//			countryMap = ${"country"}.options[i].value;
-//		}
-//	}
-//	updateCountryMaps();
-//	changeLogo("ciaLogo");
-//}
+function exportPreviewSetup()
+{
+	document.getElementById("templateStatus").style.display = "block";
+	document.getElementById("loading").src = document.getElementById("imageResourceLocation").innerHTML + "imageLoading.gif";
+	document.getElementById("loading").style.display = "block";
+	document.getElementById("preview").style.display = "none";
+}
 
-//function submitTemplate()
-//{
-//	${"templateStatus"}.style.display = "block";
-//}
+function init()
+{
+	countryMap = "aa";
+	var countryCode = document.getElementById("countryCode").innerHTML.toLowerCase();
+	for (var i = 0; i < document.getElementById("country").options.length; i++)
+	{
+		if (countryCode == document.getElementById("country").options[i].value)
+		{
+			document.getElementById("country").options.selectedIndex = i;
+			countryMap = document.getElementById("country").options[i].value;
+		}
+	}
+	updateCountryMaps();
+}
 
-//function updateCountryMaps()
-//{
-//	if (${"includeOverviewMap"}.checked)
-//	{
-//		${"overviewMap"}.style.visibility = "visible";
-//		var image = "${resource(dir: 'images/overviewMaps/', plugin: 'omar-image-magick')}" + countryMap + ".gif?_debugResources=y";
-//		${"overviewMap"}.innerHTML = "<img src = '" + image + "' width = '100'/>";
-//	}
-//	else
-//	{
-//		${"overviewMap"}.style.visibility = "hidden";
-//	}
-//}
+function submitTemplate()
+{
+	document.getElementById("templateStatus").style.display = "block";
+}
+
+function updateCountryMaps()
+{
+	if (document.getElementById("includeOverviewMap").checked)
+	{
+		document.getElementById("overviewMap").style.visibility = "visible";
+		var image = document.getElementById("imageResourceLocation").innerHTML + "overviewMaps/" + countryMap + ".gif";
+		document.getElementById("overviewMap").innerHTML = "<img src = '" + image + "' width = '100'/>";
+	}
+	else
+	{
+		document.getElementById("overviewMap").style.visibility = "hidden";
+	}
+}
