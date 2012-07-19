99ce295bd565de3bb95e6b8be3811267f5a1f39f
kjnaquin <kjnaquin@b630af17-092b-0410-afba-a002956477a4>
2012-07-19 02:33:00 +0000

    Added more resources to comply with existing OMAR styles.
    git-svn-id: http://svn.osgeo.org/ossim/omar@21315 b630af17-092b-0410-afba-a002956477a4

--- plugins/omar-image-magick/grails-app/conf/TemplateExportResources.groovy
+++ plugins/omar-image-magick/grails-app/conf/TemplateExportResources.groovy
@@ -1,5 +1,9 @@
 modules = {
 	templateExport {
+		//resource url: [dir: 'css', file: 'omar-2.0.css']
+   		resource url: [plugin: 'yui', dir: 'js/yui/reset-fonts-grids', file: 'reset-fonts-grids.css']
+		//resource url: [plugin: 'yui', dir: 'js/yui/assets/skins/sam', file: 'skin.css'] 
+		//resource url: [plugin: 'openlayers', dir: 'js/theme/default', file: 'style.css']
 		resource url: [dir: 'js', file: 'templateExport.js', plugin: 'omar-image-magick']
 	}
 }
