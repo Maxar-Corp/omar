c7c9e2073009db9805ef1f0d8a878ab1327c0929
kjnaquin <kjnaquin@b630af17-092b-0410-afba-a002956477a4>
2012-07-19 14:53:37 +0000

    Cleaned up all the resources.
    git-svn-id: http://svn.osgeo.org/ossim/omar@21331 b630af17-092b-0410-afba-a002956477a4

--- plugins/omar-image-magick/grails-app/conf/TemplateExportResources.groovy
+++ plugins/omar-image-magick/grails-app/conf/TemplateExportResources.groovy
@@ -1,33 +1,13 @@
 modules = {
 	templateExport {
-
-
-
-resource url: [dir: 'css', file: 'main.css']
-   
-
-    resource url: [plugin: 'yui', dir: "js/yui/reset-fonts-grids", file: "reset-fonts-grids.css"]
-    resource url: [plugin: 'yui', dir: "js/yui/assets/skins/sam", file: "skin.css"]
-
-    resource url: [plugin: 'yui', dir: "js/yui/yahoo-dom-event", file: "yahoo-dom-event.js"]
-    resource url: [plugin: 'yui', dir: "js/yui/element", file: "element-min.js"]
-    resource url: [plugin: 'yui', dir: "js/yui/resize", file: "resize-min.js"]
-    resource url: [plugin: 'yui', dir: "js/yui/layout", file: "layout-min.js"]
-
-    resource url: [dir: 'css', file: 'homePageLayout.css']
-    resource url: [dir: 'js', file: 'homePageLayout.js']
-
-
-
-
-
-		//resource url: [dir: 'css', file: 'main.css']
-		//resource url: [dir: 'css', file: 'omar-2.0.css']
-   		//resource url: [plugin: 'yui', dir: 'js/yui/reset-fonts-grids', file: 'reset-fonts-grids.css']
-
-		//resource url: [plugin: 'yui', dir: 'js/yui/assets/skins/sam', file: 'skin.css'] 
-		//resource url: [plugin: 'openlayers', dir: 'js/theme/default', file: 'style.css']
-		//resource url: [dir: 'css', file: 'homePageLayout.css']	
+		resource url: [dir: 'css', file: 'main.css']
+		resource url: [plugin: 'yui', dir: "js/yui/reset-fonts-grids", file: "reset-fonts-grids.css"]
+		resource url: [plugin: 'yui', dir: "js/yui/assets/skins/sam", file: "skin.css"]
+		resource url: [plugin: 'yui', dir: "js/yui/yahoo-dom-event", file: "yahoo-dom-event.js"]
+		resource url: [plugin: 'yui', dir: "js/yui/element", file: "element-min.js"]
+		resource url: [plugin: 'yui', dir: "js/yui/resize", file: "resize-min.js"]
+		resource url: [plugin: 'yui', dir: "js/yui/layout", file: "layout-min.js"]
+		resource url: [dir: 'css', file: 'homePageLayout.css']
 		resource url: [dir: 'css', file: 'templateExport.css', plugin: 'omar-image-magick']
 		resource url: [dir: 'js', file: 'templateExport.js', plugin: 'omar-image-magick']
 	}
