66f103ecb1267e9fc56e05d3bdc63d65e72cf8af
kjnaquin <kjnaquin@b630af17-092b-0410-afba-a002956477a4>
2012-07-19 11:54:26 +0000

    I added the page specific css resource.
    git-svn-id: http://svn.osgeo.org/ossim/omar@21318 b630af17-092b-0410-afba-a002956477a4

--- plugins/omar-image-magick/grails-app/conf/TemplateExportResources.groovy
+++ plugins/omar-image-magick/grails-app/conf/TemplateExportResources.groovy
@@ -4,6 +4,7 @@ modules = {
    		resource url: [plugin: 'yui', dir: 'js/yui/reset-fonts-grids', file: 'reset-fonts-grids.css']
 		//resource url: [plugin: 'yui', dir: 'js/yui/assets/skins/sam', file: 'skin.css'] 
 		//resource url: [plugin: 'openlayers', dir: 'js/theme/default', file: 'style.css']
+		resource url: [dir: 'css', file: 'templateExport.css', plugin: 'omar-image-magick']
 		resource url: [dir: 'js', file: 'templateExport.js', plugin: 'omar-image-magick']
 	}
 }
