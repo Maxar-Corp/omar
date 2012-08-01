2c6861ca3db258a83f91275e8fee9bf6e0ea8cc8
sbortman <sbortman@b630af17-092b-0410-afba-a002956477a4>
2012-08-01 13:07:56 +0000

    Updated geometry column name for ais as it changed from ais_geom to geometry_object
    git-svn-id: http://svn.osgeo.org/ossim/omar@21437 b630af17-092b-0410-afba-a002956477a4

--- apps/geodata-test/geoms.sql
+++ apps/geodata-test/geoms.sql
@@ -1,6 +1,6 @@
-ALTER TABLE location DROP COLUMN ais_geom;
-SELECT AddGeometryColumn( 'location', 'ais_geom', 4326, 'POINT', 2 );
-CREATE INDEX location_ais_geom_idx ON location USING GIST ( ais_geom GIST_GEOMETRY_OPS );
+ALTER TABLE location DROP COLUMN geometry_object;
+SELECT AddGeometryColumn( 'location', 'geometry_object', 4326, 'POINT', 2 );
+CREATE INDEX location_geometry_object_idx ON location USING GIST ( geometry_object GIST_GEOMETRY_OPS );
 
 ALTER TABLE city DROP COLUMN ground_geom;
 SELECT AddGeometryColumn( 'city', 'ground_geom', 4326, 'POINT', 2 );
