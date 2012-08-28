a43236aa0cbb674df6ba72debb8259d22e536644
gpotts <gpotts@b630af17-092b-0410-afba-a002956477a4>
2012-08-28 19:38:17 +0000

    Fixing the parallel queue ingest
    git-svn-id: http://svn.osgeo.org/ossim/omar@21540 b630af17-092b-0410-afba-a002956477a4

--- plugins/omar-stager/grails-app/services/org/ossim/omar/stager/StagerService.groovy
+++ plugins/omar-stager/grails-app/services/org/ossim/omar/stager/StagerService.groovy
@@ -2,19 +2,66 @@ package org.ossim.omar.stager
 
 import org.ossim.omar.core.Repository
 import org.ossim.omar.StagerJob
+import org.ossim.omar.core.HttpStatusMessage
+import java.util.concurrent.Executors
+import java.util.concurrent.Callable
+import java.lang.management.ManagementFactory
+import static groovyx.gpars.GParsPool.withPool
 
 class StagerService
 {
+    static transactional = true
+    def dataManagerService
+    def grailsApplication
+    def sessionFactory
+    def runStager(Repository repository)
+    {
 
-  static transactional = true
+        repository.scanStartDate = new Date()
+        repository.scanEndDate = null
+        repository.save()
 
-  def runStager(Repository repository)
-  {
+        StagerJob.triggerNow([baseDir: repository.baseDir])
+    }
+    def cleanUpGorm( )
+    {
+        def session = sessionFactory.currentSession
+        session.flush()
+        session.clear()
+    }
+    def popAndAddStagerQueueItem()
+    {
+        def result = 0
+        def nthreads = grailsApplication.config.stager.queue.threads?:4
+        try{
+            StagerQueueItem.withTransaction{
+                def records = StagerQueueItem.list(cache:false,
+                        sort:"dateCreated",
+                        max:100,
+                        order:"desc")
+                records.each{record->
+                    record.status = "indexing"
+                    record.save()
+                }
+                withPool() {
+                    records.collectParallel{item->
+                        def msg = new HttpStatusMessage();
+                        dataManagerService.add(msg, [datainfo:item.dataInfo])
+                    }
 
-    repository.scanStartDate = new Date()
-    repository.scanEndDate = null
-    repository.save()
+                }
+                result += records.size();
+                records.each{record->
+                    record.delete()
+                }
+            }
+        }
+        catch(def e)
+        {
+            println e
+        }
 
-    StagerJob.triggerNow([baseDir: repository.baseDir])
-  }
+        cleanUpGorm()
+        result
+    }
 }
