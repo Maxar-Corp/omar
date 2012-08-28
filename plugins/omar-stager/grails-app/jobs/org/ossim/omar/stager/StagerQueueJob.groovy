e7c67c4cabdd1b14ae0d06bc125fed236a1a1762
gpotts <gpotts@b630af17-092b-0410-afba-a002956477a4>
2012-08-28 19:38:17 +0000

    Fixing the parallel queue ingest
    git-svn-id: http://svn.osgeo.org/ossim/omar@21540 b630af17-092b-0410-afba-a002956477a4

--- plugins/omar-stager/grails-app/jobs/org/ossim/omar/stager/StagerQueueJob.groovy
+++ plugins/omar-stager/grails-app/jobs/org/ossim/omar/stager/StagerQueueJob.groovy
@@ -1,31 +1,38 @@
 package org.ossim.omar.stager
 
 import java.util.concurrent.atomic.AtomicInteger
-import static groovyx.gpars.GParsPool.withPool
 import org.quartz.JobExecutionContext
 import org.quartz.JobDataMap
+import scala.actors.threadpool.AtomicInteger
+import org.ossim.omar.core.HttpStatusMessage
 
 
 class StagerQueueJob {
-    def timeout = 5000l // execute job once in 5 seconds
-    def grailsApplication
-    def index
-    def execute(JobExecutionContext context) {
-        JobDataMap dataMap = context.getMergedJobDataMap();
-
-        index = new AtomicInteger( 0 )
-
-        def threadCount = grailsApplication.config.stager.queue.threads?:4
-
-        withPool(threadCount)
-        {
-            if ( index?.incrementAndGet() % threadCount == 0 )
-            {
-                cleanUpGorm()
-            }
-        }
-        // execute task
+    static triggers = {
+        simple group:'StagerQueue', name: 'StagerQueueJob',
+               priority:10, startDelay:60000, repeatInterval: 10000
     }
+    def concurrent = false
+    def grailsApplication
+    def stagerService
+    static entered = 0
 
+    def execute(JobExecutionContext context) {
+        def start = System.currentTimeMillis()
+        int count = 0;
+        def tempCount = 0;
+        try{
+            while(tempCount = stagerService.popAndAddStagerQueueItem()){count += tempCount}
+        }
+        catch(def e)
+        {
 
+        }
+
+        def stop = System.currentTimeMillis()
+        if(count)
+        {
+            log.info("Staged ${count} images in ${(stop - start)/1000} seconds")
+        }
+    }
 }
