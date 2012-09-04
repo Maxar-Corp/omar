1f869922d939d9634ed1e2ad886298a2a9fddad8
gpotts <gpotts@b630af17-092b-0410-afba-a002956477a4>
2012-09-04 12:02:46 +0000

    added logs for getTile chipping
    git-svn-id: http://svn.osgeo.org/ossim/omar@21592 b630af17-092b-0410-afba-a002956477a4

---
+++ plugins/omar-raster/grails-app/domain/org/ossim/omar/raster/GetTileLog.groovy
@@ -0,0 +1,37 @@
+package org.ossim.omar.raster
+
+class GetTileLog {
+    Double x
+    Double y
+    Long   width
+    Long   height
+    String format
+    String layers
+    Double scale
+    Double internalTime
+    Double renderTime
+    Double totalTime
+    Date   startDate
+    Date   endDate
+    String userName
+    String ip
+    String url
+
+    static constraints = {
+        x(nullable:true)
+        y(nullable:true)
+        width(nullable:true)
+        height(nullable:true)
+        format(nullable:true)
+        layers(nullable:true)
+        scale(nullable:true)
+        internalTime(nullable:true)
+        renderTime(nullable:true)
+        totalTime(nullable:true)
+        startDate(nullable:true)
+        endDate(nullable:true)
+        userName(nullable:true)
+        ip(nullable:true)
+        url(nullable:true)
+    }
+}
---
+++ plugins/omar-raster/grails-app/services/org/ossim/omar/raster/GetTileLogService.groovy
@@ -0,0 +1,38 @@
+package org.ossim.omar.raster
+
+class GetTileLogService {
+    def getTileLoggingAppender
+
+    def fixParamsForRouting(def params){
+        def paramsSave = [:]
+
+        params.each {k, v ->
+            if ( params."${k}" != null )
+            {
+                paramsSave."${k}" = v
+            }
+        }
+        paramsSave["startDate"] = params.startDate //.toString()
+        paramsSave["endDate"] = params.endDate //.toString()
+
+        paramsSave
+    }
+    def logParams(def params) {
+        println 1
+        try{
+
+            def paramsSave = fixParamsForRouting(params)
+            def getTileLogRecord = new GetTileLog()
+            getTileLogRecord.properties = paramsSave
+
+            if ( getTileLogRecord.hasErrors() || !getTileLogRecord.save() )
+            {
+                getTileLogRecord.errors.allErrors.each { println it }
+            }
+        }
+        catch(def e)
+        {
+            println e
+        }
+    }
+}
