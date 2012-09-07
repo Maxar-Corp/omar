6a4883487887bdd8be5290f2b4f791a263ef3b83
gpotts <gpotts@b630af17-092b-0410-afba-a002956477a4>
2012-09-07 16:18:00 +0000

    Added more info to the output
    git-svn-id: http://svn.osgeo.org/ossim/omar@21654 b630af17-092b-0410-afba-a002956477a4

--- plugins/omar-stager/grails-app/services/org/ossim/omar/stager/RunScriptService.groovy
+++ plugins/omar-stager/grails-app/services/org/ossim/omar/stager/RunScriptService.groovy
@@ -13,15 +13,14 @@ class RunScriptService {
         for (int j = 0; j < names.length; j++)
         {
             def info = [:]
-
             def trigger = quartzScheduler.getTrigger(names[j], group);
             info.executing = trigger.timesTriggered?true:false
             info.startTime = trigger.startTime
-            info.endTime = trigger.endTime
-            info.name = trigger.name
+            info.endTime   = trigger.endTime
+            info.name      = trigger.name
+            info.jobName   = trigger.jobName
+            info.jobGroup  = trigger.jobGroup
             result << info
-        //    var currentJob = quartzScheduler.getJobDetail(names[j], group);
-        //    result.fullName = currentJob.fullName
         }
 
         result
