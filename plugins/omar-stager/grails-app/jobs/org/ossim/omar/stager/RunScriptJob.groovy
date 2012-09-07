07d70774ad529bddeba144f9ef661f11f07a2aba
dlucas <dlucas@b630af17-092b-0410-afba-a002956477a4>
2012-09-07 02:13:42 +0000

    git-svn-id: http://svn.osgeo.org/ossim/omar@21642 b630af17-092b-0410-afba-a002956477a4

---
+++ plugins/omar-stager/grails-app/jobs/org/ossim/omar/stager/RunScriptJob.groovy
@@ -0,0 +1,21 @@
+package org.ossim.omar.stager
+
+import org.quartz.JobDataMap
+
+class RunScriptJob {
+
+	def concurrent = false
+
+    static triggers = {}
+
+    def execute(org.quartz.JobExecutionContext context) {
+        JobDataMap dataMap = context.getMergedJobDataMap();
+  		
+  		println "Executing ${dataMap.commandLineScript}"
+  		def proc = dataMap.commandLineScript.execute();
+
+  	 	proc?.waitFor()
+  	 	
+  	 	println "Finished ${dataMap.commandLineScript}"
+    }
+}
