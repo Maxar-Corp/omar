9e1db2578621c20a127c91fad41e380fc025d5cd
gpotts <gpotts@b630af17-092b-0410-afba-a002956477a4>
2012-09-25 01:12:27 +0000

    Added DESCodec for cipher
    git-svn-id: http://svn.osgeo.org/ossim/omar@21769 b630af17-092b-0410-afba-a002956477a4

---
+++ plugins/omar-core/src/java/org/ossim/omar/core/DESCodec.groovy
@@ -0,0 +1,27 @@
+package org.ossim.omar.core
+
+import java.security.*
+import javax.crypto.*
+import javax.crypto.spec.*
+
+class DESCodec {
+    static encode = { String target ->
+        def cipher = getCipher(Cipher.ENCRYPT_MODE)
+        return cipher.doFinal(target.bytes).encodeBase64()
+    }
+
+    static decode = { String target ->
+        def cipher = getCipher(Cipher.DECRYPT_MODE)
+        return new String(cipher.doFinal(target.decodeBase64()))
+    }
+
+    private static getCipher(mode) {
+        def keySpec = new DESKeySpec(getPassword())
+        def cipher = Cipher.getInstance("DES")
+        def keyFactory = SecretKeyFactory.getInstance("DES")
+        cipher.init(mode, keyFactory.generateSecret(keySpec))
+        return cipher
+    }
+
+    private static getPassword() { "secret15".getBytes("UTF-8") }
+}
