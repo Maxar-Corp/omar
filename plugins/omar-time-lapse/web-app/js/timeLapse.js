1d558d1aa5e2d820c210caec1bf295adf1242e2e
kjnaquin <kjnaquin@b630af17-092b-0410-afba-a002956477a4>
2012-10-29 16:17:42 +0000

    General updates to the time lapse plugin. Still not at a functional state.
    git-svn-id: http://svn.osgeo.org/ossim/omar@21877 b630af17-092b-0410-afba-a002956477a4

--- plugins/omar-time-lapse/web-app/js/timeLapse.js
+++ plugins/omar-time-lapse/web-app/js/timeLapse.js
@@ -1,4 +1,6 @@
+var currentLayer; 
 var dom = YAHOO.util.Dom;
+var mapLayers = new Array();
 var map;
 
 $(document).ready(
@@ -24,29 +26,121 @@ $(document).ready(
 				layout.render();
 			}
 		);
+		pageSetup();
 		mapSetup();
-		initializeButtons();
+		currentLayer = imageIds.length - 1;
+		fastForward();
 	}
 );
 
-function mapSetup()
+function fastForward()
 {
-	var mapHeight = 0.8 * $("#pageContainer").height();
-	$("#map").css("height", mapHeight);
-
-	var mapWidth = 0.8 * $("#pageContainer").width();
-	$("#map").css("width", mapWidth);
-
-	map = new OpenLayers.Map("map");
+	mapLayers[currentLayer].setVisibility(false);
+	currentLayer++;
+	if (currentLayer > imageIds.length - 1)
+	{
+		currentLayer = 0;
+	}
+	mapLayers[currentLayer].setVisibility(true);
+	updateText();
 }
 
-function initializeButtons()
+function mapSetup()
 {
+	map = new OpenLayers.Map("map", 
+	{
+		numZoomLevels: 25
+	});
+
+	var baseLayer = new OpenLayers.Layer("Empty",
+	{
+		isBaseLayer: true
+	});
+	map.addLayer(baseLayer);	
+
+	var center = bbox.getCenterLonLat();
+	var zoom = map.getZoomForExtent(bbox);
+	map.setCenter(center, zoom);
+	
+	for (var i = 0; i < imageIds.length; i++)
+	{
+		mapLayers[i] = new OpenLayers.Layer.WMS
+		(
+			"Layer" + i,
+			$("#wmsUrlBase").html(),
+			{	
+				bands: "default",
+				brightness: 0,
+				contrast: 1,
+				interpolation: "bilinear",
+				layers: indexIds[i],
+				sharpen_mode: "none",
+				stretch_mode: "linear_auto_min_max",
+				stretch_mode_region: "viewport"
+			},
+			{
+				isBaseLayer: false,
+				singleTile: true
+			}
+		);
+		map.addLayer(mapLayers[i]);
+	}
+}
+
+function pageSetup()
+{
+	var mapHeight = 0.8 * $(window).height();
+	var mapWidth = 0.8 * $(window).width();
+
+	$("#imageIdHyperlink").css("width", mapWidth);
+	$("#imageIdHyperlink").css("text-align", "center");
+	$("#imageIdHyperlink").position
+	({
+		my: "middle top",
+		at: "middle top",
+		of: $(window),
+		offset: "0 25"
+	});
+
+	$("#acquisitionDateText").css("width", mapWidth);
+	$("#acquisitionDateText").css("text-align", "center");
+	$("#acquisitionDateText").position
+	({
+		my: "middle top",
+		at: "middle bottom",
+		of: $("#imageIdHyperlink"),
+		offset: "0 0"
+	});
+
+	$("#map").css("height", mapHeight);
+	$("#map").css("width", mapWidth);
+	$("#map").position
+	({
+		my: "middle top",
+		at: "middle bottom",
+		of: $("#acquisitionDateText"),
+		offset: "0 0"
+	});
+	
+	$("#slider").css("width", $("#map").width());
+	$("#slider").position
+	({
+		my: "middle top",
+		at: "middle bottom",
+		of: $("#map"),
+		offset: "0 0"
+	});
+	$("#slider").slider();
+
 	$("#fastForwardButton").button(
 	{
 		icons: {primary: "ui-icon-seek-next"},
 		text: false
-	});
+		}).click( function()
+		{
+			fastForward();
+		}
+	);
 	$("#playForwardButton").button(
 	{
 		icons: {primary: "ui-icon-triangle-1-e"},
@@ -67,4 +161,10 @@ function initializeButtons()
 		icons: {primary: "ui-icon-stop"},
 		text: false
 	});
+}
+
+function updateText()
+{
+	$("#imageIdHyperlink").html(imageIds[currentLayer]);
+	$("#acquisitionDateText").html(acquisitionDates[currentLayer]);
 }
