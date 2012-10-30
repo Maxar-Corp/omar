2e6bb6cbb68e7489f01ddbe5f973afc18f4ae823
sbortman <sbortman@b630af17-092b-0410-afba-a002956477a4>
2012-10-30 23:55:13 +0000

    Added gpars as a dependency. Removed plugin.xml from svn,  shouldn't be there.
    git-svn-id: http://svn.osgeo.org/ossim/omar@21878 b630af17-092b-0410-afba-a002956477a4

--- plugins/omar-image-magick/grails-app/conf/BuildConfig.groovy
+++ plugins/omar-image-magick/grails-app/conf/BuildConfig.groovy
@@ -26,6 +26,7 @@ grails.project.dependency.resolution = {
         // specify dependencies here under either 'build', 'compile', 'runtime', 'test' or 'provided' scopes eg.
 
         // runtime 'mysql:mysql-connector-java:5.1.5'
+	compile "org.codehaus.gpars:gpars:0.12"
     }
 
     plugins {
