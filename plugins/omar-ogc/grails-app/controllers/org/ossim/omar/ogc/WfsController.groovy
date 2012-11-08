4b6f32e655eb8450d8f06b12855d74523be43f6f
sbortman <sbortman@b630af17-092b-0410-afba-a002956477a4>
2012-11-08 23:39:55 +0000

    Whoops,  forgot this files!  See what hunger does?
    git-svn-id: http://svn.osgeo.org/ossim/omar@21925 b630af17-092b-0410-afba-a002956477a4

---
+++ plugins/omar-ogc/grails-app/controllers/org/ossim/omar/ogc/WfsController.groovy
@@ -0,0 +1,39 @@
+package org.ossim.omar.ogc
+
+import org.apache.commons.collections.map.CaseInsensitiveMap
+
+class WfsController
+{
+  def webFeatureService
+
+  def index()
+  {
+    def wfsParams = new CaseInsensitiveMap( params ).subMap(
+        ['service', 'version', 'request', 'typeName', 'filter']
+    )
+    def wfsCommand = new WfsCommand()
+
+    bindData( wfsCommand, wfsParams )
+
+    println wfsCommand
+
+    def results
+
+    switch ( wfsCommand.request?.toUpperCase() )
+    {
+    case "GETCAPABILITIES":
+      results = webFeatureService.getCapabilities( wfsCommand )
+      break
+    case "DESCRIBEFEATURETYPE":
+      results = webFeatureService.describeFeatureType( wfsCommand )
+      break
+    case "GETFEATURE":
+      results = webFeatureService.getFeature( wfsCommand )
+      break
+    default:
+      println "ERROR"
+    }
+
+    render contentType: 'application/xml', text: results
+  }
+}
