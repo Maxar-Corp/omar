e5d29fe4559d26c5c4d581ee037b84cb984e416c
dburken <dburken@b630af17-092b-0410-afba-a002956477a4>
2012-11-27 01:49:08 +0000

    Copied documentation from DataInfoTest.java header.
    git-svn-id: http://svn.osgeo.org/ossim/omar@21958 b630af17-092b-0410-afba-a002956477a4

--- apps/omar/getinfo.sh
+++ apps/omar/getinfo.sh
@@ -1,2 +1,43 @@
 #!/bin/sh
+
+#---
+# File: getinfo.sh
+#
+# Test script to call swig generated DataInfo.java class and associated
+# coms c++ class oms::DataInfo.  Basically if this runs your oms/joms wrappers for
+# omar should work. If given a valid image you should get and xml doc to standard
+# output with image info.
+#
+# Calls usually go back and forth like this:
+# omar<-->joms<-->oms<-->ossim
+#
+# Requires: joms.jar
+# unix/linux: liboms.so and libjoms.so
+# OSX: liboms.dylib and libjoms.dylib (could be in framework on OSX also)
+#
+# Use java args, -cp and -Djava.library.path as needed.
+# -cp is CLASSPATH to joms.jar
+# -Djava.library.path points to directory where libjoms.so or .dylib resides.
+#
+# Typically:
+#
+# joms.jar is placed in omar/plugins/omar-oms/lib
+# libjoms.so or libjoms.dylib is in LD_LIBRARY_PATH or DYLD_LIBRARY_PATH
+# 
+# Usages:
+#
+# Both CLASSPATH and (DY)LD_LIBRARY_PATH set:
+# 
+# java org.ossim.oms.apps.DataInfoTest <image(s)>
+#
+# No CLASSPATH, (DY)LD_LIBRARY_PATH set:");
+#
+# java -cp $OMAR_HOME/../../plugins/omar-oms/lib/joms.jar org.ossim.oms.apps.DataInfoTest <image(s)>
+#
+# No CLASSPATH, No (DY)LD_LIBRARY_PATH set:");
+# Note: /path/to/lib should have libjoms.so or libjoms.dylib in it.");
+#
+# java -Djava.library.path=/path/to/lib -cp $OMAR_HOME/../../plugins/omar-oms/lib/joms.jar org.ossim.oms.apps.DataInfoTest <image(s)>
+# 
+# ---
 java -cp $OMAR_HOME/../../plugins/omar-oms/lib/joms.jar org.ossim.oms.apps.DataInfoTest $1
