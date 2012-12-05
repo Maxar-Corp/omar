252792735532568d872ac21279891bdef847db7c
Scott Bortman <scott.bortman@gmail.com>
2012-12-05 15:03:17 -0500

    Ignore invalid SHA when downloading jars.

--- plugins/geoscript/grails-app/conf/BuildConfig.groovy
+++ plugins/geoscript/grails-app/conf/BuildConfig.groovy
@@ -6,6 +6,8 @@ grails.project.test.class.dir = "target/test-classes"
 grails.project.test.reports.dir = "target/test-reports"
 //grails.project.war.file = "target/${appName}-${appVersion}.war"
 grails.project.dependency.resolution = {
+  checksums false
+
   // inherit Grails' default dependencies
   inherits( "global" ) {
     // uncomment to disable ehcache
