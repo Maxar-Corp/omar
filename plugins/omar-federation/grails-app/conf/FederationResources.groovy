81fe3d84a8051716b08b976910abc837a01d5234
Garrett Potts <potts@cfl.rr.com>
2012-12-05 15:30:51 -0500

    Adding federation resources

---
+++ plugins/omar-federation/grails-app/conf/FederationResources.groovy
@@ -0,0 +1,7 @@
+modules{
+    federationRasterSearch{
+        resource url: [plugin: 'underscore', dir: 'js/underscore', file:'underscore-1.4.2.js']
+        resource url: [plugin: 'backbonejs', dir: 'js', file: 'backbone.js']
+        //resource url: [dir: 'js', file: 'Test.js']
+    }
+}
