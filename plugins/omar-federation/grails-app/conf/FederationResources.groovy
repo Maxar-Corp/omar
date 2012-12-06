5e010b0dfcba4d1251b4b39734faece03af998a2
Garrett Potts <potts@cfl.rr.com>
2012-12-06 12:58:53 -0500

    Modifiying federation

--- plugins/omar-federation/grails-app/conf/FederationResources.groovy
+++ plugins/omar-federation/grails-app/conf/FederationResources.groovy
@@ -1,8 +1,8 @@
 modules{
-    federationRasterSearch{
-        resource url: [plugin: 'underscore', dir: 'js/underscore', file:'underscore-1.4.2.js']
-        resource url: [plugin: 'backbonejs', dir: 'js', file: 'json2.js']
-        resource url: [plugin: 'backbonejs', dir: 'js', file: 'backbone.js']
-        //resource url: [dir: 'js', file: 'Test.js']
-    }
+    //federationRasterSearch{
+    //    resource url: [plugin: 'underscore', dir: 'js/underscore', file:'underscore-1.4.2.js']
+    //    resource url: [plugin: 'backbonejs', dir: 'js', file: 'json2.js']
+    //   resource url: [plugin: 'backbonejs', dir: 'js', file: 'backbone.js']
+    //resource url: [dir: 'js', file: 'Test.js']
+    //}
 }
