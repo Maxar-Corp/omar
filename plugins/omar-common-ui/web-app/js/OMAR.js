bd6b8516d468c049c07507bfc8b155098513d6e2
Garrett Potts <potts@cfl.rr.com>
2012-12-12 14:20:46 -0500

    added a parser allocator for XML that works on all browsers.  Jquery is having problems

--- plugins/omar-common-ui/web-app/js/OMAR.js
+++ plugins/omar-common-ui/web-app/js/OMAR.js
@@ -125,3 +125,22 @@ OMAR.isIso8601 = function(value)
 
     return result;
 }
+
+
+OMAR.parseXml = null;
+
+if (typeof window.DOMParser != "undefined") {
+    OMAR.parseXml = function(xmlStr) {
+        return ( new window.DOMParser() ).parseFromString(xmlStr, "text/xml");
+    };
+} else if (typeof window.ActiveXObject != "undefined" &&
+    new window.ActiveXObject("Microsoft.XMLDOM")) {
+    OMAR.parseXml = function(xmlStr) {
+        var xmlDoc = new window.ActiveXObject("Microsoft.XMLDOM");
+        xmlDoc.async = "false";
+        xmlDoc.loadXML(xmlStr);
+        return xmlDoc;
+    };
+} else {
+    throw new Error("No XML parser found");
+}
