6bffdec86f90f5c5a4cfda8bad65cc8742761042
Garrett Potts <potts@cfl.rr.com>
2012-12-13 15:38:12 -0500

    Removing all code from gsp

--- plugins/omar-federation/web-app/js/rasterSearch.js
+++ plugins/omar-federation/web-app/js/rasterSearch.js
@@ -155,3 +155,52 @@ OMAR.pages.FederatedRasterSearch = (function($){
     //alert($(temp).find('id'));
     return result;
 });
+
+$(document).ready(function () {
+
+    // OUTER-LAYOUT
+    $('body').layout({
+        center__paneSelector:	".outer-center"
+        ,	west__paneSelector:		".outer-west"
+        ,	east__paneSelector:		".outer-east"
+        ,	west__size:				125
+        ,	east__size:				125
+        ,	spacing_open:			8  // ALL panes
+        ,	spacing_closed:			12 // ALL panes
+        //,	north__spacing_open:	0
+        //,	south__spacing_open:	0
+        ,	north__maxSize:			50
+        ,   north__minSize:         50
+        ,	south__maxSize:			50
+        ,   south__minSize:         50
+
+        // MIDDLE-LAYOUT (child of outer-center-pane)
+        ,	center__childOptions: {
+            center__paneSelector:	".middle-center"
+            ,	west__paneSelector:		".middle-west"
+            ,	east__paneSelector:		".middle-east"
+            ,	west__size:				100
+            ,	east__size:				100
+            ,	spacing_open:			8  // ALL panes
+            ,	spacing_closed:			12 // ALL panes
+
+            // INNER-LAYOUT (child of middle-center-pane)
+            ,	center__childOptions: {
+                center__paneSelector:	".inner-center"
+                ,	west__paneSelector:		".inner-west"
+                ,	east__paneSelector:		".inner-east"
+                ,	west__size:				225
+                ,   west__minSize:          225
+                ,	east__size:				75
+                ,	spacing_open:			8  // ALL panes
+                ,	spacing_closed:			8  // ALL panes
+                ,	west__spacing_closed:	12
+                ,	east__spacing_closed:	12
+            }
+        }
+    });
+
+    var searchPageController = new OMAR.pages.FederatedRasterSearch(jQuery);
+    searchPageController.render();
+
+});
