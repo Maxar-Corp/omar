95d7adbf99282bf29cfd63b1711698dc8d3e0a98
Garrett Potts <potts@cfl.rr.com>
2012-12-18 13:25:50 -0500

    Added validation

--- plugins/omar-common-ui/web-app/js/BboxModelView.js
+++ plugins/omar-common-ui/web-app/js/BboxModelView.js
@@ -16,6 +16,23 @@ OMAR.models.BBOX = Backbone.Model.extend(
     },
     validate:function(attrs)
     {
+        if(attrs.minx&&(!OMAR.isFloat(attrs.minx)))
+        {
+            return ("Minx value is invalid: " + attrs.minx);
+        }
+        if(attrs.miny&&(!OMAR.isFloat(attrs.miny)))
+        {
+            return ("Miny value is invalid: " + attrs.miny);
+        }
+        if(attrs.maxx&&(!OMAR.isFloat(attrs.maxx)))
+        {
+            return ("Maxx value is invalid: " + attrs.maxx);
+        }
+        if(attrs.maxy&&(!OMAR.isFloat(attrs.maxy)))
+        {
+            return ("Maxy value is invalid: " + attrs.maxy);
+        }
+        return null;
     },
     setFromWmsString:function(s)
     {
@@ -79,6 +96,7 @@ OMAR.views.BBOX = Backbone.View.extend({
             this.model.set({minx:values[1],miny:values[0]});
             this.model.on("change", this.bboxModelChange, this);
         }
+        this.render();
     },
     urOnChange: function(e){
         var v = this.upperRightBboxEl.val();
@@ -89,6 +107,7 @@ OMAR.views.BBOX = Backbone.View.extend({
             this.model.set({maxx:values[1],maxy:values[0]});
             this.model.on("change", this.bboxModelChange, this);
         }
+        this.render();
     },
     bboxError:function(model, err){
     },
