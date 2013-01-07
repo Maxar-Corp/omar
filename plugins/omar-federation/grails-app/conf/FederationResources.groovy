e0582ff7f1ccfe94de25705386835df17de6a585
Garrett Potts <potts@cfl.rr.com>
2013-01-07 10:18:02 -0500

    Fixing styling

--- plugins/omar-federation/grails-app/conf/FederationResources.groovy
+++ plugins/omar-federation/grails-app/conf/FederationResources.groovy
@@ -1,8 +1,5 @@
 modules = {
-	federationRasterSearch{
-resource url: [dir: 'css', file: 'main.css']
-resource url: [dir: 'css', file: 'omar-2.0.css']
-
+    federationRasterSearch{
         resource url: [plugin: "omar-core", dir: "js/prototype", file: "prototype.js"]
         resource url: [plugin: 'omar-common-ui', dir: 'js', file:'OMAR.js']
         resource url: [plugin: 'omar-core', dir: 'js', file: 'jquery.js']
@@ -14,22 +11,18 @@ resource url: [dir: 'css', file: 'omar-2.0.css']
         resource url: [plugin: 'omar-common-ui', dir: 'js', file: 'BboxModelView.js']
         resource url: [plugin: 'omar-common-ui', dir: 'js', file: 'PointModelView.js']
         resource url: [plugin: 'omar-ogc', dir: 'js', file: 'WfsModel.js']
-
-    resource url: [plugin: 'openlayers', dir: 'css', file: 'mapwidget2.css']
-      
         resource url: [plugin: 'openlayers', dir: 'js/theme/default', file: 'style.css']
         resource url: [plugin: 'openlayers', dir: 'js', file: 'OpenLayers.js']
         resource url: [dir: 'js', file: 'spin-min.js', plugin: 'omar-core']
         // css resources for jquery
-		resource url: [plugin: 'omar-common-ui', dir: 'css/jQuery', file: 'layout-default-latest.css']
-		resource url: [plugin: 'omar-common-ui', dir: 'css/jQuery/smoothness', file: 'jquery-ui-1.9.2.custom.css']
-		resource url: [plugin: 'omar-common-ui', dir: 'css/jQuery', file: 'jquery-ui-timepicker-addon.css']
+        resource url: [plugin: 'omar-common-ui', dir: 'css/jQuery', file: 'layout-default-latest.css']
+        resource url: [plugin: 'omar-common-ui', dir: 'css/jQuery/smoothness', file: 'jquery-ui-1.9.2.custom.css']
+        resource url: [plugin: 'omar-common-ui', dir: 'css/jQuery', file: 'jquery-ui-timepicker-addon.css']
 
         resource url: [plugin: 'omar-common-ui', dir: 'js/jQuery', file: 'jquery-ui-timepicker-addon.js']
         resource url: [plugin: 'omar-common-ui', dir: 'js', file: 'DateModelView.js']
 
         resource url: [plugin: 'omar-common-ui', dir: 'js', file: 'MapModelView.js']
-        
         resource url: [dir: 'js', file: 'ServerListModelView.js']
         resource url: [plugin: 'omar-common-ui', dir: 'images', file: 'calendar.gif']
         resource url: [dir: 'css', file: 'rasterSearch.css']
