e8ca8d31d148f304bad9c49eecc667161a5e1f6c
Garrett Potts <potts@cfl.rr.com>
2013-02-06 13:24:12 -0500

    Fixed thumbnail problem with parsing extension

--- apps/omar/grails-app/controllers/org/ossim/omar/app/ThumbnailController.groovy
+++ apps/omar/grails-app/controllers/org/ossim/omar/app/ThumbnailController.groovy
@@ -9,187 +9,189 @@ import org.ossim.omar.core.HttpStatus
 import org.ossim.omar.core.ImageGenerator
 import org.ossim.omar.raster.RasterEntry
 import org.ossim.omar.video.VideoDataSet
+import org.apache.commons.io.FilenameUtils
 
 class ThumbnailController implements InitializingBean
 {
 
-  def grailsApplication
-  def thumbnailService
-  def nullImage = new BufferedImage( 128, 128, BufferedImage.TYPE_INT_RGB );
+    def grailsApplication
+    def thumbnailService
+    def nullImage = new BufferedImage( 128, 128, BufferedImage.TYPE_INT_RGB );
 
 
-  def list()
-  {
-    try
+    def list()
     {
-      response.contentType = "image/jpeg"
-      ImageIO.write( nullImage, "jpeg", response.outputStream )
-    }
-    catch ( Exception e )
-    {
-
-    }
-  }
-
-  def show()
-  {
-    try
-    {
-      def httpStatusMessage = new HttpStatusMessage()
-      httpStatusMessage.status = HttpStatus.OK
-      def rasterEntry = RasterEntry.compositeId( params.id as String).findWhere()
-      def image = null
-      def mimeType = "image/jpeg"
-      File outputFile
-
-      if ( !rasterEntry )
-      {
-        httpStatusMessage.message = "RasterEntry not found with id ${params.id}"
-        httpStatusMessage.status = HttpStatus.NOT_FOUND
-        image = ImageGenerator.createErrorImage( 128, 128, "Raster Image Not\nFound" );
-        log.error( httpStatusMessage )
-      }
-      else
-      {
-        //params.mimeType = mimeType
-        outputFile = thumbnailService.getRasterEntryThumbnailFile( httpStatusMessage, rasterEntry, params )
-        // if ( ( httpStatusMessage.status == HttpStatus.OK ) &&
-        //         outputFile?.exists() &&
-        //         ( outputFile.length() > 0 ) )
-        // {
-        //   image = ImageIO.read( outputFile )
-        //}
-        // if ( !image )
-        if ( httpStatusMessage.status != HttpStatus.OK )
+        try
         {
-          image = ImageGenerator.createErrorImage( 128, 128, "\nUnable to\n produce\n thumbnail.\n\nNo overviews\npresent.\n" );
-
+            response.contentType = "image/jpeg"
+            ImageIO.write( nullImage, "jpeg", response.outputStream )
         }
-      }
-
-      httpStatusMessage.initializeResponse( response )
-
-      if ( outputFile?.exists() )
-      {
-        def tempFile = outputFile as String
-        def ext = tempFile.substring( 0, tempFile.lastIndexOf( '.' ) )
-        ext = ext.toLowerCase()
-        switch ( ext )
+        catch ( Exception e )
         {
-        case 'jpeg':
-          mimeType = "image/jpeg"
-          break;
-        default:
-          mimeType = "image/${ext}"
-          break;
+
         }
-        def bytes = outputFile.bytes
-        response.contentType = mimeType
-        response.contentLength = bytes.size()
-        response.outputStream << bytes
-      }
-      else if ( image )
-      {
-        response.contentType = "image/jpeg"
-        ImageIO.write( image, "jpeg", response.outputStream )
-      }
-
-    }
-    catch ( Exception e )
-    {
-      println( e.message )
-      log.error( "exception ${e.message}" )
     }
 
-    return null
-  }
-
-
-  def frame()
-  {
-    try
+    def show()
     {
-      def videoDataSet = VideoDataSet.findByIndexId( params.id ) ?: VideoDataSet.get( params.id );
-      def image = (Image)null
-      def mimeType = "image/jpeg"
-      def httpStatusMessage = new HttpStatusMessage()
-      httpStatusMessage.status = HttpStatus.OK
-
-      if ( !videoDataSet )
-      {
-        httpStatusMessage.message = "VideoDataSet not found with id ${params.id}"
-        httpStatusMessage.status = HttpStatus.NOT_FOUND
-      }
-      else
-      {
-        File outputFile = thumbnailService.getVideoDataSetThumbnailFile( httpStatusMessage, videoDataSet, params )
-
-        if ( !outputFile.exists() )
+        try
         {
-          httpStatusMessage.message = "Unable to write thumbnail for video dataset ${videoDataSet.getMainFile()}"
-          httpStatusMessage.status = HttpStatus.NOT_FOUND
+            def httpStatusMessage = new HttpStatusMessage()
+            httpStatusMessage.status = HttpStatus.OK
+            def rasterEntry = RasterEntry.compositeId( params.id as String).findWhere()
+            def image = null
+            def mimeType = "image/jpeg"
+            File outputFile
+
+            if ( !rasterEntry )
+            {
+                httpStatusMessage.message = "RasterEntry not found with id ${params.id}"
+                httpStatusMessage.status = HttpStatus.NOT_FOUND
+                image = ImageGenerator.createErrorImage( 128, 128, "Raster Image Not\nFound" );
+                log.error( httpStatusMessage )
+            }
+            else
+            {
+                //params.mimeType = mimeType
+                outputFile = thumbnailService.getRasterEntryThumbnailFile( httpStatusMessage, rasterEntry, params )
+                // if ( ( httpStatusMessage.status == HttpStatus.OK ) &&
+                //         outputFile?.exists() &&
+                //         ( outputFile.length() > 0 ) )
+                // {
+                //   image = ImageIO.read( outputFile )
+                //}
+                // if ( !image )
+                if ( httpStatusMessage.status != HttpStatus.OK )
+                {
+                    image = ImageGenerator.createErrorImage( 128, 128, "\nUnable to\n produce\n thumbnail.\n\nNo overviews\npresent.\n" );
+
+                }
+            }
+
+            httpStatusMessage.initializeResponse( response )
+
+            if ( outputFile?.exists() )
+            {
+                def tempFile = outputFile as String
+                def ext = FilenameUtils.getExtension(tempFile)//tempFile.substring( 0, tempFile.lastIndexOf( '.' ) )
+                ext = ext.toLowerCase()
+                switch ( ext )
+                {
+                    case 'jpg':
+                    case 'jpeg':
+                        mimeType = "image/jpeg"
+                        break;
+                    default:
+                        mimeType = "image/${ext}"
+                        break;
+                }
+                def bytes = outputFile.bytes
+                response.contentType = mimeType
+                response.contentLength = bytes.size()
+                response.outputStream << bytes
+            }
+            else if ( image )
+            {
+                response.contentType = "image/jpeg"
+                ImageIO.write( image, "jpeg", response.outputStream )
+            }
+
         }
-        // check the status for errors
-        if ( ( httpStatusMessage.status == HttpStatus.OK ) )
+        catch ( Exception e )
         {
-          image = ImageIO.read( outputFile )
+            println( e.message )
+            log.error( "exception ${e.message}" )
         }
-      }
-      if ( httpStatusMessage.status != HttpStatus.OK )
-      {
-        if ( !image )
+
+        return null
+    }
+
+
+    def frame()
+    {
+        try
         {
-          image = ImageGenerator.createErrorImage( 128, 128 );
+            def videoDataSet = VideoDataSet.findByIndexId( params.id ) ?: VideoDataSet.get( params.id );
+            def image = (Image)null
+            def mimeType = "image/jpeg"
+            def httpStatusMessage = new HttpStatusMessage()
+            httpStatusMessage.status = HttpStatus.OK
+
+            if ( !videoDataSet )
+            {
+                httpStatusMessage.message = "VideoDataSet not found with id ${params.id}"
+                httpStatusMessage.status = HttpStatus.NOT_FOUND
+            }
+            else
+            {
+                File outputFile = thumbnailService.getVideoDataSetThumbnailFile( httpStatusMessage, videoDataSet, params )
+
+                if ( !outputFile.exists() )
+                {
+                    httpStatusMessage.message = "Unable to write thumbnail for video dataset ${videoDataSet.getMainFile()}"
+                    httpStatusMessage.status = HttpStatus.NOT_FOUND
+                }
+                // check the status for errors
+                if ( ( httpStatusMessage.status == HttpStatus.OK ) )
+                {
+                    image = ImageIO.read( outputFile )
+                }
+            }
+            if ( httpStatusMessage.status != HttpStatus.OK )
+            {
+                if ( !image )
+                {
+                    image = ImageGenerator.createErrorImage( 128, 128 );
+                }
+            }
+            httpStatusMessage.initializeResponse( response )
+            response.contentType = "image/jpeg"
+            ImageIO.write( image, "jpeg", response.outputStream )
         }
-      }
-      httpStatusMessage.initializeResponse( response )
-      response.contentType = "image/jpeg"
-      ImageIO.write( image, "jpeg", response.outputStream )
-    }
-    catch ( Exception e )
-    {
-      println "Error: ${e.message}"
-    }
-  }
-
-  def asHTML()
-  {
-    def size = params.size
-
-    if ( !size )
-    {
-      grailsApplication.config.thumbnail.defaultSize
-    }
-
-    render( contentType: "text/html" ) {
-      html {
-        body {
-          img( src: createLink( action: "show", id: params.id, params: params ), width: size, height: size )
+        catch ( Exception e )
+        {
+            println "Error: ${e.message}"
         }
-      }
-    }
-  }
-
-  def proxy()
-  {
-
-    def url = "${params.url}"
-
-    params.each {
-      url += "&${it}"
     }
 
-    println url
+    def asHTML()
+    {
+        def size = params.size
 
-    def text = url.toURL().text
+        if ( !size )
+        {
+            grailsApplication.config.thumbnail.defaultSize
+        }
 
-    println text
+        render( contentType: "text/html" ) {
+            html {
+                body {
+                    img( src: createLink( action: "show", id: params.id, params: params ), width: size, height: size )
+                }
+            }
+        }
+    }
 
-    render( contentType: "text/xml", text: text )
-  }
+    def proxy()
+    {
 
-  public void afterPropertiesSet()
-  {
-    //To change body of implemented methods use File | Settings | File Templates.
-  }
+        def url = "${params.url}"
+
+        params.each {
+            url += "&${it}"
+        }
+
+        println url
+
+        def text = url.toURL().text
+
+        println text
+
+        render( contentType: "text/xml", text: text )
+    }
+
+    public void afterPropertiesSet()
+    {
+        //To change body of implemented methods use File | Settings | File Templates.
+    }
 }
