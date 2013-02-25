cbf481a1ebbb2ba17f7ab8a43b5b1292fbfdba28
Scott Bortman <scott.bortman@gmail.com>
2013-02-25 09:05:35 -0500

    Refreshed the FilterPane dependency

--- plugins/filterpane/grails-app/conf/UrlMappings.groovy
+++ plugins/filterpane/grails-app/conf/UrlMappings.groovy
@@ -1,13 +1,14 @@
-class UrlMappings {
+class UrlMappings
+{
 
-	static mappings = {
-		"/$controller/$action?/$id?"{
-			constraints {
-				// apply constraints here
-			}
-		}
+  static mappings = {
+    "/$controller/$action?/$id?" {
+      constraints {
+        // apply constraints here
+      }
+    }
 
-		"/"(view:"/index")
-		"500"(view:'/error')
-	}
+    "/"( view: "/index" )
+    "500"( view: '/error' )
+  }
 }
--- plugins/filterpane/test/integration/com/zeddware/grails/plugins/filterpane/FilterUtilsTests.groovy
+++ plugins/filterpane/test/integration/com/zeddware/grails/plugins/filterpane/FilterUtilsTests.groovy
@@ -2,41 +2,47 @@ package com.zeddware.grails.plugins.filterpane
 
 import grails.test.*
 
-class FilterUtilsTests extends GrailsUnitTestCase {
+class FilterUtilsTests extends GrailsUnitTestCase
+{
 
-	def params
-	Date testDate
+  def params
+  Date testDate
 
-    protected void setUp() {
-        super.setUp()
-		testDate = java.sql.Timestamp.valueOf('2010-07-26 20:38:15.000')
+  protected void setUp()
+  {
+    super.setUp()
+    testDate = java.sql.Timestamp.valueOf( '2010-07-26 20:38:15.000' )
 
-		params = [testDate:testDate,
-			testDate_year:'2010',
-			testDate_month:'7',
-			testDate_day:'26',
-			testDate_hour:'20',
-			testDate_minute:'38',
-			testDate_second:'15'
-		]
-    }
+    params = [testDate: testDate,
+        testDate_year: '2010',
+        testDate_month: '7',
+        testDate_day: '26',
+        testDate_hour: '20',
+        testDate_minute: '38',
+        testDate_second: '15'
+    ]
+  }
 
-    protected void tearDown() {
-        super.tearDown()
-    }
+  protected void tearDown()
+  {
+    super.tearDown()
+  }
 
-    // parseDateFromDatePickerParams tests
-	void testParseFromDate() {
-		assertNotNull('value was null', FilterUtils.parseDateFromDatePickerParams('testDate', params))
-	}
+  // parseDateFromDatePickerParams tests
+  void testParseFromDate()
+  {
+    assertNotNull( 'value was null', FilterUtils.parseDateFromDatePickerParams( 'testDate', params ) )
+  }
 
-	void testParseFromToString() {
-		params.testDate = params.testDate.toString()
-		assertNotNull('value was null', FilterUtils.parseDateFromDatePickerParams('testDate', params))
-	}
+  void testParseFromToString()
+  {
+    params.testDate = params.testDate.toString()
+    assertNotNull( 'value was null', FilterUtils.parseDateFromDatePickerParams( 'testDate', params ) )
+  }
 
-	void testParseFromStruct() {
-		params.testDate = 'struct'
-		assertNotNull('value was null', FilterUtils.parseDateFromDatePickerParams('testDate', params))
-	}
+  void testParseFromStruct()
+  {
+    params.testDate = 'struct'
+    assertNotNull( 'value was null', FilterUtils.parseDateFromDatePickerParams( 'testDate', params ) )
+  }
 }
