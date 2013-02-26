bbb7198f27a399589930a5e5db9d07e886cfe174
Scott Bortman <scott.bortman@gmail.com>
2013-02-25 19:14:49 -0500

    Cleaned up the the WfsController Added util methods to WfsCommand to marshall to/from XML for HTTP POSTs

--- plugins/omar-ogc/grails-app/controllers/org/ossim/omar/ogc/WfsCommand.groovy
+++ plugins/omar-ogc/grails-app/controllers/org/ossim/omar/ogc/WfsCommand.groovy
@@ -1,6 +1,10 @@
 package org.ossim.omar.ogc
 
-import org.apache.commons.lang.builder.ToStringBuilder
+import grails.validation.Validateable
+import groovy.transform.EqualsAndHashCode
+import groovy.transform.ToString
+import groovy.util.slurpersupport.GPathResult
+import groovy.xml.StreamingMarkupBuilder
 
 /**
  * Created with IntelliJ IDEA.
@@ -9,76 +13,139 @@ import org.apache.commons.lang.builder.ToStringBuilder
  * Time: 10:36 PM
  * To change this template use File | Settings | File Templates.
  */
+@Validateable
+@ToString( includeNames = true, includeFields = true, excludes = 'errors,dateCreated,lastUpdated,metaClass' )
+@EqualsAndHashCode
+
 class WfsCommand
 {
-    String service
-    String version
-    String request
-    String typeName
-    String filter
+  String service
+  String version
+  String request
+  String typeName
+  String filter
 
-    String outputFormat
-    Integer maxFeatures
-    Integer offset
-    String resultType
-    String sortBy
+  String outputFormat
+  Integer maxFeatures
+  Integer offset
+  String resultType
+  String sortBy
 
-    /**
-     * Google earth is not replacing their variables as described by the
-     * SPEC so I can't add a replacement variable in the filter to adhere
-     * to the WFS SPEC.  We have to support floating BBOX for now when doing
-     * KML Network Links
-     */
-    String bbox
-    @Override
-    String toString()
-    {
-        return ToStringBuilder.reflectionToString( this )
+  /**
+   * Google earth is not replacing their variables as described by the
+   * SPEC so I can't add a replacement variable in the filter to adhere
+   * to the WFS SPEC.  We have to support floating BBOX for now when doing
+   * KML Network Links
+   */
+  String bbox
+
+  def toQuery()
+  {
+    def wfsParams = ( this.metaClass.properties.name - ['class', 'metaClass'] ).inject( [:] ) { a, b ->
+      if ( this[b] )
+      {
+        a[b] = this[b]
+      }
+      return a
     }
 
-    def toQuery()
+    def query = wfsParams?.collect { k, v -> "${ k }=${ URLEncoder.encode( v as String, 'UTF-8' ) }" }?.join( '&' )
+
+    //println query
+
+    return query
+  }
+
+  def convertSortByToArray()
+  {
+    def result = [];
+
+
+    if ( !sortBy )
     {
-        def wfsParams = ( this.metaClass.properties.name - ['class', 'metaClass'] ).inject( [:] ) { a, b ->
-            if ( this[b] )
-            {
-                a[b] = this[b]
-            }
-            return a
+      return null
+    };
+    def arrayOfValues = sortBy.split( "," )
+    def idx = 0;
+    arrayOfValues.each { element ->
+      def splitParam = element.split( "\\+" );
+      if ( splitParam.length == 1 )
+      {
+        result << [splitParam]
+      }
+      else
+      {
+        if ( splitParam[1].toLowerCase() == "a" )
+        {
+          result << [splitParam[0], "ASC"]
         }
-
-        def query = wfsParams?.collect { k, v -> "${ k }=${ URLEncoder.encode( v as String, 'UTF-8' ) }" }.join( '&' )
-
-        //println query
-
-        return query
-    }
-    def convertSortByToArray()
-    {
-        def result = [];
-
-
-        if(!sortBy) return null;
-        def arrayOfValues = sortBy.split(",")
-        def idx = 0;
-        arrayOfValues.each{element->
-            def splitParam = element.split("\\+");
-            if (splitParam.length == 1)
-            {
-                result << [splitParam]
-            }
-            else
-            {
-                if (splitParam[1].toLowerCase() == "a")
-                {
-                    result << [splitParam[0], "ASC"]
-                }
-                else
-                {
-                    result << [splitParam[0], "DESC"]
-                }
-            }
+        else
+        {
+          result << [splitParam[0], "DESC"]
         }
-        result;
+      }
+    }
+    result;
+  }
+
+  static WfsCommand fromXML(String xmlText)
+  {
+    fromXML( new XmlSlurper().parseText( xmlText ) )
+  }
+
+  static WfsCommand fromXML(GPathResult xml)
+  {
+    def params = [
+        service: xml.@service.text(),
+        version: xml.@version.text(),
+        request: xml.name(),
+    ]
+
+    switch ( params.request )
+    {
+    case 'DescribeFeatureType':
+      params.typeName = xml.TypeName.text()
+      break
+    case 'GetFeature':
+      params.with {
+        typeName = xml.Query.collect { it.@typeName.text() }?.first()
+        filter = xml.Query.collect { new StreamingMarkupBuilder().bindNode( it.Filter ).toString().trim() }?.first()
+      }
+      break
     }
 
+    new WfsCommand( params )
+  }
+
+  String toXML()
+  {
+    def x = {
+      mkp.declareNamespace( xsi: "http://www.w3.org/2001/XMLSchema-instance" )
+
+      switch ( request )
+      {
+      case 'GetCapabilities':
+        "${request}"( service: service, version: version, xmlns: "http://www.opengis.net/wfs",
+            'xsi:schemaLocation': "http://www.opengis.net/wfs  http://schemas.opengis.net/wfs/1.1.0/wfs.xsd" )
+        break
+      case 'DescribeFeatureType':
+        "${request}"( service: service, version: version, xmlns: "http://www.opengis.net/wfs",
+            'xsi:schemaLocation': "http://www.opengis.net/wfs  http://schemas.opengis.net/wfs/1.1.0/wfs.xsd" ) {
+          TypeName( typeName )
+        }
+        break
+      case 'GetFeature':
+        mkp.declareNamespace( ogc: "http://www.opengis.net/ogc" )
+        "${request}"( service: service, version: version, xmlns: "http://www.opengis.net/wfs",
+            'xsi:schemaLocation': "http://www.opengis.net/wfs  http://schemas.opengis.net/wfs/1.1.0/wfs.xsd" ) {
+          Query( typeName: typeName ) {
+            mkp.yieldUnescaped filter
+          }
+        }
+        break
+      }
+    }
+
+    new StreamingMarkupBuilder().bind( x ).toString()
+  }
 }
--- plugins/omar-ogc/grails-app/controllers/org/ossim/omar/ogc/WfsController.groovy
+++ plugins/omar-ogc/grails-app/controllers/org/ossim/omar/ogc/WfsController.groovy
@@ -14,6 +14,7 @@ class WfsController
   {
     def results, contentType
     def wfsCommand = new WfsCommand()
+
     try
     {
       //println request.method
@@ -21,53 +22,25 @@ class WfsController
       switch ( request.method.toUpperCase() )
       {
       case "POST":
-        def builder = new StreamingMarkupBuilder()
-
-        //println "POST: ${ builder.bind { mkp.yield request.XML } }"
-
-        wfsCommand.service = request.XML.@service
-        wfsCommand.version = request.XML.@version
-        wfsCommand.request = request.XML.name()
-
-        if ( wfsCommand.request?.toUpperCase() == "DESCRIBEFEATURETYPE" )
-        {
-          wfsCommand.typeName = request.XML.TypeName.text()
-        }
-        else if ( wfsCommand.request?.toUpperCase() == "GETFEATURE" )
-        {
-          def foo = new StreamingMarkupBuilder().bindNode( request.XML ).toString()
-          def x = new XmlSlurper().parseText( foo )
-
-          def max =  x.@maxFeatures.text()
-          def start =  x.@offset?.text()
-
-          wfsCommand.with {
-            service = x.@service?.text() ?: "WFS"
-            version = x.@version?.text() ?: "1.0.0"
-            request = x.name() ?: request ?: "GetFeature"
-            typeName = x.Query.collect { it.@typeName.text() }?.first()
-            filter = x.Query.collect { new StreamingMarkupBuilder().bindNode( it.Filter ).toString().trim() }?.first()
-
-            maxFeatures = ( max ) ? max.toInteger() : 1000
-            offset = ( start ) ? start.toInteger() : 0
-            outputFormat = x.@outputFormat?.text() ?: "GML2"
-          }
-        }
+        wfsCommand = WfsCommand.fromXML( request.XML )
         break
 
       case "GET":
-          def tempParams = new CaseInsensitiveMap( params )
+        wfsCommand = new WfsCommand()
 
-          def wfsParams = tempParams.subMap(
-                  ['service', 'version', 'request', 'resultType', 'typeName', 'filter', 'outputFormat', 'bbox', 'maxFeatures', 'offset','sortBy']
-          )
-          bindData( wfsCommand, wfsParams )
-          if((!wfsCommand.maxFeatures)||(wfsCommand.maxFeatures.toInteger() > 1000))
-          {
-              wfsCommand.maxFeatures = 1000
-          }
+        def wfsParams = new CaseInsensitiveMap( params ).subMap(
+            wfsCommand.properties.keySet()
+        )
 
-          break
+        bindData( wfsCommand, wfsParams )
+
+
+        if ( ( !wfsCommand.maxFeatures ) || ( wfsCommand.maxFeatures.toInteger() > 1000 ) )
+        {
+          wfsCommand.maxFeatures = 1000
+        }
+
+        break
       }
 
       //println wfsCommand
@@ -82,9 +55,10 @@ class WfsController
         break
       case "GETFEATURE":
         (results, contentType) = webFeatureService.getFeature( wfsCommand );
-          if (params.callback){
-              results="${params.callback}(${results});";
-          }
+        if ( params.callback )
+        {
+          results = "${params.callback}(${results});";
+        }
         break
       default:
         throw new Exception( "Unsupported Operation: ${ wfsCommand.request }" )
@@ -111,10 +85,10 @@ class WfsController
     }
     finally
     {
-        def attachment ="WFS-${wfsCommand.outputFormat}"
-        response.setHeader( "Content-disposition", "attachment; ${attachment}" )
-        // println "contentType: ${contentType}, text: ${results}"
-        render contentType: contentType, text: results
+      def attachment = "WFS-${wfsCommand.outputFormat}"
+      response.setHeader( "Content-disposition", "attachment; ${attachment}" )
+      // println "contentType: ${contentType}, text: ${results}"
+      render contentType: contentType, text: results
     }
   }
 }
