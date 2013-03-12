3b1421759b5df027fd6bf0f19da0eb60dd0fd396
Garrett Potts <potts@cfl.rr.com>
2013-03-12 13:39:12 -0400

    Alter rules for date in raster_entry

---
+++ apps/omar/grails-app/migrations/alter_timestamp.sql
@@ -0,0 +1,5 @@
+set timezone TO UTC;
+alter table raster_entry alter COLUMN ingest_date type timestamp with time zone;
+alter table raster_entry alter COLUMN acquisition_date type timestamp with time zone;
+alter table raster_entry alter COLUMN access_date type timestamp with time zone;
+alter table raster_entry alter COLUMN receive_date type timestamp with time zone;
