40a0541ad73cc4193d5c4d98c00d1950f422a079
Garrett Potts <potts@cfl.rr.com>
2013-03-23 17:36:17 -0400

    Fixed errors in the config

--- support/omarConfig.groovy
+++ support/omarConfig.groovy
@@ -1,36 +1,37 @@
-//grails.serverIP=
-//grails.serverURL=
-//wms.referenceDataDirectory=
+//omar.serverIP=
+//omar.serverURL=
+//wms.referenceDataDirectory=""
 //wms.mapFile=
 
 thumbnail.cacheDir="${wms.referenceDataDirectory}/omar-cache"
 //videoStreaming.flashDirRoot=
-image.download.prefix = "http://${grails.serverIP}"
+image.download.prefix = "http://${omr.serverIP}"
 
 wms.base.defaultOptions = [isBaseLayer: true, buffer: 0, transitionEffect: "resize"]
 wms.data.mapFile = "${wms.referenceDataDirectory}/bmng.map"
 wms.supportIE6=true
 
 wms.base.layers = [
-            [
-                    url: "http://${grails.serverIP}/cgi-bin/mapserv.sh?map=${wms.mapFile}",
-                    params: [layers: "Reference", format: "image/jpeg"],
-                    name: "Reference Data",
-                    options: defaultOptions
-            ]
+        [
+                url: "http://${omar.serverIP}/cgi-bin/mapserv.exe?map=${wms.mapFile}",
+                params: [layers: "Reference", format: "image/jpeg"],
+                name: "Reference Data",
+                options: wms.base.defaultOptions
+        ]
 
-    ]
+]
+
 
 wms.data.raster = [
         url: "${ omar.serverURL }/wms/footprints",
-        params: [layers: ( supportIE6 ) ? "Imagery" : "ImageData", format: ( supportIE6 ) ? "image/gif" : "image/png"],
+        params: [layers: ( wms.supportIE6) ? "Imagery" : "ImageData", format: ( wms.supportIE6) ? "image/gif" : "image/png"],
         name: "OMAR Imagery Coverage",
         options: [styles: "byFileType", footprintLayers: "Imagery"]
 ]
 
 wms.data.video = [
         url: "${ omar.serverURL }/wms/footprints",
-        params: [layers: ( supportIE6 ) ? "Videos" : "VideoData", format: ( supportIE6 ) ? "image/gif" : "image/png"],
+        params: [layers: ( wms.supportIE6) ? "Videos" : "VideoData", format: ( wms.supportIE6) ? "image/gif" : "image/png"],
         name: "OMAR Video Coverage",
         options: [styles: "red", footprintLayers: "Videos"]
 ]
@@ -38,4 +39,16 @@ wms.data.video = [
 login.registration.enabled=true
 login.registration.userVerification="manual"
 login.registration.createLdapUser=false
-login.registration.useMail=(userVerification == "email")
+login.registration.useMail=(login.registration.userVerification== "email")
+
+security {
+//  level = 'UNCLASS'
+//level = 'SECRET'
+//level = 'TOPSECRET'
+    UNCLASS = [description: "Unclassified", color: "green"]
+    SECRET = [description: "Secret // NOFORN", color: "red"]
+    TOPSECRET = [description: "Top Secret", color: "yellow"]
+    sessionTimeout = 60
+    level = "UNCLASS"
+}
+
