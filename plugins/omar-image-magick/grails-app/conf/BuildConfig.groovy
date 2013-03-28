9d0b565487afefdc1977fe73da2cfd474f24506c
Scott Bortman <scott.bortman@gmail.com>
2013-03-28 10:21:13 -0400

    Upgraded to gpars 1.0.0 from 0.12

--- plugins/omar-image-magick/grails-app/conf/BuildConfig.groovy
+++ plugins/omar-image-magick/grails-app/conf/BuildConfig.groovy
@@ -27,7 +27,7 @@ grails.project.dependency.resolution = {
     // specify dependencies here under either 'build', 'compile', 'runtime', 'test' or 'provided' scopes eg.
 
     // runtime 'mysql:mysql-connector-java:5.1.5'
-    compile "org.codehaus.gpars:gpars:0.12"
+    compile "org.codehaus.gpars:gpars:1.0.0"
   }
 
   plugins {
