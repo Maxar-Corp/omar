ffff7a8d8b3a9dc0bfd8984570779b20743e0ed5
Scott Bortman <scott.bortman@gmail.com>
2013-03-28 13:08:03 -0400

    For some reason,  the URL mappings for kmlQuery were commented out.   Need to figure out why....

--- plugins/omar-raster/grails-app/conf/RasterUrlMappings.groovy
+++ plugins/omar-raster/grails-app/conf/RasterUrlMappings.groovy
@@ -11,9 +11,10 @@ class RasterUrlMappings {
          *  Commenting out the URL mappings fixes this.  Not sure why yet.
          */
 
-//    "/kmlQuery/topImages"(controller: "rasterKmlQuery", action: "topImages")
-//    "/kmlQuery/imageFootprints"(controller: "rasterKmlQuery", action: "imageFootprints")
-//    "/kmlQuery/getImagesKml"(controller: "rasterKmlQuery", action: "getImagesKml")
+    "/kmlQuery/topImages"(controller: "rasterKmlQuery", action: "topImages")
+    "/kmlQuery/imageFootprints"(controller: "rasterKmlQuery", action: "imageFootprints")
+    "/kmlQuery/getImagesKml"(controller: "rasterKmlQuery", action: "getImagesKml")
+
 		"/$controller/$action?/$id?"{
 			constraints {
 				// apply constraints here
