9df07c1405bc77367e43c1aa85d78578188ae23d
Garrett Potts <potts@cfl.rr.com>
2013-05-13 12:13:59 -0400

    Removed dead code that was comented out

--- plugins/omar-raster/OmarRasterGrailsPlugin.groovy
+++ plugins/omar-raster/OmarRasterGrailsPlugin.groovy
@@ -48,50 +48,6 @@ OMAR Raster support
     imagerySearchService( RasterEntrySearchService ) {
       grailsApplication = ref( "grailsApplication" )
     }
-
-    /*
-    byFileType( org.ossim.omar.raster.PropertyNameStyle ) { bean ->
-      propertyName = 'fileType'
-      outlineLookupTable = [
-          //aaigrid: 4,
-          cadrg: new Color( 0, 255, 255, 255 ),
-          cib: new Color(0,128,128,255),
-          ccf: new Color( 128, 100, 255, 255 ),
-          adrg: new Color( 50, 111, 111, 255 ),
-          mrsid: new Color(0,188,0,255),
-          //doqq: 2,
-          dted: new Color( 0, 255, 0, 255 ), // green
-          jpeg: new Color( 255, 255, 0, 255 ), // yellow
-          jpeg2000: new Color( 255, 200, 0, 255 ), //
-          landsat7: new Color( 255, 0, 255, 255 ), // purple
-          nitf: new Color( 0, 0, 255, 255 ),  // blue
-          tiff: new Color( 255, 0, 0, 255 ),  // red
-          mpeg: new Color(164,254,255,255),
-          unspecified: new Color(255,255,255,255) // white
-      ]
-    }
-
-    bySensorId( org.ossim.omar.raster.PropertyNameStyle ) { bean ->
-      propertyName = 'sensorId'
-      outlineLookupTable = [
-          'ACES_YOGI-HRI1': new Color( 255, 0, 0, 255 ),
-          'ACES_YOGI-HRI2': new Color( 255, 0, 0, 255 ),
-          'ACES_YOGI-HRI3': new Color( 255, 0, 0, 255 ),
-          'ACES_YOGI-HSI': new Color( 255, 255, 0, 255 ),
-          ALPHA: new Color( 255, 0, 255, 255 ),
-          BRAVO: new Color( 0, 255, 0, 255 ),
-          CHARLIE: new Color( 0, 255, 255, 255 ),
-          DELTA: new Color( 0, 0, 255, 255 ),
-          unspecified: new Color(255,255,255,255) // white
-      ]
-    }
-
-
-    red( org.ossim.omar.raster.PropertyNameStyle ) { bean ->
-      propertyName = 'id'
-      defaultOutlineColor = new Color( 255, 0, 0, 255 )  // red
-    }
-    */
   }
 
   def doWithDynamicMethods = { ctx ->
