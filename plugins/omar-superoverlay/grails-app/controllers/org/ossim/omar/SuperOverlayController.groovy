23ad2f2fc9467ecb26c2fa66e73d53928961dcb5
Garrett Potts <potts@cfl.rr.com>
2013-05-17 13:11:08 -0400

    Added try catch so we can catch any problems

--- plugins/omar-superoverlay/grails-app/controllers/org/ossim/omar/SuperOverlayController.groovy
+++ plugins/omar-superoverlay/grails-app/controllers/org/ossim/omar/SuperOverlayController.groovy
@@ -19,103 +19,129 @@ class SuperOverlayController implements InitializingBean
   def outputKmz = false
 
   def index()
-  { render "" }
+  {
+    render ""
+  }
 
   def createKml()
   {
-    def rasterEntry = null
-    try
-    {
-      if ( params.id )
-      {
-        rasterEntry = RasterEntry.compositeId( params.id as String ).findWhere()
-      }
-    }
-    catch ( Exception e )
-    {
-      rasterEntry = null
-    }
+    try{
 
-    if ( rasterEntry )
-    {
-      def tempOutputKmz = outputKmz
-
-      // There is currently a bug when a comma separated band list is given for kmz output
-      // we will force to false if such a param is given.
-      // For some reason if bands= is given and has more than one
-      // band indicator it causes X to appear on the google earth window.
-      // STill can't locate the reasoning so for now if KMZ is enabled and
-      // we see a band= with more than 1 band indicator we will force to false and
-      // output as a KML and add a href to the WMS chip service instead of embedding the chip
-      // within the KMZ.
-      //
-      def caseInsensitiveParams = new CaseInsensitiveMap( params )
-      if ( tempOutputKmz && caseInsensitiveParams.bands )
+      def rasterEntry = null
+      try
       {
-        tempOutputKmz = caseInsensitiveParams.bands.split( "," ).length < 2
-      }
-      // we will return the root document if any level of detail param is null
-      //
-      def isRoot = ( ( params.level == null ) && ( params.row == null ) && ( params.col == null ) )
-      if ( !tempOutputKmz )
-      {
-        if ( !isRoot )
+        if ( params.id )
         {
-          def kmlString = superOverlayService.createTileKml( rasterEntry, params )
-          //response.setDateHeader("Expires", System.currentTimeMillis()+(24*24*60*60*1000));
-          // response.addHeader("Cache-Control", "max-age=120")
-          //   response.setHeader("max-age", "120");
-          render( contentType: "application/vnd.google-earth.kml+xml", text: kmlString,
-              encoding: "UTF-8" )
-        }
-        else
-        {
-          def kmlString = superOverlayService.createRootKml( rasterEntry, params )
-          response.setHeader( "Content-disposition", "attachment; filename=doc.kml" )
-          render( contentType: "application/vnd.google-earth.kml+xml",
-              text: kmlString,
-              encoding: "UTF-8" )
+          rasterEntry = RasterEntry.compositeId( params.id as String ).findWhere()
         }
       }
-      else
+      catch ( Exception e )
       {
-        if ( !isRoot )
+        rasterEntry = null
+      }
+
+      if ( rasterEntry )
+      {
+        def tempOutputKmz = outputKmz
+
+        // There is currently a bug when a comma separated band list is given for kmz output
+        // we will force to false if such a param is given.
+        // For some reason if bands= is given and has more than one
+        // band indicator it causes X to appear on the google earth window.
+        // STill can't locate the reasoning so for now if KMZ is enabled and
+        // we see a band= with more than 1 band indicator we will force to false and
+        // output as a KML and add a href to the WMS chip service instead of embedding the chip
+        // within the KMZ.
+        //
+        def caseInsensitiveParams = new CaseInsensitiveMap( params )
+        if ( tempOutputKmz && caseInsensitiveParams.bands )
         {
-          def kmlInfoMap = superOverlayService.createTileKmzInfo( rasterEntry, params )
-          response.contentType = "application/vnd.google-earth.kmz"
-          response.setHeader( "Content-disposition", "attachment; filename=output.kmz" )
-
-          def zos = new ZipOutputStream( response.outputStream )
-          //create a new zip entry
-          def anEntry = null
-
-          anEntry = new ZipEntry( "doc.kml" );
-          //place the zip entry in the ZipOutputStream object
-          zos.putNextEntry( anEntry );
-
-          zos << kmlInfoMap.kml
-          if ( kmlInfoMap.imagePath )
+          tempOutputKmz = caseInsensitiveParams.bands.split( "," ).length < 2
+        }
+        // we will return the root document if any level of detail param is null
+        //
+        def isRoot = ( ( params.level == null ) && ( params.row == null ) && ( params.col == null ) )
+        if ( !tempOutputKmz )
+        {
+          if ( !isRoot )
           {
-            anEntry = new ZipEntry( "${kmlInfoMap.imagePath}" );
-            //place the zip entry in the ZipOutputStream object
-            zos.putNextEntry( anEntry );
-            if ( kmlInfoMap.image )
+            try{
+              def kmlString = superOverlayService.createTileKml( rasterEntry, params )
+              //response.setDateHeader("Expires", System.currentTimeMillis()+(24*24*60*60*1000));
+              // response.addHeader("Cache-Control", "max-age=120")
+              //   response.setHeader("max-age", "120");
+              render( contentType: "application/vnd.google-earth.kml+xml", text: kmlString,
+                      encoding: "UTF-8" )
+            }
+            catch(e)
             {
-              ImageIO.write( kmlInfoMap.image, kmlInfoMap.format, zos );
+
             }
           }
-          zos.close();
-          response.outputStream.close();
+          else
+          {
+            try{
+              def kmlString = superOverlayService.createRootKml( rasterEntry, params )
+              response.setHeader( "Content-disposition", "attachment; filename=doc.kml" )
+              render( contentType: "application/vnd.google-earth.kml+xml",
+                      text: kmlString,
+                      encoding: "UTF-8" )
+            }
+            catch(e)
+            {
+
+            }
+          }
         }
         else
         {
-          def kmlString = superOverlayService.createRootKml( rasterEntry, params )
-          response.setHeader( "Content-disposition", "attachment; filename=doc.kml" )
-          render( contentType: "application/vnd.google-earth.kml+xml",
-              text: kmlString,
-              encoding: "UTF-8" )
+          if ( !isRoot )
+          {
+            def kmlInfoMap = superOverlayService.createTileKmzInfo( rasterEntry, params )
+            response.contentType = "application/vnd.google-earth.kmz"
+            response.setHeader( "Content-disposition", "attachment; filename=output.kmz" )
+
+            def zos = new ZipOutputStream( response.outputStream )
+            //create a new zip entry
+            def anEntry = null
+
+            anEntry = new ZipEntry( "doc.kml" );
+            //place the zip entry in the ZipOutputStream object
+            zos.putNextEntry( anEntry );
+
+            zos << kmlInfoMap.kml
+            if ( kmlInfoMap.imagePath )
+            {
+              anEntry = new ZipEntry( "${kmlInfoMap.imagePath}" );
+              //place the zip entry in the ZipOutputStream object
+              zos.putNextEntry( anEntry );
+              if ( kmlInfoMap.image )
+              {
+                ImageIO.write( kmlInfoMap.image, kmlInfoMap.format, zos );
+              }
+            }
+            zos.close();
+          }
+          else
+          {
+            try{
+              def kmlString = superOverlayService.createRootKml( rasterEntry, params )
+              response.setHeader( "Content-disposition", "attachment; filename=doc.kml" )
+              render( contentType: "application/vnd.google-earth.kml+xml",
+                      text: kmlString,
+                      encoding: "UTF-8" )
+            }
+            catch(e)
+            {
+
+            }
+          }
         }
       }
+    }
+    catch(e)
+    {
+
     }
     null
   }
