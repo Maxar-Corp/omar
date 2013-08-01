b4bde1e3d67309dcf81de496a39720d68a8884ab
Scott Bortman <scott.bortman@gmail.com>
2013-08-01 10:37:38 -0400

    Upgraded the database migration plugin from 1.3.3 to 1.3.5

--- apps/geodata-test/grails-app/conf/BuildConfig.groovy
+++ apps/geodata-test/grails-app/conf/BuildConfig.groovy
@@ -60,7 +60,7 @@ grails.project.dependency.resolution = {
 
     build ":tomcat:$grailsVersion"
 
-    runtime ":database-migration:1.3.2"
+    runtime ":database-migration:1.3.5"
 
     compile ':filterpane:2.2.1'
     compile ':cache:1.0.1'
--- apps/omar/grails-app/conf/BuildConfig.groovy
+++ apps/omar/grails-app/conf/BuildConfig.groovy
@@ -65,7 +65,7 @@ grails.project.dependency.resolution = {
 
     build ":tomcat:$grailsVersion"
 
-    runtime ":database-migration:1.3.3"
+    runtime ":database-migration:1.3.5"
 
     compile ':cache:1.0.1'
   }
