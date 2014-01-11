7164bd06361b63b294459626ccb06e01c10d9fa4
Scott Bortman <scott.bortman@gmail.com>
2014-01-11 17:16:23 -0500

    Redirect / to two color multi

--- apps/chipper/grails-app/conf/UrlMappings.groovy
+++ apps/chipper/grails-app/conf/UrlMappings.groovy
@@ -1,13 +1,15 @@
-class UrlMappings {
+class UrlMappings
+{
 
-	static mappings = {
-		"/$controller/$action?/$id?"{
-			constraints {
-				// apply constraints here
-			}
-		}
+  static mappings = {
+    "/$controller/$action?/$id?" {
+      constraints {
+        // apply constraints here
+      }
+    }
 
-		"/"(view:"/index")
-		"500"(view:'/error')
-	}
+//    "/"( view: "/index" )
+    "/"( controller: 'twoColorMulti' )
+    "500"( view: '/error' )
+  }
 }
