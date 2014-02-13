18d096f549f321373716ad5fafaee643c6a2adbb
David Burken <dburken@comcast.net>
2014-02-13 13:40:39 -0500

    Added note on thumbnail size.

--- apps/omar/grails-app/conf/Config.groovy
+++ apps/omar/grails-app/conf/Config.groovy
@@ -238,7 +238,9 @@ wms {
   }
 }
 
-
+//---
+// Note: defaultSize of 512 was killing speed on search page when browser is forced to resample.
+//---
 thumbnail {
     cacheDir = ( System.properties["os.name"].contains("Windows") ) ? "c:/temp" : "${ wms.referenceDataDirectory }/omar-cache"
     defaultSize = 128
