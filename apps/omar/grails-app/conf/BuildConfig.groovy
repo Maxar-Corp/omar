1a684c5376a72bd7c2bc2fe74645b1604f52c44a
Scott Bortman <scott.bortman@gmail.com>
2015-03-03 14:45:25 -0500

    Took JAI stuff out of GeoScript deps, assuming it will be installed at JDK level.
    Upgraded version of jQuery

--- apps/omar/grails-app/conf/BuildConfig.groovy
+++ apps/omar/grails-app/conf/BuildConfig.groovy
@@ -60,7 +60,7 @@ grails.project.dependency.resolution = {
 
   plugins {
     runtime ":hibernate:$grailsVersion"
-    runtime ":jquery:1.10.2"
+    runtime ":jquery:1.11.0.1"
     runtime ":resources:1.2.8"
 
     // Uncomment these (or add new ones) to enable additional resources capabilities
