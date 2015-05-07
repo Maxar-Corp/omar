063042bed74fc9e155a202c1fd5f0c0e86b213d8
Scott Bortman <scott.bortman@gmail.com>
2015-05-07 18:15:56 -0400

    These are new files created as part of the port to Grails 2.5.0. More to come.

---
+++ plugins/omar-common-ui/grails-app/assets/javascripts/homePage.js
@@ -0,0 +1,5 @@
+//= require yui/yahoo-dom-event/yahoo-dom-event.js
+//= require yui/element/element-min.js
+//= require yui/resize/resize-min.js
+//= require yui/layout/layout-min.js
+//= require homePageLayout.js
