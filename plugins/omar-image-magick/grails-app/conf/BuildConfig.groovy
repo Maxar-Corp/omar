1f311e38d1ec43b3c2513147851bcb5f085de3f4
Scott Bortman <scott.bortman@gmail.com>
2015-06-23 14:57:20 -0400

    Upgraded ImageMagick plugin to Grails 2.5.x

--- plugins/omar-image-magick/grails-app/conf/BuildConfig.groovy
+++ plugins/omar-image-magick/grails-app/conf/BuildConfig.groovy
@@ -1,10 +1,22 @@
-grails.work.dir="${System.env.OMAR_DEV_HOME}/.grails"
-grails.dependency.cache.dir = "${System.env.OMAR_DEV_HOME}/.grails/ivy-cache"
-
 grails.project.class.dir = "target/classes"
 grails.project.test.class.dir = "target/test-classes"
 grails.project.test.reports.dir = "target/test-reports"
 
+grails.project.fork = [
+    // configure settings for compilation JVM, note that if you alter the Groovy version forked compilation is required
+    //  compile: [maxMemory: 256, minMemory: 64, debug: false, maxPerm: 256, daemon:true],
+
+    // configure settings for the test-app JVM, uses the daemon by default
+    test: [maxMemory: 768, minMemory: 64, debug: false, maxPerm: 256, daemon:true],
+    // configure settings for the run-app JVM
+    run: [maxMemory: 768, minMemory: 64, debug: false, maxPerm: 256, forkReserve:false],
+    // configure settings for the run-war JVM
+    war: [maxMemory: 768, minMemory: 64, debug: false, maxPerm: 256, forkReserve:false],
+    // configure settings for the Console UI JVM
+    console: [maxMemory: 768, minMemory: 64, debug: false, maxPerm: 256]
+]
+
+grails.project.dependency.resolver = "maven" // or ivy
 grails.project.dependency.resolution = {
     // inherit Grails' default dependencies
     inherits("global") {
@@ -12,35 +24,28 @@ grails.project.dependency.resolution = {
         // excludes 'ehcache'
     }
     log "warn" // log level of Ivy resolver, either 'error', 'warn', 'info', 'debug' or 'verbose'
-    legacyResolve false // whether to do a secondary resolve on plugin installation, not advised and here for backwards compatibility
     repositories {
-        mavenRepo "http://repo.grails.org/grails/plugins/"
-
-        //grailsCentral()
+        grailsCentral()
+        mavenLocal()
         mavenCentral()
         // uncomment the below to enable remote dependency resolution
         // from public Maven repositories
-        mavenLocal()
-        //mavenRepo "http://snapshots.repository.codehaus.org"
         //mavenRepo "http://repository.codehaus.org"
         //mavenRepo "http://download.java.net/maven/2/"
         //mavenRepo "http://repository.jboss.com/maven2/"
     }
     dependencies {
         // specify dependencies here under either 'build', 'compile', 'runtime', 'test' or 'provided' scopes eg.
-
-        // runtime 'mysql:mysql-connector-java:5.1.21'
-        compile 'org.codehaus.gpars:gpars:1.0.0'
-
+        // runtime 'mysql:mysql-connector-java:5.1.27'
     }
 
     plugins {
-        build(":tomcat:$grailsVersion",
-              ":release:2.2.1",
-              ":rest-client-builder:1.0.3") {
+        build(":release:3.1.0",
+              ":rest-client-builder:2.1.0") {
             export = false
         }
     }
 }
 
-grails.plugin.location.omarCore="${System.getenv('OMAR_DEV_HOME')}/plugins/omar-core"
+//grails.plugin.location.omarOMS='../../plugins/omar-oms'
+grails.plugin.location.omarCore='../../plugins/omar-core'
