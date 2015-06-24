0d920401afad5f95343d3f20958460dc8d01f9b7
Scott Bortman <scott.bortman@gmail.com>
2015-06-24 14:14:37 -0400

    Added mavenRepo for vector tiles jar dependency

--- plugins/geoscript/grails-app/conf/BuildConfig.groovy
+++ plugins/geoscript/grails-app/conf/BuildConfig.groovy
@@ -7,56 +7,57 @@ grails.project.fork = [
     //  compile: [maxMemory: 256, minMemory: 64, debug: false, maxPerm: 256, daemon:true],
 
     // configure settings for the test-app JVM, uses the daemon by default
-    test: [maxMemory: 768, minMemory: 64, debug: false, maxPerm: 256, daemon:true],
+    test: [maxMemory: 768, minMemory: 64, debug: false, maxPerm: 256, daemon: true],
     // configure settings for the run-app JVM
-    run: [maxMemory: 768, minMemory: 64, debug: false, maxPerm: 256, forkReserve:false],
+    run: [maxMemory: 768, minMemory: 64, debug: false, maxPerm: 256, forkReserve: false],
     // configure settings for the run-war JVM
-    war: [maxMemory: 768, minMemory: 64, debug: false, maxPerm: 256, forkReserve:false],
+    war: [maxMemory: 768, minMemory: 64, debug: false, maxPerm: 256, forkReserve: false],
     // configure settings for the Console UI JVM
     console: [maxMemory: 768, minMemory: 64, debug: false, maxPerm: 256]
 ]
 
 grails.project.dependency.resolver = "maven" // or ivy
 grails.project.dependency.resolution = {
-    // inherit Grails' default dependencies
-    inherits("global") {
-        // uncomment to disable ehcache
-        // excludes 'ehcache'
-    }
-    log "warn" // log level of Ivy resolver, either 'error', 'warn', 'info', 'debug' or 'verbose'
-    repositories {
-        mavenRepo 'http://repo.boundlessgeo.com/main'
+  // inherit Grails' default dependencies
+  inherits( "global" ) {
+    // uncomment to disable ehcache
+    // excludes 'ehcache'
+  }
+  log "warn" // log level of Ivy resolver, either 'error', 'warn', 'info', 'debug' or 'verbose'
+  repositories {
+    mavenRepo 'http://repo.boundlessgeo.com/main'
+    mavenRepo 'https://github.com/ElectronicChartCentre/ecc-mvn-repo/raw/master/releases'
 
-        grailsCentral()
-        mavenLocal()
-        mavenCentral()
-        // uncomment the below to enable remote dependency resolution
-        // from public Maven repositories
-        //mavenRepo "http://repository.codehaus.org"
-        //mavenRepo "http://download.java.net/maven/2/"
-        //mavenRepo "http://repository.jboss.com/maven2/"
-    }
-    dependencies {
-        // specify dependencies here under either 'build', 'compile', 'runtime', 'test' or 'provided' scopes eg.
-        // runtime 'mysql:mysql-connector-java:5.1.27'
-        compile 'net.sf.opencsv:opencsv:2.3'
-        compile 'org.postgresql:postgresql:9.3-1103-jdbc41'
+    grailsCentral()
+    mavenLocal()
+    mavenCentral()
+    // uncomment the below to enable remote dependency resolution
+    // from public Maven repositories
+    //mavenRepo "http://repository.codehaus.org"
+    //mavenRepo "http://download.java.net/maven/2/"
+    //mavenRepo "http://repository.jboss.com/maven2/"
+  }
+  dependencies {
+    // specify dependencies here under either 'build', 'compile', 'runtime', 'test' or 'provided' scopes eg.
+    // runtime 'mysql:mysql-connector-java:5.1.27'
+    compile 'net.sf.opencsv:opencsv:2.3'
+    compile 'org.postgresql:postgresql:9.3-1103-jdbc41'
 
-        compile( 'org.geoscript:geoscript-groovy:1.5.0' ) {
-          excludes "slf4j-log4j12",
-              'ehcache',
-              "jai-core",
-              "jai-codec",
-              "jai_imageio",
-              "postgresql"
-              "opencsv"
-        }        
+    compile( 'org.geoscript:geoscript-groovy:1.5.0' ) {
+      excludes "slf4j-log4j12",
+          'ehcache',
+          "jai-core",
+          "jai-codec",
+          "jai_imageio",
+          "postgresql"
+      "opencsv"
     }
+  }
 
-    plugins {
-        build(":release:3.1.0",
-              ":rest-client-builder:2.1.0") {
-            export = false
-        }
+  plugins {
+    build( ":release:3.1.0",
+        ":rest-client-builder:2.1.0" ) {
+      export = false
     }
+  }
 }
