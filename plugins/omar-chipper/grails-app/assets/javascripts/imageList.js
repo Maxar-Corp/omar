57277ea9b9796cf021d56c031e8c556e838054a3
Scott Bortman <scott.bortman@gmail.com>
2015-07-08 12:01:14 -0400

    Fixed path to OpenLayers for Product Builder

--- plugins/omar-chipper/grails-app/assets/javascripts/imageList.js
+++ plugins/omar-chipper/grails-app/assets/javascripts/imageList.js
@@ -2,7 +2,7 @@
 //= require jquery-easyui/jquery.easyui.min.js
 // require jQuery/jquery-ui.js
 //= require jQuery/jquery.layout.js
-//= require openlayers/OpenLayers.js
+//= require OpenLayers.js
 //= require backbone/underscore-min.js
 //= require backbone/backbone-min.js
 
