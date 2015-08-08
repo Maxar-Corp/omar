d754ec14c8727612d32ca2028be9dc9b058faa66
Garrett Potts <gpotts@radiantblue.com>
2015-08-08 18:18:51 -0400

    Added mail plugin but might not need it for I can use a mailto link for feedback. Will do an interim checkin

--- plugins/omar-core/grails-app/conf/CoreBootStrap.groovy
+++ plugins/omar-core/grails-app/conf/CoreBootStrap.groovy
@@ -1,6 +1,5 @@
 import grails.util.GrailsUtil
 import groovy.sql.Sql
-import org.ossim.omar.core.Report
 import org.ossim.omar.core.Repository
 
 import javax.sql.DataSource
@@ -11,35 +10,5 @@ class CoreBootStrap
 
   def init = { servletContext ->
 
-    def sql = new Sql( dataSourceUnproxied as DataSource )
-
-    if ( Report.count() == 0 )
-    {
-//      sql.execute( "ALTER TABLE report alter column date_created type timestamp with time zone" )
-//      sql.execute( "ALTER TABLE report alter column last_updated type timestamp with time zone" )
-    }
-
-    if ( Repository.count() == 0 )
-    {
-//      sql.execute( "ALTER TABLE repository alter column scan_start_date type timestamp with time zone" )
-//      sql.execute( "ALTER TABLE repository alter column scan_end_date type timestamp with time zone" )
-
-//      def baseDirs
-
-//      if ( GrailsUtil.isDevelopmentEnv() )
-//      {
-//        baseDirs = ["/", "/data/uav", "/Volumes/Iomega_HDD/data"]
-//      }
-//      else
-//      {
-//        baseDirs = ["/"]
-//      }
-
-//      baseDirs.each { baseDir ->
-//        Repository.findOrSaveWhere( baseDir: baseDir )
-//      }
-    }
-
-    sql?.close()
   }
 }
