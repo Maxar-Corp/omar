f10f230d2362640efc15ffb246cc8db45a5d93ed
Garrett Potts <gpotts@radiantblue.com>
2015-08-13 07:32:13 -0400

    Added a disable support

--- plugins/omar-opensearch/grails-app/taglib/org/ossim/omar/opensearch/tag/OpensearchDescriptorTagLib.groovy
+++ plugins/omar-opensearch/grails-app/taglib/org/ossim/omar/opensearch/tag/OpensearchDescriptorTagLib.groovy
@@ -4,11 +4,14 @@ class OpensearchDescriptorTagLib {
     static namespace = "opensearch"
     static encodeAsForTags = [descriptors: [taglib:'none']]
     def descriptors = {attrs ->
-        grailsApplication.config.opensearch.descriptorList.each{k,v->
-            String tagString = "<link rel='search' type='application/opensearchdescription+xml' href='${v.href}' title='${v.title}' />"
+        if(grailsApplication.config.opensearch.enabled)
+        {
+            grailsApplication.config.opensearch.pluginList.each{k,v->
+                String tagString = "<link rel='search' type='application/opensearchdescription+xml' href='${v.href}' title='${v.title}' />"
 
-            out<<tagString
+                out<<tagString
 
+            }
         }
     }
 }
