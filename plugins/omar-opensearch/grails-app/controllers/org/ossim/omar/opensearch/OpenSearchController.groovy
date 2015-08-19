28d101797274f520e47b2316e845e6e1d237b2f5
Garrett Potts <gpotts@radiantblue.com>
2015-08-19 13:26:16 -0400

    Removed debug

--- plugins/omar-opensearch/grails-app/controllers/org/ossim/omar/opensearch/OpenSearchController.groovy
+++ plugins/omar-opensearch/grails-app/controllers/org/ossim/omar/opensearch/OpenSearchController.groovy
@@ -11,7 +11,6 @@ class OpenSearchController {
     def generalSearch()
     {
         def cqlFilter = ""
-        println params
         if(!params.s)
         {
             // need to create the filter
@@ -29,9 +28,7 @@ class OpenSearchController {
     }
     def searchDescriptor()
     {
-        println params
         String description = grailsApplication?.config?.opensearch?.pluginList?."${params.name}"?.description
-        println description
 
         render contentType: "application/xml", text: description?:""
     }
