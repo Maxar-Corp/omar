fad4bfc3829c2986644933c6ab97db7f951e8d2d
Garrett Potts <gpotts@radiantblue.com>
2015-10-13 14:00:30 -0400

    Added timezone sql alteration

---
+++ support/sql/alter_timezones.sql
@@ -0,0 +1,4 @@
+alter table raster_entry alter COLUMN acquisition_date type timestamp with time zone;
+alter table raster_entry alter COLUMN access_date type timestamp with time zone;
+alter table raster_entry alter COLUMN ingest_date type timestamp with time zone;
+alter table raster_entry alter COLUMN receive_date type timestamp with time zone;
