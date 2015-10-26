ed9262bcfa714d0efe39186d3bb7a07f73ebd574
Garrett Potts <gpotts@radiantblue.com>
2015-10-26 09:29:17 -0400

    Adding buld scripts for gocd pipeline

---
+++ build_scripts/linux/setup.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+if [ ! -d "$HOME/.sdkman" ]; then
+   curl -s get.sdkman.io | bash
+fi
+source "$HOME/.sdkman/bin/sdkman-init.sh"
+
+#if [ ! -z "$GROOVY_VERSION" ]; then
+#   if [ ! -d "$HOME/.sdkman/groovy/$GROOVY_VERSION" ]; then
+#      sdk install groovy $GROOVY_VERSION
+#   fi
+#else
+#   echo "******OMAR SETUP ERROR: Environment variable GROOVY_VERSION must be set"
+#   exit 1
+#fi
+
+if [ ! -z "$GRAILS_VERSION" ]; then
+   if [ ! -d "$HOME/.sdkman/grails/$GRAILS_VERSION" ]; then
+      sdk install grails $GRAILS_VERSION
+   fi
+else
+   echo "******OMAR SETUP ERROR: Environment variable GRAILS_VERSION must be set"
+   exit 1
+fi
+
+#if [ ! -z "$GRADLE_VERSION" ]; then
+#   if [ ! -d "$HOME/.sdkman/gradle/$GRADLE_VERSION" ]; then
+#      sdk install gradle $GRADLE_VERSION
+#   fi
+#else
+#   echo "******OMAR SETUP ERROR: Environment variable GRADLE_VERSION must be set"
+#   exit 1
+#fi
