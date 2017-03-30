9035aafdf072ab716897ee1005ae88d3833af3b6
Garrett Potts <gpotts@radiantblue.com>
2017-03-30 10:09:01 -0400

    Added ossim-deps

--- build_scripts/linux/setup.sh
+++ build_scripts/linux/setup.sh
@@ -22,7 +22,7 @@ else
    echo "******OMAR SETUP ERROR: Environment variable GRAILS_VERSION must be set"
    exit 1
 fi
-export OSSIM_MAVEN_PROXY="https://artifacts.radiantbluecloud.com/artifactory"
+export OSSIM_MAVEN_PROXY="https://artifacts.radiantbluecloud.com/artifactory/ossim-deps"
 
 #if [ ! -z "$GRADLE_VERSION" ]; then
 #   if [ ! -d "$HOME/.sdkman/gradle/$GRADLE_VERSION" ]; then
