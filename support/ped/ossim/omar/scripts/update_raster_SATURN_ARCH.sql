ac0bd55cbfdbfb3ec7e5040e3778c56ef9cd96b1
Scott Bortman <scott.bortman@gmail.com>
2017-12-21 13:39:05 -0500

    added missing mission

--- support/ped/ossim/omar/scripts/update_raster_SATURN_ARCH.sql
+++ support/ped/ossim/omar/scripts/update_raster_SATURN_ARCH.sql
@@ -3,6 +3,7 @@ UPDATE raster_entry SET mission_id='Y1' where filename LIKE '%Y1%' and mission_i
 UPDATE raster_entry SET mission_id='YL' where filename LIKE '%YL%' and mission_id IS NULL;
 UPDATE raster_entry SET mission_id='SAMI' where filename LIKE '%SAMI%' and mission_id IS NULL;
 UPDATE raster_entry SET mission_id='INUKA' where filename LIKE '%INUKA%' and mission_id IS NULL;
+UPDATE raster_entry SET mission_id='KODIAK' where filename LIKE '%KODIAK%' and mission_id IS NULL;
 
 UPDATE raster_entry SET sensor_id='ALPHA' where filename LIKE '%ALPHA%' and sensor_id IS NULL;
 UPDATE raster_entry SET sensor_id='BRAVO' where filename LIKE '%BRAVO%' and sensor_id IS NULL;
